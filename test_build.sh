a6103ded645400c5bacdbd17b9d315dad6097a7a
smay1613 <dimaafa0@gmail.com>
2020-09-22 18:09:58 +0300

    Remove workflow

--- test_build.sh
+++
@@ -1,8 +0,0 @@
-find . -mindepth 1 -maxdepth 2 -name student | while read -r task
-do
-    echo "$task"
-    cd "$task" && cmake . && make -j4 && cd ..
-    if [[ "$?" -ne 0 ]]; then 
-      break
-    fi
-done
