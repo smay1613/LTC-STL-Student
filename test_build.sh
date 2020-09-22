e982ce2b7dd4a13f299321473b9376a3a655697a
smay1613 <dimaafa0@gmail.com>
2020-09-22 16:17:11 +0300

    Adds testing script

---
+++ test_build.sh
@@ -0,0 +1,8 @@
+find . -mindepth 1 -maxdepth 2 -name student | while read -r task
+do
+    echo "$task"
+    cd "$task" && cmake . -G "MSYS Makefiles" && make -j4 && cd ..
+    if [[ "$?" -ne 0 ]]; then 
+      break
+    fi
+done
