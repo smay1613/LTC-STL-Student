9037d6884232df5f2f34063e737fbbf159ab8a1e
smay1613 <dimaafa0@gmail.com>
2020-09-22 17:37:56 +0300

    Update build script

--- test_build.sh
+++ test_build.sh
@@ -1,7 +1,7 @@
 find . -mindepth 1 -maxdepth 2 -name student | while read -r task
 do
     echo "$task"
-    cd "$task" && cmake . -G "MSYS Makefiles" && make -j4 && cd ..
+    cd "$task" && cmake . && make -j4 && cd ..
     if [[ "$?" -ne 0 ]]; then 
       break
     fi
