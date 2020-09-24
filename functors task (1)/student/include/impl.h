28ad0189252b7a789c6f14bb54fc9f34153b9bbc
Ernest Nikonov <enikonov@luxoft.com>
2020-09-24 18:06:19 +0300

    Implement functors task #1

--- functors task (1)/student/include/impl.h
+++ functors task (1)/student/include/impl.h
@@ -1,6 +1,10 @@
 #pragma once
+
 #include "bar_serving.h"
+
 #include <functional>
+#include <map>
+#include <utility>
 
 /**
  * @todo Implement functor-generator that will return next beer brand (cyclic)
@@ -8,6 +12,26 @@
  */
 struct BeerOrganizer
 {
+    constexpr int beerBrandBegin() const
+    {
+        return static_cast<int>(BeerBrand::None) + 1;
+    }
+
+    constexpr int beerBrandEnd() const
+    {
+        return static_cast<int>(BeerBrand::Max);
+    }
+
+    BeerBrand operator()()
+    {
+        if (nextBeerBrand == beerBrandEnd())
+        {
+            nextBeerBrand = beerBrandBegin();
+        }
+        return static_cast<BeerBrand>(nextBeerBrand++);
+    }
+
+    int nextBeerBrand = beerBrandBegin();
 };
 
 /**
@@ -16,16 +40,18 @@ struct BeerOrganizer
  *
  * @note Only Corona and HoeGaarden are expensive
  */
-bool isExpensiveBeer(/**???*/)
+bool isExpensiveBeer(BeerBrand beerBrand)
 {
+    return beerBrand == BeerBrand::Corona || beerBrand == BeerBrand::HoeGaarden;
 }
 
 /**
  * @todo Implement lambda beer country equality comparator
  * @return true if beer county is the same, false otherwise
  */
-auto sameCountry = [](/**???*/)
+auto sameCountry = [](BeerBrand lhs, BeerBrand rhs)
 {
+    return getBeerCountry(lhs) == getBeerCountry(rhs);
 };
 
 struct MixingPolicy
@@ -40,9 +66,19 @@ struct MixingPolicy
      * Whiskey + SevenUp = SevenPlusSeven;
      * Others + Others = Oops;
      */
-    static Cocktail mix(/**???*/)
+    static Cocktail mix(AlcoholDrink alcoholDrink, NonAlcoholDrink nonAlcoholDrink)
     {
+        static const std::map<std::pair<AlcoholDrink, NonAlcoholDrink>, Cocktail> kCocktailsMap =
+        {
+            { { AlcoholDrink::Gin    , NonAlcoholDrink::LimeJuice       }, Cocktail::Gimlet         },
+            { { AlcoholDrink::Gin    , NonAlcoholDrink::GrapefruitJuice }, Cocktail::Greyhount      },
+            { { AlcoholDrink::Whiskey, NonAlcoholDrink::SevenUp         }, Cocktail::SevenPlusSeven },
+        };
+
+        const auto foundCocktail = kCocktailsMap.find({ alcoholDrink, nonAlcoholDrink });
+
+        return foundCocktail != kCocktailsMap.end() ? foundCocktail->second : Cocktail::Oops;
     }
 };
 
-std::function</**???*/> mixer {&MixingPolicy::mix};
+std::function<decltype(MixingPolicy::mix)> mixer {&MixingPolicy::mix};
