ce331f1d159a6b1ea7b9f6cd4ee2224f74fac151
Andrey Sakun <sakun.andrey@gmail.com>
2020-09-25 00:06:24 +0300

    changed according to review

--- functors task (1)/student/src/impl.cpp
+++ functors task (1)/student/src/impl.cpp
@@ -1,22 +1,32 @@
 #include "impl.h"
+#include <array>
 
 constexpr int count = 6;
-BeerBrand states[count] = { BeerBrand::HoeGaarden, BeerBrand::Corona, BeerBrand::Carlsberg, BeerBrand::Bud, BeerBrand::ZlataPraha, BeerBrand::Leffe };
+std::array<BeerBrand, count> states = { BeerBrand::HoeGaarden, BeerBrand::Corona, BeerBrand::Carlsberg, BeerBrand::Bud, BeerBrand::ZlataPraha, BeerBrand::Leffe };
 
 BeerBrand BeerOrganizer::operator()()
 {
-    BeerBrand ret = states[current%count];
+    BeerBrand ret = states[current % count];
     current++;
     return ret;
 }
 
 Cocktail MixingPolicy::mix(AlcoholDrink alcoDrink, NonAlcoholDrink nonAlcoDrink)
 {
-    if (alcoDrink == AlcoholDrink::Gin && nonAlcoDrink == NonAlcoholDrink::LimeJuice)
-        return Cocktail::Gimlet;
-    if (alcoDrink == AlcoholDrink::Gin && nonAlcoDrink == NonAlcoholDrink::GrapefruitJuice)
-        return Cocktail::Greyhount;
-    if (alcoDrink == AlcoholDrink::Whiskey && nonAlcoDrink == NonAlcoholDrink::SevenUp)
-        return Cocktail::SevenPlusSeven;
-    return Cocktail::Oops;
+    Cocktail result = Cocktail::Oops;
+    switch (nonAlcoDrink)
+    {
+    case NonAlcoholDrink::LimeJuice:
+        if (alcoDrink == AlcoholDrink::Gin) { result = Cocktail::Gimlet; }
+        break;
+    case NonAlcoholDrink::GrapefruitJuice:
+        if (alcoDrink == AlcoholDrink::Gin) { result = Cocktail::Greyhount; }
+        break;
+    case NonAlcoholDrink::SevenUp:
+        if (alcoDrink == AlcoholDrink::Whiskey) { result = Cocktail::SevenPlusSeven; }
+        break;
+    default:
+        break;
+    }
+    return result;
 }
