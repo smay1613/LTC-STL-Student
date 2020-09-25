c2513d1c2b37240f4b8305459ecebb783a74c751
Andrey Sakun <sakun.andrey@gmail.com>
2020-09-26 00:08:25 +0300

    Iterators_task_1

--- iterators task (1)/student/include/impl.h
+++ iterators task (1)/student/include/impl.h
@@ -2,20 +2,77 @@
 #include <iterator>
 
 template <class Container>
-struct accumulator
+struct accumulator 
 {
-    /** @todo Iterator traits*/
-    /** @todo Constructor with container*/
-    /** @todo Copy assignment operator*/
-    /** @todo Move assignment operator*/
+private:
+    Container& mContainer;
+public:
+    /** Iterator traits*/
+    using iterator_category = std::output_iterator_tag;
+    using value_type =  typename Container::value_type;
+    using difference_type = typename Container::difference_type;
+    using pointer = typename Container::pointer;
+    using reference = typename Container::reference;
+    
+    /** Constructor with container*/
+    explicit accumulator(Container & container)
+    : mContainer (container)
+    {
 
-    /** @todo Iterator operations */
+    }
+
+    /** Copy assignment operator*/  
+    accumulator& operator=(const value_type& value)
+    {
+	    mContainer.push_back(value);
+	    return *this;
+    }
+
+    template <typename T>
+    accumulator& operator=(const T& value)
+    {
+        for (auto && item: value)
+        {
+            mContainer.push_back(item);
+        }
+        return *this;
+    }
+
+    /** Move assignment operator*/
+    accumulator& operator=(value_type&& value)
+    {
+        mContainer.push_back(std::move(value));
+        return *this;
+    }
+
+    template <typename T>
+    accumulator& operator=(T&& value)
+    {
+        for (auto && item: value)
+        {
+            mContainer.push_back(std::move(item));
+        }
+        
+        return *this;
+    }
+
+    /** Iterator operations */
+    accumulator& operator*()
+    { 
+        return *this; 
+    }
+
+    accumulator& operator++()
+    { 
+        return *this; 
+    }
 };
 
 /**
- *  @todo Implement accumulator object creator
+ *  accumulator object creator
  */
 template<class Container>
-accumulator<Container> make_accumulator(Container& container)
+inline accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>(container);
 }
