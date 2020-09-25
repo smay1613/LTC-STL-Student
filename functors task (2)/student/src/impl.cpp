f0404d2fa9920c61b9d3086014753087c564134e
Dmitrii Melnichenko <dmelnichenko@luxoft.com>
2020-09-25 19:51:01 +0300

    functors task 2 melnichenko

--- functors task (2)/student/src/impl.cpp
+++ functors task (2)/student/src/impl.cpp
@@ -7,48 +7,48 @@ void DataBrowser::userLeave(const std::string &userId)
 
 bool DataBrowser::getDataType1(const std::string &userId, std::vector<size_t> &returnValues) const
 {
-    const auto& it = m_dataReaders.find(userId);
-    if (it == m_dataReaders.cend())
+    auto call = [&returnValues, this](const std::unique_ptr<IDataSelector>& selector)->bool
     {
-        return false;
-    }
-    if (it->second == nullptr)
-    {
-        return false;
-    }
-    return it->second->getDataType1(returnValues, 0);
+        auto func = [](IDataSelector* selector, std::vector<size_t> &returnValues)->bool
+        {
+            return selector->getDataType1(returnValues, 0);
+        };
+        return invokeDataRequest(func, selector, returnValues);
+    };
+
+    return safeCall(userId, call);
 }
 
 bool DataBrowser::getDataType2(std::vector<size_t> &returnValues, const std::string &userId) const
 {
-    const auto& it = m_dataReaders.find(userId);
-    if (it == m_dataReaders.cend())
+    auto call = [&returnValues, this](const std::unique_ptr<IDataSelector>& selector)->bool
     {
-        return false;
-    }
-    if (it->second == nullptr)
-    {
-        return false;
-    }
-    return it->second->getDataType2(returnValues);
+        auto func = [](IDataSelector* selector, std::vector<size_t> &returnValues)->bool
+        {
+            return selector->getDataType2(returnValues);
+        };
+        return invokeDataRequest(func, selector, returnValues);
+    };
+
+    return safeCall(userId, call);
 }
 
 bool DataBrowser::getDataType3(const std::string &userId, std::vector<std::string> &returnValues) const
 {
-    const auto& it = m_dataReaders.find(userId);
-    if (it == m_dataReaders.cend())
+    auto call = [this,&returnValues](const std::unique_ptr<IDataSelector>& selector)->bool
     {
-        return false;
-    }
-    if (it->second == nullptr)
-    {
-        return false;
-    }
+        auto func = [this](IDataSelector* selector, std::vector<std::string> &returnValues)->bool
+        {
+            std::deque<size_t> unprocessedResults {};
+            bool success = selector->getDataType3(unprocessedResults);
+            returnValues = process(unprocessedResults);
+            return success;
+        };
 
-    std::deque<size_t> unprocessedResults {};
-    bool success {it->second->getDataType3(unprocessedResults)};
-    returnValues = process(unprocessedResults);
-    return success;
+        return invokeDataRequest(func, selector, returnValues);
+    };
+
+    return safeCall(userId, call);
 }
 
 template<class T>
