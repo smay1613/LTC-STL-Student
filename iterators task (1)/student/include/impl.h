a4ce7ae385fabcdc1a56e5f52e52ad242e413440
Zhan Poplavskyi <gan.popl@gmail.com>
2020-09-27 03:10:56 +0300

    Add implementation for Iterators task 1

--- iterators task (1)/student/include/impl.h
+++ iterators task (1)/student/include/impl.h
@@ -4,12 +4,41 @@
 template <class Container>
 struct accumulator
 {
+private:
+    Container* m_container = nullptr;
+public:
     /** @todo Iterator traits*/
+    using iterator_category = std::output_iterator_tag;
+    using value_type = Container;
+    using difference_type = std::ptrdiff_t;
+    using pointer = Container*;
+    using reference = Container&;
+
     /** @todo Constructor with container*/
+    accumulator<Container>(Container& container)
+    {
+        m_container = &container;
+    }
+
     /** @todo Copy assignment operator*/
+    template<class AnyContainer>
+    accumulator<Container>& operator=(AnyContainer const& container)
+    {
+        m_container->insert(m_container->end(), container.cbegin(), container.cend());
+        return *this;
+    }
+
     /** @todo Move assignment operator*/
+    template<class AnyContainer>
+    accumulator<Container>& operator=(AnyContainer&& container)
+    {
+        m_container->insert(m_container->end(), std::make_move_iterator(container.begin()), std::make_move_iterator(container.end()));
+        return *this;
+    }
 
     /** @todo Iterator operations */
+    accumulator<Container>& operator*() { return *this; }
+    accumulator<Container>& operator++() { return *this; }
 };
 
 /**
@@ -18,4 +47,5 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>(container);
 }
