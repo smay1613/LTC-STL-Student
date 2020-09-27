637e464566de00187603d10c5d649fb6b3a63b8f
Yocto Admin <yoctoadm@kickseed.localdomain>
2020-09-27 23:18:01 +0000

    Fix compile errors. Pass all tests.

--- iterators task (1)/student/include/impl.h
+++ iterators task (1)/student/include/impl.h
@@ -2,14 +2,61 @@
 #include <iterator>
 
 template <class Container>
-struct accumulator
+struct accumulator : public std::iterator_traits< typename Container::iterator >
 {
     /** @todo Iterator traits*/
+    using iterator_category = std::output_iterator_tag;
+    // using value_type = typename Container::value_type;
+    // using pointer    = accumulator*;
+    // using reference  = accumulator&; 
+
     /** @todo Constructor with container*/
+
+    accumulator( Container& container ) : result( container )
+    {
+
+    }
+
     /** @todo Copy assignment operator*/
+    accumulator& operator=( const Container& rhs )
+    {
+        result += rhs;
+        return *this;
+    }
+
+    template<typename T>
+    accumulator& operator=( const T& rhs )
+    {
+        for ( const auto& it : rhs )
+        {
+            result.push_back( it );
+        }
+        return *this;
+    }
+
     /** @todo Move assignment operator*/
+    accumulator& operator=( Container&& rhs )
+    {
+        for ( auto& it : rhs )
+        {
+            result.push_back( std::move( it ) );
+        }
+        return *this;
+    }
 
     /** @todo Iterator operations */
+    accumulator& operator*( )
+    {
+        return *this;
+    }
+
+    accumulator& operator++( )
+    {
+        // move to next value
+        return *this;
+    }
+
+    Container& result;
 };
 
 /**
@@ -18,4 +65,5 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>( container );
 }
