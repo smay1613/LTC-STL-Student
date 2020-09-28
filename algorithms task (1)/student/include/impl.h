9ac1dc45d9fd5d12750ec8ef214bceee62b2e111
Artur Hnoinskyi <arti.gn.98@gmail.com>
2020-09-28 17:04:08 +0300

    Algoritms task 1 - Artur Hnoinskyi

--- algorithms task (1)/student/include/impl.h
+++ algorithms task (1)/student/include/impl.h
@@ -11,4 +11,12 @@
   * @return consecutive range
 */
 template<class FI, class Comparator = std::equal_to<typename std::iterator_traits<FI>::value_type>>
-std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {});
+std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {})
+{
+    auto firstEqual = std::adjacent_find(start, end, comp);
+    auto lastEqual = std::find_if_not(firstEqual, end, [&firstEqual](typename std::iterator_traits<FI>::value_type element)
+    {
+        return element == *firstEqual;
+    });
+    return std::make_pair(firstEqual, lastEqual);
+}
