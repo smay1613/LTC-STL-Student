f934a009000451ecda3ef4ccda9068af7a9e85ee
Andrey Sakun <sakun.andrey@gmail.com>
2020-09-28 16:43:14 +0300

    First solution

--- algorithms task (2)/student/include/impl.h
+++ algorithms task (2)/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <algorithm>
 
 /**
  * @todo Check if strings starts with another string
@@ -9,4 +10,12 @@
  *
  * @return true if source starts from a prefix
  */
-bool startsWith(const std::string& source, const std::string& prefix);
+bool startsWith(const std::string& source, const std::string& prefix)
+{
+    if (prefix.size() > source.size())
+    {
+        return false;
+    }
+    auto lastSource = source.begin() + prefix.size();
+    return std::equal(source.begin(), lastSource, prefix.begin());
+}
