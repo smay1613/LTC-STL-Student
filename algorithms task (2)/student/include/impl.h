28a3b681aebb96906bb2a14685f8ebe4c288a90d
Dima Spektor <d.spektor.v@gmail.com>
2020-09-28 16:38:05 +0300

    algorithms 2 Dima Spektor

--- algorithms task (2)/student/include/impl.h
+++ algorithms task (2)/student/include/impl.h
@@ -9,4 +9,7 @@
  *
  * @return true if source starts from a prefix
  */
-bool startsWith(const std::string& source, const std::string& prefix);
+bool startsWith(const std::string& source, const std::string& prefix)
+{
+    return std::equal(prefix.begin(), prefix.end(), source.begin());
+}
