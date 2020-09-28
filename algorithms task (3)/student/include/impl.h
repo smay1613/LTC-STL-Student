638f7fa46a9312adaab6df8cf96a0c0dcfb8d517
Mykola Kobets <mkobets@luxoft.com>
2020-09-29 02:46:42 +0900

    Algorithm task 3.

--- algorithms task (3)/student/include/impl.h
+++ algorithms task (3)/student/include/impl.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 /** @todo Implement right-shifting algorithm for bidirectional ranges
  *
  *  BI - bidirectional iterator type
@@ -13,4 +15,14 @@
  * @note behavior is undefined if shift out of bounds
  */
 template<class BiIt>
-BiIt shift_right_n(BiIt begin, BiIt end, typename BiIt::difference_type shift);
+BiIt shift_right_n(BiIt begin, BiIt end, typename BiIt::difference_type shift)
+{
+    ///Didn't get the idea for "end" parameter, as shift specifies the number of elements to shift.
+    ///begin+shift - start of the destination sequence
+    //return std::move_backward(begin, std::next(begin, shift), std::next(begin, shift*2));
+
+    //source & dest ranges never overlap, so move can be used instead of move_backward.
+    auto dest = std::next(begin, shift);
+    std::move(begin, dest, dest);
+    return dest;
+}
