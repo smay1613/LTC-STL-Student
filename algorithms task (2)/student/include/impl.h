1dc442a942f869d1994cf985d38735776ca51db4
Andrey Sakun <sakun.andrey@gmail.com>
2020-09-28 16:46:39 +0300

    alg 2

--- algorithms task (2)/student/include/impl.h
+++ algorithms task (2)/student/include/impl.h
@@ -10,12 +10,22 @@
  *
  * @return true if source starts from a prefix
  */
+
+// 1st solution
 bool startsWith(const std::string& source, const std::string& prefix)
 {
-    if (prefix.size() > source.size())
-    {
-        return false;
-    }
-    auto lastSource = source.begin() + prefix.size();
-    return std::equal(source.begin(), lastSource, prefix.begin());
+    return std::equal(prefix.begin(), prefix.end(), source.begin());
 }
+
+// 2nd solution
+// bool startsWith(const std::string& source, const std::string& prefix)
+// {
+//     return (prefix.size() > source.size()) ? std::equal(source.begin(), source.begin() + prefix.size(), prefix.begin()) : false;
+// }
+
+// 3rd solution
+// bool startsWith(const std::string& source, const std::string& prefix)
+// {
+//     auto mismatchPoint = std::mismatch(source.begin(), source.end(), prefix.begin());
+//     return prefix.end() == mismatchPoint.second;
+// }
