a0e8bb89d74041c8b5bc84690e3496b781115696
Tetiana Dobrovolska <tdobrovolska@luxoft.com>
2020-09-28 22:53:45 +0300

    algorithms task #1 TDobrovolska

--- algorithms task (1)/student/include/impl.h
+++ algorithms task (1)/student/include/impl.h
@@ -11,4 +11,17 @@
   * @return consecutive range
 */
 template<class FI, class Comparator = std::equal_to<typename std::iterator_traits<FI>::value_type>>
-std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {});
+std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {})
+{
+    auto first = std::adjacent_find(start, end, comp);
+    if (end == first)
+    {
+        return std::make_pair(end, end);
+    }
+    auto lambda = [&first](typename std::iterator_traits<FI>::value_type value)
+    {
+        return *first == value;
+    };
+    auto last = std::find_if_not(first, end, lambda);
+    return std::make_pair(first, last);
+}
