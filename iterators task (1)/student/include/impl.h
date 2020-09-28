b0782f50db80c895ff395a112d418c583ab44ffb
Tetiana Dobrovolska <tdobrovolska@luxoft.com>
2020-09-28 12:09:00 +0300

    iterator task #1 tdobrovolska

--- iterators task (1)/student/include/impl.h
+++ iterators task (1)/student/include/impl.h
@@ -5,11 +5,46 @@ template <class Container>
 struct accumulator
 {
     /** @todo Iterator traits*/
+    using traits = std::iterator_traits<typename Container::iterator>;
+    using difference_type = typename traits::difference_type;
+    using value_type = void;
+    using pointer = void;
+    using reference = void;
+    using iterator_category = std::output_iterator_tag;
+
     /** @todo Constructor with container*/
+    accumulator<Container>(Container& container)
+        : container_{container}
+    {
+    }
+    accumulator<Container>(const accumulator<Container>& c) = default;
+    accumulator<Container>(accumulator<Container>&& c) = default;
+
     /** @todo Copy assignment operator*/
+    template <class Input>
+    accumulator<Container>& operator =(const Input& inputData)
+    {
+        std::copy(inputData.begin(),
+                  inputData.end(),
+                  std::back_inserter(container_));
+        return *this;
+    }
+
     /** @todo Move assignment operator*/
+    template <class Input>
+    accumulator<Container>& operator =(Input&& inputData)
+    {
+        std::move(inputData.begin(),
+                  inputData.end(),
+                  std::back_inserter(container_));
+        return *this;
+    }
 
     /** @todo Iterator operations */
+    accumulator<Container>& operator*() { return *this; }
+    accumulator<Container>& operator++() { return *this; }
+private:
+    Container& container_;
 };
 
 /**
@@ -18,4 +53,5 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>(container);
 }
