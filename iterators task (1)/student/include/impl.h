83a6dc1151c377ebec300351b7a1f25592a71059
Ernest Nikonov <enikonov@luxoft.com>
2020-09-29 00:09:07 +0300

    Implement iterators task #1

--- iterators task (1)/student/include/impl.h
+++ iterators task (1)/student/include/impl.h
@@ -4,18 +4,39 @@
 template <class Container>
 struct accumulator
 {
-    /** @todo Iterator traits*/
-    /** @todo Constructor with container*/
-    /** @todo Copy assignment operator*/
-    /** @todo Move assignment operator*/
+    using iterator_category = std::output_iterator_tag;
+    using value_type        = void;
+    using difference_type   = void;
+    using pointer           = void;
+    using reference         = void;
 
-    /** @todo Iterator operations */
+    explicit accumulator(Container& container) : container_(container) {}
+
+    template <class OtherContainer>
+    accumulator& operator=(const OtherContainer& value)
+    {
+        container_.insert(container_.end(), value.begin(), value.end());
+        return *this;
+    }
+
+    template <class OtherContainer>
+    accumulator& operator=(OtherContainer&& value)
+    {
+        container_.insert(container_.end(),
+                          std::make_move_iterator(value.begin()),
+                          std::make_move_iterator(value.end()));
+        return *this;
+    }
+
+    accumulator& operator*()     { return *this; }
+    accumulator& operator++()    { return *this; }
+    accumulator& operator++(int) { return *this; }
+
+    Container& container_;
 };
 
-/**
- *  @todo Implement accumulator object creator
- */
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>(container);
 }
