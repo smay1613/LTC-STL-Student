d91c10b6e04bd8d8df6bdcb1af6f234391dbac98
Ernest Nikonov <enikonov@luxoft.com>
2020-09-29 20:36:18 +0300

    Implement algorithms task #2

--- algorithms task (2)/student/src/impl.cpp
+++ algorithms task (2)/student/src/impl.cpp
@@ -1,1 +1,9 @@
 #include "impl.h"
+
+#include <algorithm>
+
+bool startsWith(const std::string& source, const std::string& prefix)
+{
+    return prefix.length() <= source.length() &&
+           std::equal(prefix.begin(), prefix.end(), source.begin());
+}
