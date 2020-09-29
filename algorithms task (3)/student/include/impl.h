19eb3711b829e3e347f4d726f3e2a7c4fb69eecc
Ernest Nikonov <enikonov@luxoft.com>
2020-09-29 20:42:27 +0300

    Implement algorithms task #3

--- algorithms task (3)/student/include/impl.h
+++ algorithms task (3)/student/include/impl.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <algorithm>
+#include <iterator>
+
 /** @todo Implement right-shifting algorithm for bidirectional ranges
  *
  *  BI - bidirectional iterator type
@@ -13,4 +16,7 @@
  * @note behavior is undefined if shift out of bounds
  */
 template<class BiIt>
-BiIt shift_right_n(BiIt begin, BiIt end, typename BiIt::difference_type shift);
+BiIt shift_right_n(BiIt begin, BiIt end, typename BiIt::difference_type shift)
+{
+    return std::move_backward(begin, end, std::next(end, shift));
+}
