05831625a56bc1214787b3c0a53d8d15cff0985c
Andrey Sakun <sakun.andrey@gmail.com>
2020-09-29 11:58:33 +0300

    algorithms task 1

--- algorithms task (1)/student/include/impl.h
+++ algorithms task (1)/student/include/impl.h
@@ -11,4 +11,14 @@
   * @return consecutive range
 */
 template<class FI, class Comparator = std::equal_to<typename std::iterator_traits<FI>::value_type>>
-std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {});
+std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {})
+{
+  auto foundStart = std::adjacent_find(start, end, comp);
+  auto unaryComparator = [&foundStart](typename FI::value_type element)
+  {
+    return *foundStart == element;
+  };
+  auto foundEnd = std::find_if_not(foundStart, end, unaryComparator);
+  
+  return {foundStart, foundEnd};
+}
