2458cabe3576b9c68e7c0b49a44c193d733f9da6
Tetiana Dobrovolska <tdobrovolska@luxoft.com>
2020-09-30 12:11:31 +0300

    review changes

--- algorithms task (3)/student/include/impl.h
+++ algorithms task (3)/student/include/impl.h
@@ -15,6 +15,5 @@
 template<class BiIt>
 BiIt shift_right_n(BiIt begin, BiIt end, typename BiIt::difference_type shift)
 {
-    std::move_backward(begin, end, std::next(end, shift));
-    return std::next(begin, shift);
+    return std::move_backward(begin, end, std::next(end, shift));
 }
