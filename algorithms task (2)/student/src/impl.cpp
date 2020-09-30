3a83608df018fd1dc17b40aebf7c5723f0f6b41b
Zhan Poplavskyi <gan.popl@gmail.com>
2020-09-30 13:33:53 +0300

    Add check that prefix not longer than source

--- algorithms task (2)/student/src/impl.cpp
+++ algorithms task (2)/student/src/impl.cpp
@@ -2,5 +2,9 @@
 
 bool startsWith(const std::string& source, const std::string& prefix)
 {
-	return std::equal(prefix.begin(), prefix.end(), source.begin());
+    if (prefix.size() > source.size())
+    {
+        return false;
+    }
+    return std::equal(prefix.begin(), prefix.end(), source.begin());
 }
