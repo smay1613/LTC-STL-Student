bdcc4945b549e025ad7f8a11f2a319818cf28d7e
Andrey Sakun <sakun.andrey@gmail.com>
2020-10-01 22:01:46 +0300

    algorithms task 7

--- algorithms task (7)/student/include/impl.h
+++ algorithms task (7)/student/include/impl.h
@@ -7,4 +7,13 @@
  * @return N first fibonacci numbers
  */
 template<size_t N>
-std::array<size_t, N> fibonacci();
+std::array<size_t, N> fibonacci()
+{
+    std::array<size_t, N> array = {1};
+    auto op = [](const size_t& item1, const size_t& item2)
+    {
+        return item1 + item2;
+    };
+    std::adjacent_difference(std::begin(array), std::prev(std::end(array)), std::next(std::begin(array)), op);
+    return array;
+}
