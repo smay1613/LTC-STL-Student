eae8b6b1ba79557da87ad2f2503acf2de76b0e33
Ernest Nikonov <enikonov@luxoft.com>
2020-10-02 02:08:47 +0300

    Implement algorithms task #7

--- algorithms task (7)/student/include/impl.h
+++ algorithms task (7)/student/include/impl.h
@@ -1,10 +1,20 @@
 #pragma once
+
 #include <numeric>
 #include <array>
+#include <functional>
+#include <iterator>
 
 /**
  * @todo Implement function that will return N first fibonnaci numbers, started from 1
  * @return N first fibonacci numbers
  */
 template<size_t N>
-std::array<size_t, N> fibonacci();
+std::array<size_t, N> fibonacci()
+{
+    std::array<size_t, N> result = { 1 };
+    std::adjacent_difference(result.begin(), std::prev(result.end()),
+                             std::next(result.begin()),
+                             std::plus<size_t>{});
+    return result;
+}
