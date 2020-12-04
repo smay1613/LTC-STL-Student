9ce046b9d4012fc000ce8aff75090b3c3c884773
Denis Plotnikov <virtuallyworks@gmail.com>
2020-12-04 17:18:40 +0200

    Add googletest

---
+++ gmock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bill/Repo/stl_study/STL-Student/LTC-STL-Student/googletest-release-1.10.0
+# Build directory: /home/bill/Repo/stl_study/STL-Student/LTC-STL-Student/gmock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ gmock/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bill/Repo/stl_study/STL-Student/LTC-STL-Student/googletest-release-1.10.0/googletest
+# Build directory: /home/bill/Repo/stl_study/STL-Student/LTC-STL-Student/gmock/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
