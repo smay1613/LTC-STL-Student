3487ea0755192ead64ccd6f1b4e7fd89c030d763
Ubuntu 20.04 <asg@ubuntu20.localdomain>
2020-12-09 21:57:03 +0200

    Traits

--- sequence_task_1/student/include/impl.h
+++ sequence_task_1/student/include/impl.h
@@ -1,11 +1,11 @@
 #pragma once
+#include <stdexcept>
 #include <string>
 
-struct Song
-{
+struct Song {
     std::string name;
     explicit Song(std::string song_name)
-        : name {std::move(song_name)}
+        : name { std::move(song_name) }
     {
     }
 };
@@ -15,36 +15,100 @@ bool operator==(const Song& lhs, const Song& rhs)
     return lhs.name == rhs.name;
 }
 
-template<class Container>
-class StaticPlaylist
-{
+template <class Container>
+class StaticPlaylist {
 public:
+    using ContainerTraits = std::iterator_traits<typename Container::iterator>;
+    using value_type = typename ContainerTraits::value_type;
+    using difference_type = typename ContainerTraits::difference_type;
+    using reference = typename Container::reference;
+    using const_reference = typename Container::const_reference;
+    using iterator = typename Container::iterator;
+    using const_iterator = typename Container::const_iterator;
+    using reverse_iterator = typename Container::reverse_iterator;
+    using size_type = typename Container::size_type;
+
     /** @todo Member traits */
 
     /** @todo Iterators */
+    iterator begin()
+    {
+        return m_tracklist.begin();
+    }
+
+    const_iterator begin() const
+    {
+        return m_tracklist.begin();
+    }
+
+    iterator end()
+    {
+        return m_tracklist.end();
+    }
+
+    const_iterator end() const
+    {
+        return m_tracklist.end();
+    }
 
     /** @todo Constructor from any reversible sequence container */
+    template <typename OtherContainer>
+    StaticPlaylist(OtherContainer c)
+        : m_tracklist(c.begin(), c.end())
+    {
+    }
 
     /** @todo Assignment from any reversible sequence container */
+    template <typename OtherContainer>
+    StaticPlaylist& operator=(const OtherContainer& c)
+    {
+        Container tmp(c.begin(), c.end());
+        std::swap(m_tracklist, tmp);
+        return *this;
+    }
 
     /** @todo Add track from initializer */
-    template<class... Args>
-    const Song& play(Args&&... songData);
+    template <typename... Args>
+    const Song& play(Args&&... songData)
+    {
+        m_tracklist.insert(m_tracklist.begin(), std::forward<Song>(Song { std::string { songData } })...);
+        return *m_tracklist.begin();
+    }
 
     /** @todo Add track */
-    const Song& play(const Song& song);
+    const Song& play(Song const& song)
+    {
+        m_tracklist.insert(m_tracklist.begin(), song);
+        return *m_tracklist.begin();
+    }
 
     /** @todo Get first track in playlist stack */
-    const Song& current() const;
+    const Song& current() const
+    {
+        return *m_tracklist.begin();
+    }
 
     /** @todo Skip to the next track in playlist, remove current */
-    void switchNext();
+    void switchNext()
+    {
+        if (!m_tracklist.empty()) {
+            m_tracklist.erase(m_tracklist.begin());
+        } else {
+            throw std::out_of_range("");
+        }
+    }
 
     /** @todo Amount of tracks in playlist */
-    size_type count() const;
+    size_type count() const
+    {
+        return m_tracklist.size();
+    }
 
     /** @todo Checks if playlist has any playable tracks */
-    bool hasTracks() const;
+    bool hasTracks() const
+    {
+        return !m_tracklist.empty();
+    }
 
 private:
     Container m_tracklist;
