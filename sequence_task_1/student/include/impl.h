526b72db7a9276d521e5a187ba4dc5c74f1e256f
Dima Spektor <d.spektor.v@gmail.com>
2020-12-11 01:28:07 +0200

    Solve sequence_task_1

--- sequence_task_1/student/include/impl.h
+++ sequence_task_1/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <stdexcept>
 
 struct Song
 {
@@ -20,31 +21,84 @@ class StaticPlaylist
 {
 public:
     /** @todo Member traits */
+    using value_type      = typename Container::value_type;
+    using size_type       = typename Container::size_type;
+    using allocator_type  = typename Container::allocator_type;
+    using difference_type = typename Container::difference_type;
+    using iterator        = typename Container::iterator;
+    using const_iterator  = typename Container::const_iterator;
+    using reference       = typename Container::reference;
+    using const_reference = typename Container::const_reference;
 
     /** @todo Iterators */
+    const_iterator begin()
+    {
+        return m_tracklist.cbegin();
+    }
+
+    const_iterator end()
+    {
+        return m_tracklist.cend();
+    }
 
     /** @todo Constructor from any reversible sequence container */
+    template<class OtherContainer>
+    StaticPlaylist(const OtherContainer& otherConatiner)
+        : m_tracklist(otherConatiner.cbegin(), otherConatiner.cend())
+    {}
 
     /** @todo Assignment from any reversible sequence container */
+    template<class OtherContainer>
+    Container& operator=(const OtherContainer& otherConatiner)
+    {
+        m_tracklist.assign(otherConatiner.cbegin(), otherConatiner.cend());
+        return m_tracklist;
+    }
 
     /** @todo Add track from initializer */
     template<class... Args>
-    const Song& play(Args&&... songData);
+    const Song& play(Args&&... songData)
+    {
+        return *m_tracklist.insert(end(), {Song(std::forward<Args>(songData))...});
+    }
 
     /** @todo Add track */
-    const Song& play(const Song& song);
+    const Song& play(const Song& song)
+    {
+        return m_tracklist.push_back(song);
+    }
 
     /** @todo Get first track in playlist stack */
-    const Song& current() const;
+    const Song& current() const
+    {
+        if (!hasTracks())
+        {
+            throw std::out_of_range("Playlist is empty");
+        }
+        return m_tracklist.front();
+    }
 
     /** @todo Skip to the next track in playlist, remove current */
-    void switchNext();
+    void switchNext()
+    {
+        if (!hasTracks())
+        {
+            throw std::out_of_range("Playlist is empty");
+        }
+        m_tracklist.erase(begin());
+    }
 
     /** @todo Amount of tracks in playlist */
-    size_type count() const;
+    size_type count() const
+    {
+        return m_tracklist.size();
+    }
 
     /** @todo Checks if playlist has any playable tracks */
-    bool hasTracks() const;
+    bool hasTracks() const
+    {
+        return !m_tracklist.empty();
+    }
 
 private:
     Container m_tracklist;
