9969b4b734e27e91b2af9afeea6b6019ef01b431
Oleksandr Stepanenko <ovstepanenko@luxoft.com>
2020-12-11 15:38:10 +0200

    Sequence_3
    Oleksandr Stepanenko

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,27 @@
 #include "impl.h"
+#include <algorithm>
 
+
+std::string reverseWords(const std::string& input)
+{
+    std::istringstream ss(input);
+
+    std::deque<std::string> strDeque;
+
+    std::for_each(std::istream_iterator<std::string>(ss), std::istream_iterator<std::string>(),
+        [&strDeque](const std::string& str){
+            strDeque.push_front(str);
+        });
+
+    std::ostringstream oss;
+    for (auto word : strDeque) {
+        oss << " " << word;
+    }
+    std::string result = oss.str();
+
+    if(result.size() > 0) {
+        return {std::next(result.begin()), result.end()};
+    } else {
+        return result;
+    }
+}
