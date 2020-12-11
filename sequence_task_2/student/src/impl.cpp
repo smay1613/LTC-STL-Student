9a9de1c60ed31f0dfa804cddb58211689bbebd12
okolesnichenko <okolesnichenko@luxoft.com>
2020-12-11 14:04:35 +0100

    Sequence task 2 Kolesnichenko Oleksandr

--- sequence_task_2/student/src/impl.cpp
+++ sequence_task_2/student/src/impl.cpp
@@ -1,1 +1,9 @@
 #include "impl.h"
+
+template<class T, class BI>
+void rotate(std::list<T>& list, BI begin, BI new_begin, BI end) {
+    list.splice(begin, list, new_begin, end);
+}
+
+template void rotate(std::list<size_t>&, std::list<size_t>::iterator,
+    std::list<size_t>::iterator, std::list<size_t>::iterator);
