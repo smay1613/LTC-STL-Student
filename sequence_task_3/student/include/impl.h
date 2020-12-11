576a092336ec57ccf2cdfa36436b23a1536f3104
Ubuntu 20.04 <asg@ubuntu20.localdomain>
2020-12-11 15:05:45 +0200

    Implementation

--- sequence_task_3/student/include/impl.h
+++ sequence_task_3/student/include/impl.h
@@ -1,7 +1,8 @@
 #pragma once
-#include <sstream>
+#include <algorithm>
 #include <deque>
 #include <iterator>
+#include <sstream>
 
 /**
  * @todo Implement function that will reverse words in a sentence
@@ -11,4 +12,18 @@
  *
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
-std::string reverseWords(const std::string& input);
+std::string reverseWords(const std::string& input)
+{
+    std::istringstream iss(input);
+    std::deque<std::string> words;
+    for_each(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(),
+        [&words](const std::string& word) { words.push_front(word); });
+
+    std::ostringstream oss;
+    std::copy(words.begin(), words.end(), std::ostream_iterator<std::string>(oss, " "));
+    std::string result { oss.str() };
+    if (!result.empty()) {
+        result.resize(result.size() - 1);
+    }
+    return result;
+}
