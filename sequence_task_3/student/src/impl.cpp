def602a4b5a29119017a184cfd58dfea737a836a
Dima Spektor <d.spektor.v@gmail.com>
2020-12-12 15:20:37 +0200

    Solve sequence_task_3

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,16 @@
 #include "impl.h"
 
+
+std::string reverseWords(const std::string &input)
+{
+    std::istringstream iss{input};
+    std::deque<std::string> container{std::istream_iterator<std::string> {iss},
+                                      std::istream_iterator<std::string> {}};
+
+    std::ostringstream oss{};
+    std::copy(container.rbegin(), std::prev(container.rend()), std::ostream_iterator<std::string>{oss, " "});
+    oss << container.front();
+    //not sure if this is the best way to avoid trailing space, but at least it's short and clear.
+
+    return oss.str();
+}
