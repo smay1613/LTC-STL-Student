3e0b679f3a86fd3ce43c6c400b2fe8ee3574f1a0
AP <>
2020-12-13 23:22:11 +0200

    done reverse str

--- sequence_task_3/student/include/impl.h
+++ sequence_task_3/student/include/impl.h
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <deque>
 #include <iterator>
+#include <algorithm>
 
 /**
  * @todo Implement function that will reverse words in a sentence
@@ -11,4 +12,16 @@
  *
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
-std::string reverseWords(const std::string& input);
+std::string reverseWords(const std::string& input) {
+    std::stringstream output;
+    std::string res;
+    std::istringstream ss(input);
+    std::deque<std::string> cont;
+    copy(std::istream_iterator<std::string>(ss), std::istream_iterator<std::string>(), front_inserter(cont));
+    std::copy(cont.begin(), cont.end(), std::ostream_iterator<std::string>(output, " "));
+    res = output.str();
+    if (!res.empty()) {
+        res.erase(res.end()-1);
+    }
+    return res;
+}
