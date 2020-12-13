2c58982594973b93200d16c01a58d59f62d496e9
okolesnichenko <okolesnichenko@luxoft.com>
2020-12-13 15:00:19 +0100

    Sequence task 3 Kolesnichenko Oleksandr

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,15 @@
 #include "impl.h"
 
+#include <algorithm>
+
+std::string reverseWords(const std::string& input) {
+    std::istringstream istr(input);
+    std::deque<std::string> deq{std::istream_iterator<std::string>(istr),
+                                std::istream_iterator<std::string>()};
+    std::ostringstream ostr;
+    std::copy(deq.rbegin(), deq.rend(), std::ostream_iterator<std::string>(ostr, " "));
+    std::string ret(ostr.str());
+    if (!ret.empty())
+        ret.pop_back();
+    return ret;
+}
