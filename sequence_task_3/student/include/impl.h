b53305654fcfed101cb072251734e7c0f45c598b
Yevhenii Zaiets <YZaiets@luxoft.com>
2020-12-13 12:03:33 +0200

    Sequence_Task_1_Zaiets_Completed

--- sequence_task_3/student/include/impl.h
+++ sequence_task_3/student/include/impl.h
@@ -11,4 +11,16 @@
  *
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
-std::string reverseWords(const std::string& input);
+std::string reverseWords(const std::string& input)
+{
+    std::istringstream iss(input);
+    std::ostringstream oss;
+    std::deque<std::string> strings((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
+
+    for (auto it = strings.rbegin(); it != strings.rend(); ++it)
+    {
+      oss << *it;
+      if(std::next(it) != strings.rend()) oss << " ";
+    }
+    return oss.str();
+}
