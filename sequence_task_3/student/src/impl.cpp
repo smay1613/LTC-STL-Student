c0e1c3b51550f16de45ee982f097d514158abcc4
Tetiana Dobrovolska <TDobrovolska@luxoft.com>
2020-12-13 23:23:23 +0200

    sequence task #3 TDobrovolska

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,20 @@
 #include "impl.h"
 
+std::string reverseWords(const std::string& input)
+{
+    std::istringstream input_stream(input);
+    std::deque<std::string> words;
+    std::move(std::istream_iterator<std::string>(input_stream),
+              std::istream_iterator<std::string>(),
+              std::front_inserter(words));
+
+    std::ostringstream output_stream;
+    std::move(words.begin(), words.end(),
+              std::ostream_iterator<std::string>(output_stream, " "));
+    std::string str(output_stream.str());
+    if(!str.empty())
+    {
+        str.pop_back();
+    }
+    return str;
+}
