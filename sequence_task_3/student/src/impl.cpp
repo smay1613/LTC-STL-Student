b3a7de14b361bba21752fb38ea5094200cb01b99
Marianna Vedernikova <mvedernikova@luxoft.com>
2020-12-13 17:59:57 +0200

    sequence_task3

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,15 @@
 #include "impl.h"
 
+std::string reverseWords(const std::string& input) {
+    std::deque<std::string> strDeq;
+    std::string world;
+    std::string result;
+    std::stringstream myString(input);
+
+    while (myString >> world) {
+        strDeq.push_front(world);
+        strDeq.push_front(" ");
+    }
+
+    return result;
+}
