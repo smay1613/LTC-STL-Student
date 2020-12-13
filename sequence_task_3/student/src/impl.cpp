b36ab5caf05bdb11b928f5d6d9f3b7b56302a6a2
Anna Molodchenkova <amolodchenkova@luxoft.com>
2020-12-14 01:42:01 +0200

    reverse_list

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,26 @@
 #include "impl.h"
+#include <algorithm>
+
+std::string reverseWords(const std::string& input)
+{
+    std::stringstream istream(input);
+    std::deque<std::string> deque;
+
+    std::for_each(std::istream_iterator<std::string>(istream), std::istream_iterator<std::string>(), [&deque]( const std::string& s )
+    {
+        deque.push_front(s);
+    });
+
+    std::stringstream outstream;
+    std::for_each(deque.begin(), deque.end(), [&outstream, &deque]( const std::string& word )
+    {
+        outstream << word;
+        if (word != deque.back())
+        {
+            outstream << ' ';
+        }
+    });
+
+    return outstream.str();
+}
 
