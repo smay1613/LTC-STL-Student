bb348fada5df305d4d56a01d6799adc5e6aca999
Dima Spektor <d.spektor.v@gmail.com>
2020-12-14 20:35:55 +0200

    adaptors_task dspektor

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,36 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
+#include <algorithm>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string &source)
+{
+    std::stack<char> stack{};
+
+    for (const auto c : source)
+    {
+        const auto typeIt = std::find_if(brackets.cbegin(), brackets.cend(),
+                                         [c](const std::pair<char, char>& type)
+                                         { return c == type.first || c == type.second; });
+        if (brackets.cend() == typeIt)
+        {
+            continue;
+        }
+        else if (!stack.empty() && stack.top() == typeIt->first && c == typeIt->second)
+        {
+            stack.pop();
+        }
+        else
+        {
+            stack.push(c);
+        }
+    }
+
+    return stack.empty();
+}
