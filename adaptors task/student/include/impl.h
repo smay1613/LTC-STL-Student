e8a8ca254d609296e6d0ad71064119778137bf9f
Iurii Gulov <IGulov@luxoft.com>
2020-12-14 22:59:48 +0200

    adaptors task is done

--- adaptors task/student/include/impl.h
+++ adaptors task/student/include/impl.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <stack>
+#include <iostream>
 
 /**
  * @todo isValid - check if given expression is valid - all open brackets are closed
@@ -11,4 +13,48 @@
  *
  * @return true if expression is valid
  */
-bool isValid(const std::string& source);
+bool isValid(const std::string& source)
+{
+   std::stack<char> charsStack;
+   bool result = true;
+
+   auto checkCloseBrackets{[&charsStack](const char ch){
+         if (!charsStack.empty() && ch == charsStack.top())
+         {
+            charsStack.pop();
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+      }};
+
+   for (const char& ch : source)
+   {
+      switch (ch) {
+      case '(':
+      case '[':
+      case '{':
+         charsStack.push(ch);
+         break;
+      case ')':
+         result = checkCloseBrackets('(');
+         break;
+      case ']':
+         result = checkCloseBrackets('[');
+         break;
+      case '}':
+         result = checkCloseBrackets('{');
+         break;
+      default:
+         continue;
+         break;
+      }
+
+      if (!result){
+         break;
+      }
+   }
+   return result && charsStack.empty();
+}
