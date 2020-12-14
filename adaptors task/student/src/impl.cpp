3a07ec38321f52661a16c4f6aa3af55e2a72fc43
Oleksandr Stepanenko <ovstepanenko@luxoft.com>
2020-12-14 12:17:21 +0200

    Adaptors task Oleksandr Stepanenko

--- adaptors task/student/src/impl.cpp
+++ adaptors task/student/src/impl.cpp
@@ -1,8 +1,32 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    std::stack<char> stackChecker;
+
+    for (auto nextSymbol: source) {
+        for (auto nextPair: brackets) {
+            if (nextSymbol == nextPair.first) {
+                stackChecker.push(nextSymbol);
+            }
+            if (nextSymbol == nextPair.second) {
+                if (!stackChecker.empty() && stackChecker.top() == nextPair.first){
+                    stackChecker.pop();
+                } else {
+                    stackChecker.push(nextSymbol);
+                }
+            }
+        }
+    }
+
+    if (stackChecker.empty()){ return true; }
+    else { return false; }
+}
