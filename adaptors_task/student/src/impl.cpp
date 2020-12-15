bdf37603cf578f66905d2eb7c3dafdff5ffb3465
Ubuntu 20.04 <asg@ubuntu20.localdomain>
2020-12-15 19:23:47 +0200

    Implementation

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,4 +1,6 @@
 #include "impl.h"
+#include <algorithm>
+#include <stack>
 #include <vector>
 
 const std::vector<std::pair<char, char>> brackets {
@@ -6,3 +8,36 @@ const std::vector<std::pair<char, char>> brackets {
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    std::stack<char> stack;
+    for (const auto& bracket : source) {
+        auto it = std::find_if(brackets.begin(), brackets.end(), [&](std::pair<char, char> const p) {
+            if (p.first == bracket || p.second == bracket) {
+                return true;
+            }
+            return false;
+        });
+        if (it == brackets.end()) {
+            continue;
+        }
+
+        if (bracket == it->first) {
+            stack.push(bracket);
+        } else {
+            if (stack.empty()) {
+                return false;
+            }
+            if (stack.top() != it->first) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    if (stack.empty()) {
+        return true;
+    } else {
+        return false;
+    }
+}
