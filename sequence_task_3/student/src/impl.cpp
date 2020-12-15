b89be23ce356491333db146393787dc1ea8d16b1
VladSemenyuk <vsemenyuk@luxoft.com>
2020-12-15 22:35:21 +0200

    Sequence task 3 Semenyuk

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,18 @@
 #include "impl.h"
 
+std::string reverseWords(const std::string& input) {
+  std::stringstream ss(input);
+  const std::deque<std::string> words{std::istream_iterator<std::string>(ss),
+                                      std::istream_iterator<std::string>()};
+  std::stringstream output_ss;
+  std::copy(words.rbegin(),
+            words.rend(),
+            std::ostream_iterator<std::string>(output_ss, " "));
+
+  std::string str = output_ss.str();
+
+  if (!str.empty())
+    str.pop_back();
+
+  return str;
+}
