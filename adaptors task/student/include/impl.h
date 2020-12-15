2b86170ef676e33854f4923175f2fa3e64a180ce
Kryvonos <MKryvonos@kiev.luxoft.com>
2020-12-16 01:43:40 +0200

    [adaptors_task][MKryvonos] Rework after code review

--- adaptors task/student/include/impl.h
+++ adaptors task/student/include/impl.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <string>
-#include <stack>
-#include <vector>
-#include <utility>
+
 /**
  * @todo isValid - check if given expression is valid - all open brackets are closed
  * by CORRESPONDING closed bracket.
@@ -13,64 +11,5 @@
  *
  * @return true if expression is valid
  */
-bool isOpenBracket(char ch, std::vector< std::pair <char,char> >& vector){
-    bool isOpen = false;
-    for(int j=0; j< vector.size();++j){
-        if(ch == vector[j].first)
-            isOpen = true;
-    }
-    return isOpen;
-}
 
-bool isClosedBracket(char ch, std::vector< std::pair <char,char> >& vector){
-    bool isOpen = false;
-    for(int j=0; j< vector.size();++j){
-        if(ch == vector[j].second)
-            isOpen = true;
-    }
-    return isOpen;
-}
-
-char correspondingClosedBracket(char ch, std::vector< std::pair <char,char> >& vector){
-    for(int j=0; j< vector.size();++j){
-        if(ch == vector[j].first)
-            return vector[j].second;
-    }
-}
-bool isValid(const std::string& source){
-    std::stack<char> bracketsStack;
-    std::vector< std::pair <char,char> > bracketsVector;
-
-    bracketsVector.push_back(std::make_pair('(',')'));
-    bracketsVector.push_back(std::make_pair('{','}'));
-    bracketsVector.push_back(std::make_pair('[',']'));
-
-    bool isValid = true;
-    int lenght = source.size();
-    for(int i=0; i<lenght; i++){
-        char currentChar = source[i];
-        if(isOpenBracket(currentChar, bracketsVector))
-            bracketsStack.push(currentChar);
-        else {
-            if(!isClosedBracket(currentChar, bracketsVector))
-                continue;
-            else {
-                if(bracketsStack.empty()) {
-                    isValid = false;
-                    break;
-                }
-                char hab = bracketsStack.top();
-                char closedBracket = correspondingClosedBracket(hab,bracketsVector);
-                if(closedBracket == currentChar)
-                    bracketsStack.pop();
-                else {
-                    isValid = false;
-                    break;
-                }
-            }
-        }
-    }
-    if(!bracketsStack.empty())
-        isValid= false;
-    return isValid;
-}
+bool isValid(const std::string& source);
--- adaptors task/student/src/impl.cpp
+++ adaptors task/student/src/impl.cpp
@@ -1,8 +1,58 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
+#include <utility>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isOpenBracket(char ch) {
+    for (auto v : brackets) {
+        if(ch == v.first)
+            return true;
+    }
+    return false;
+}
+
+bool isClosedBracket(char ch) {
+    for (auto v : brackets) {
+        if(ch == v.second)
+            return true;
+    }
+    return false;
+}
+
+char correspondingClosedBracket(char ch) {
+    for (auto v : brackets) {
+        if(ch == v.first)
+            return v.second;
+    }
+}
+
+bool isValid(const std::string& source){
+    std::stack<char> bracketsStack;
+
+    for(char currentChar : source){
+        if(isOpenBracket(currentChar))
+            bracketsStack.push(currentChar);
+        else {
+            if(!isClosedBracket(currentChar))
+                continue;
+            else {
+                if(bracketsStack.empty())
+                    return false;
+
+                char currentOpenBracket = bracketsStack.top();
+                char closedBracket = correspondingClosedBracket(currentOpenBracket);
+                if(closedBracket == currentChar)
+                    bracketsStack.pop();
+                else
+                    return false;
+            }
+        }
+    }
+    return bracketsStack.empty();
+}
