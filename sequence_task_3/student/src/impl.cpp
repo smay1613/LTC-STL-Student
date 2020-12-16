72375250b95ebdbc755d0a71ab72d29fd4bfec7a
vlod <7e1ght.jm@gmail.com>
2020-12-16 04:03:12 +0200

    sequence_task_3 VRazvodovskyi

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,18 @@
 #include "impl.h"
+#include <sstream>
+#include <iterator>
+#include <numeric>
 
+std::string reverseWords(const std::string& input)
+{
+    std::istringstream iss(input);
+
+    auto res = std::accumulate(std::istream_iterator<std::string>(iss), 
+                               std::istream_iterator<std::string>(), 
+                               std::string(),
+                               [](std::string a, std::string b) { return b + " " + a; });
+    
+    if(true != res.empty()) res.pop_back();
+
+    return res;
+}
