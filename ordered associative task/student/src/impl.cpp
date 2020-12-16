dcbe32cc9c5dbc460578f1b76ad1bb7d63df4954
kshestakova <teststudy14032018@gmail.com>
2020-12-16 11:26:24 +0200

    Map task

--- ordered associative task/student/src/impl.cpp
+++ ordered associative task/student/src/impl.cpp
@@ -1,1 +1,13 @@
 #include "impl.h"
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point) {
+    std::map<size_t, int> markMap;
+    markMap.emplace(29, 1);
+    markMap.emplace(49, 2);
+    markMap.emplace(69, 3);
+    markMap.emplace(89, 4);
+
+    auto result = markMap.lower_bound(markIn100Point);
+
+    return result != markMap.end() ? result->second : (--markMap.end())->second + 1;
+}
