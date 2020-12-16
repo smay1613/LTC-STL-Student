958816b1aa21bcf54a8b2a2dd59fe683e25a3425
lexa <myrmik@gmail.com>
2020-12-16 11:55:51 +0200

    adaptors_task_AMarmusevich

--- adaptors task/student/src/impl.cpp
+++ adaptors task/student/src/impl.cpp
@@ -1,8 +1,40 @@
 #include "impl.h"
 #include <vector>
 
+#include <algorithm>
+#include <stack>
+
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    bool ret = true;
+    std::stack<char> tempBrackets;
+    for (const char c : source)
+    {
+        auto currentBracket = std::find_if(brackets.cbegin(), brackets.cend(),
+            [c](const std::pair<char, char>& type) { return c == type.first || c == type.second; } );
+
+        if (currentBracket != brackets.cend())
+        {
+            if (c == currentBracket->first)
+            {
+                tempBrackets.push(c);
+            }
+            else if (!tempBrackets.empty() && tempBrackets.top() == currentBracket->first)
+            {
+                tempBrackets.pop();
+            }
+            else
+            {
+                ret = false;
+                break;
+            }
+        }
+    }
+    return ret && tempBrackets.empty();
+}
