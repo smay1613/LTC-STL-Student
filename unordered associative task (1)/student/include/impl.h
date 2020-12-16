894e24424671d135422967d3234433fbdf83a2c6
Kryvonos <MKryvonos@kiev.luxoft.com>
2020-12-16 22:25:41 +0200

    [unordered_associative_task_1][MKryvonos] implementation

--- unordered associative task (1)/student/include/impl.h
+++ unordered associative task (1)/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <map>
 
 /**
  * @todo Implement function that will check if one string has the same characters
@@ -12,4 +13,26 @@
  * @param second - second word
  * @return true if first is anagram of second
  */
-bool is_anagram(const std::string& first, const std::string& second);
+
+bool is_anagram(const std::string& first, const std::string& second){
+    std::map<char,int> charMap;
+    for (char currentChar : first) {
+        auto iterator = charMap.lower_bound(currentChar);
+        if (iterator == charMap.end())
+            charMap[currentChar] = 1;
+        else
+            charMap[currentChar] = charMap[currentChar] + 1;
+    }
+    for (char currentChar : second) {
+        auto iterator = charMap.lower_bound(currentChar);
+        if (iterator == charMap.end())
+            return false;
+        else
+            charMap[currentChar] = charMap[currentChar] - 1;
+    }
+    for (auto i : charMap) {
+        if(i.second != 0)
+            return false;
+    }
+    return true;
+}
