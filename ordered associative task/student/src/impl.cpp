28d16dca5b3cb58a648fe315ad29bd29ae22f469
Oleksandr Stepanenko <ovstepanenko@luxoft.com>
2020-12-16 12:37:32 +0200

    ordered_associative_task_Stepanenko_Oleksandr

--- ordered associative task/student/src/impl.cpp
+++ ordered associative task/student/src/impl.cpp
@@ -1,1 +1,14 @@
 #include "impl.h"
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    static std::map<size_t, size_t> marks {{30, 1}, {50, 2}, {70, 3}, {90, 4}};
+
+    for (auto markPair: marks) {
+        if (markIn100Point < markPair.first) {
+            return markPair.second;
+        }
+    }
+
+    return 5; // + out of bound cases
+}
