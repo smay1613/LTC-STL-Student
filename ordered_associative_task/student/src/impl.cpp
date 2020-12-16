c0ff5bf5376cc35440eaa10d726abfc0c5207435
Tetiana Dobrovolska <TDobrovolska@luxoft.com>
2020-12-16 11:57:11 +0200

    ordered associative task TDobrovolska

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -1,1 +1,8 @@
 #include "impl.h"
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    std::map <size_t, size_t> converter {{29, 1}, {49, 2}, {69, 3}, {89, 4}, {90, 5}};
+    auto result = converter.lower_bound(markIn100Point);
+    return converter.end() != result ? result->second : converter.at(90);
+}
