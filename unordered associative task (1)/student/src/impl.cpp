7afbb308f45d1fb169bc34ee2dd205b0fd003289
lexa <myrmik@gmail.com>
2020-12-16 12:31:52 +0200

    unordered_associative_task_1_AMarmusevich

--- unordered associative task (1)/student/src/impl.cpp
+++ unordered associative task (1)/student/src/impl.cpp
@@ -1,1 +1,21 @@
 #include "impl.h"
+#include <unordered_set>
+
+
+/**
+ * @todo Implement function that will check if one string has the same characters
+ * as the second one
+ *
+ * @example is_anagram("abcd", "acdb") -> true
+ * @example is_anagram("abcd", "abec") -> false
+ *
+ * @param first - first word
+ * @param second - second word
+ * @return true if first is anagram of second
+ */
+bool is_anagram(const std::string& first, const std::string& second)
+{
+    std::unordered_multiset<char> firstWord(first.begin(), first.end());
+    std::unordered_multiset<char> secondWord(second.begin(), second.end());
+    return firstWord == secondWord;
+}
