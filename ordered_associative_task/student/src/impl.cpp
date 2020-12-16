0b861db65130e8ee2298be7d2b2e01ec6f576afd
Ubuntu 20.04 <asg@ubuntu20.localdomain>
2020-12-16 11:47:51 +0200

    Simplification

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -2,7 +2,7 @@
 
 size_t convertMarkTo5PointSystem(size_t markIn100Point)
 {
-    const std::map<size_t, size_t> table { { 29, 1 }, { 49, 2 }, { 69, 3 }, { 89, 4 }, { 99, 5 } };
+    const std::map<size_t, size_t> table { { 29, 1 }, { 49, 2 }, { 69, 3 }, { 89, 4 } };
     auto const it = table.lower_bound(markIn100Point);
     return it != table.cend() ? it->second : 5;
 }
