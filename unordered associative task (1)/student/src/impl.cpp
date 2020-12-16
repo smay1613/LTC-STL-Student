8c344f271e12cb13099f5e2f59d130407c8a488b
Oleksandr Stepanenko <ovstepanenko@luxoft.com>
2020-12-16 12:17:33 +0200

    unordered_assotiative_task_1_Stepanenko_Oleksandr

--- unordered associative task (1)/student/src/impl.cpp
+++ unordered associative task (1)/student/src/impl.cpp
@@ -1,1 +1,22 @@
 #include "impl.h"
+#include "unordered_map"
+
+bool is_anagram(const std::string& first, const std::string& second)
+{
+    std::unordered_map<char, size_t> firstMap;
+    std::unordered_map<char, size_t> secondMap;
+
+    for (auto symbol: first) {
+        firstMap[symbol]++;
+    }
+
+    for (auto symbol: second) {
+        secondMap[symbol]++;
+    }
+
+    if (firstMap == secondMap) {
+        return true;
+    } else {
+        return false;
+    }
+}
