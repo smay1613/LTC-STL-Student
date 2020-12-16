f41dcfd63f5a45bbc3fd4eb4b3736567249a42bd
Iurii Gulov <IGulov@luxoft.com>
2020-12-16 22:31:33 +0200

    Task is done

--- ordered associative task/student/src/impl.cpp
+++ ordered associative task/student/src/impl.cpp
@@ -1,1 +1,8 @@
 #include "impl.h"
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+   std::map<size_t, size_t> ranges{{29, 1}, {49, 2}, {69, 3}, {89, 4}};
+   const auto it = ranges.lower_bound(markIn100Point);
+   return it == ranges.end() ? 5 : it->second;
+}
