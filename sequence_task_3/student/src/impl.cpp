f37b81e1100db0d14ccbb2905de4d5378818e934
lexa <myrmik@gmail.com>
2020-12-16 09:22:23 +0200

    sequence_task_3_AMarmusevich

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,21 @@
 #include "impl.h"
 
+std::string reverseWords(const std::string& input)
+{
+    std::string ret;
+    if(!input.empty())
+    {
+        std::stringstream in(input);
+        const std::deque<std::string> ds { std::istream_iterator<std::string>(in), std::istream_iterator<std::string>() };
+        std::stringstream out;
+        std::copy(ds.rbegin(), ds.rend(), std::ostream_iterator<std::string>(out, " "));
+        ret = out.str();
+
+        if (!ret.empty())
+        {
+            ret.pop_back();
+        }
+    }
+    return ret;
+}
+
