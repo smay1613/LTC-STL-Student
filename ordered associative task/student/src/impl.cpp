ca1e848c08bb0abf31a85b99f916e0cc6b25a223
lexa <myrmik@gmail.com>
2020-12-16 10:42:15 +0200

    ordered_associative_task_AMarmusevich

--- ordered associative task/student/src/impl.cpp
+++ ordered associative task/student/src/impl.cpp
@@ -1,1 +1,13 @@
 #include "impl.h"
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    const std::map<size_t, size_t> scale {{29, 1}, {49, 2}, {69, 3}, {89, 4}};
+    size_t ret = 5;
+    auto it = scale.lower_bound(markIn100Point);
+    if(it != scale.end())
+    {
+        ret = it->second;
+    }
+    return ret;
+}
