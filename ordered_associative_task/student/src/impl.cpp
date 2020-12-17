33d96e40c8690d3ff84bdc5e005a214c256223c8
Dima Spektor <d.spektor.v@gmail.com>
2020-12-17 22:46:24 +0200

    ordered_associative_task dspektor

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -1,1 +1,11 @@
 #include "impl.h"
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    const std::map<size_t, size_t> marksLUT{{29, 1}, {49, 2}, {69, 3}, {89, 4}, {100, 5}};
+
+    const auto it = marksLUT.lower_bound(markIn100Point);
+
+    return it != marksLUT.cend() ? it->second
+                                 : std::prev(it)->second;
+}
