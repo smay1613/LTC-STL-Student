08281979ef1e9ad626e811abcd0aa1455efbfb45
Yevhenii Zaiets <YZaiets@luxoft.com>
2020-12-17 17:49:08 +0200

    Adaptor_Tast_Zaiets_Completed

--- adaptors task/student/include/impl.h
+++ adaptors task/student/include/impl.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <string>
+#include <stack>
+#include <vector>
+#include <algorithm>
 
 /**
  * @todo isValid - check if given expression is valid - all open brackets are closed
@@ -11,4 +14,35 @@
  *
  * @return true if expression is valid
  */
-bool isValid(const std::string& source);
+
+std::vector<std::pair<char, char>> braces_collection =
+{
+    {'{', '}'},
+    {'(', ')'},
+    {'[', ']'}
+};
+
+bool isValid(const std::string& source)
+{
+    std::stack<char> char_sequence;
+    for(auto& ch : source)
+    {
+        auto it = std::find_if(braces_collection.begin(), braces_collection.end(), [&ch](std::pair<char, char> braces)
+        {
+            return (braces.first == ch || braces.second == ch);
+        });
+
+        if(it != braces_collection.end())
+        {
+            if(it->first == ch) char_sequence.push(ch);
+            else if(it->second == ch)
+            {
+                if(!char_sequence.empty() && char_sequence.top() == it->first) char_sequence.pop();
+                else return false;
+            }
+        }
+    }
+
+    return char_sequence.empty();
+}
+
