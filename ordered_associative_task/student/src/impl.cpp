079d96bacb681686fd06a3235c5b6323c410bcc3
Tetiana Dobrovolska <TDobrovolska@luxoft.com>
2020-12-17 16:15:11 +0200

    Minor fix

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -10,7 +10,11 @@ size_t convertMarkTo5PointSystem(size_t markIn100Point)
         FOUR,
         MAX_POINT
     };
-    const std::map <size_t, size_t> converter {{29, Point::ONE}, {49, Point::TWO}, {69, Point::THREE}, {89, Point::FOUR}, {90, Point::MAX_POINT}};
+    const std::map <size_t, size_t> converter {{29, static_cast<size_t>(Point::ONE)},
+                                               {49, static_cast<size_t>(Point::TWO)},
+                                               {69, static_cast<size_t>(Point::THREE)},
+                                               {89, static_cast<size_t>(Point::FOUR)},
+                                               {90, static_cast<size_t>(Point::MAX_POINT)}};
     const auto result = converter.lower_bound(markIn100Point);
-    return converter.end() != result ? result->second : Point::MAX_POINT;
+    return converter.end() != result ? result->second : static_cast<size_t>(Point::MAX_POINT);
 }
