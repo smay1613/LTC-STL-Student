ea95c33c632128645d8aabe259af386c0d4a1d87
Dima Spektor <d.spektor.v@gmail.com>
2020-12-17 23:25:49 +0200

    unordered_associative_task_1 dspektor

--- unordered_associative_task_1/student/src/impl.cpp
+++ unordered_associative_task_1/student/src/impl.cpp
@@ -1,1 +1,10 @@
 #include "impl.h"
+#include <unordered_set>
+
+bool is_anagram(const std::string &first, const std::string &second)
+{
+    const std::unordered_multiset<char> firstCharset{first.cbegin(), first.cend()};
+    const std::unordered_multiset<char> secondCharset{second.cbegin(), second.cend()};
+
+    return firstCharset == secondCharset;
+}
