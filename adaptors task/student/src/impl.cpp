0e29e246ecabbc59b08a99fe4657e76397c5e40d
VladSemenyuk <vsemenyuk@luxoft.com>
2020-12-17 21:39:42 +0200

    Adaptors task Semenyuk

--- adaptors task/student/src/impl.cpp
+++ adaptors task/student/src/impl.cpp
@@ -1,8 +1,37 @@
 #include "impl.h"
+
+#include <algorithm>
+#include <stack>
 #include <vector>
 
-const std::vector<std::pair<char, char>> brackets {
-    {'(', ')'},
-    {'[', ']'},
-    {'{', '}'}
-};
+const std::vector<std::pair<char, char> > brackets{
+    {'(', ')'}, {'[', ']'}, {'{', '}'}};
+
+bool isValid(const std::string& source) {
+  std::stack<char> result;
+  for (const auto& symbol : source) {
+    auto it =
+        std::find_if(brackets.begin(),
+                     brackets.end(),
+                     [&symbol](const std::pair<char, char>& brackets_pair) {
+                       return brackets_pair.first == symbol ||
+                              brackets_pair.second == symbol;
+                     });
+
+    if (it != brackets.end()) {
+      if (symbol == it->first) {
+        result.push(symbol);
+      } else if (result.empty()) {
+        return false;
+      } else {
+        if (result.top() == it->first) {
+          result.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  return result.empty();
+}
