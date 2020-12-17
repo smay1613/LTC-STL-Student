58f0facfe513bb8c4ab6863a12f003fe94dee7f7
Verzhbytskyi <DVerzhbytskyi@kiev.luxoft.com>
2020-12-17 19:53:11 +0200

    resolving adaptors task. Empty string test has not passed

--- adaptors task/student/src/impl.cpp
+++ adaptors task/student/src/impl.cpp
@@ -1,8 +1,35 @@
 #include "impl.h"
 #include <vector>
-
+#include <stack>
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+bool isValid(const std::string& source){
+    if (source.empty() or source.size()==1)
+    {
+        return false;
+    }
+    std::stack<char> res;
+
+    for (auto stringCh : source) {
+        for (auto reg : brackets) {
+            if (stringCh == reg.first)
+                {
+                    res.push(stringCh);
+                    continue;
+                }
+                else if (stringCh == reg.second)
+                {
+                    if (res.empty() or source.at(0) == reg.second or res.top() != reg.first)
+                    {
+                        return false;
+                    }
+                    res.pop();
+                    continue;
+                }
+            }
+        }
+        return res.empty();
+}
