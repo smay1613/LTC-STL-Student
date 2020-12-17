7cb5c7aa9aba4e2f7072a72cbadcc14d218bbfd6
visper <zzzvisperzzz@yandex.ru>
2020-12-17 15:28:38 +0200

    sequence_task_3

--- sequence_task_3/student/include/impl.h
+++ sequence_task_3/student/include/impl.h
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <deque>
 #include <iterator>
+#include <stack>
 
 /**
  * @todo Implement function that will reverse words in a sentence
@@ -11,4 +12,27 @@
  *
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
-std::string reverseWords(const std::string& input);
+std::string reverseWords(const std::string& input)
+{
+    std::stack<std::string> inputWords;
+    std::string word;
+    std::stringstream s(input);
+
+    while (getline(s, word, ' ')) {
+        if( word.size() != 0) {
+            inputWords.push(word);
+        }
+    }
+
+    std::string out("");
+    while(!inputWords.empty()) {
+        out += inputWords.top();
+        inputWords.pop();
+
+        if(!inputWords.empty()) {
+            out += " ";
+        }
+    }
+
+    return out;
+}
