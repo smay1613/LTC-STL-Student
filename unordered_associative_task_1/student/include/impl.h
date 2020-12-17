79ac512186f4b43e02af39d7f881c8f2055da19c
visper <zzzvisperzzz@yandex.ru>
2020-12-17 15:50:09 +0200

    unordered_associative_task_1_1

--- unordered_associative_task_1/student/include/impl.h
+++ unordered_associative_task_1/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <set>
 
 /**
  * @todo Implement function that will check if one string has the same characters
@@ -12,4 +13,20 @@
  * @param second - second word
  * @return true if first is anagram of second
  */
-bool is_anagram(const std::string& first, const std::string& second);
+bool is_anagram(const std::string& first, const std::string& second)
+{
+    std::multiset<char> first_mset;
+    std::copy(first.begin(),
+              first.end(),
+              std::inserter(first_mset, first_mset.end()));
+
+    std::multiset<char> second_mset;
+    std::copy(second.begin(),
+              second.end(),
+              std::inserter(second_mset, second_mset.end()));
+
+    if(first_mset == second_mset)
+        return true;
+    else
+        return false;
+}
