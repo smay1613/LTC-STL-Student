efe829e8eb3594e9aedabd68ed593c205019ec0a
Tetiana Dobrovolska <TDobrovolska@luxoft.com>
2020-12-17 22:44:59 +0200

    Adaptor task TDobrovolska

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,4 +1,6 @@
 #include "impl.h"
+#include <algorithm>
+#include <stack>
 #include <vector>
 
 const std::vector<std::pair<char, char>> brackets {
@@ -6,3 +8,31 @@ const std::vector<std::pair<char, char>> brackets {
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    std::stack<char> stack;
+    for (const char& c : source)
+    {
+        const auto it = std::find_if(brackets.cbegin(), brackets.cend(), [&](std::pair<char, char> const & pair)
+        {
+                return pair.first == c || pair.second == c;
+        });
+        if (it != brackets.end())
+        {
+            if (it->first == c)
+            {
+                stack.push(c);
+            }
+            else if (it->second == c)
+            {
+                if (stack.empty() || stack.top() != it->first)
+                {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+    }
+    return stack.empty();
+}
