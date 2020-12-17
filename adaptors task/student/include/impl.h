317a3adf5992b80873da5b4374fa28cebae14fab
Yevhenii Zaiets <YZaiets@luxoft.com>
2020-12-17 19:32:48 +0200

    Revert "Adaptor_Tast_Zaiets_Completed"
    This reverts commit 08281979ef1e9ad626e811abcd0aa1455efbfb45.

--- adaptors task/student/include/impl.h
+++ adaptors task/student/include/impl.h
@@ -1,8 +1,5 @@
 #pragma once
 #include <string>
-#include <stack>
-#include <vector>
-#include <algorithm>
 
 /**
  * @todo isValid - check if given expression is valid - all open brackets are closed
@@ -14,35 +11,4 @@
  *
  * @return true if expression is valid
  */
-
-std::vector<std::pair<char, char>> braces_collection =
-{
-    {'{', '}'},
-    {'(', ')'},
-    {'[', ']'}
-};
-
-bool isValid(const std::string& source)
-{
-    std::stack<char> char_sequence;
-    for(auto& ch : source)
-    {
-        auto it = std::find_if(braces_collection.begin(), braces_collection.end(), [&ch](std::pair<char, char> braces)
-        {
-            return (braces.first == ch || braces.second == ch);
-        });
-
-        if(it != braces_collection.end())
-        {
-            if(it->first == ch) char_sequence.push(ch);
-            else if(it->second == ch)
-            {
-                if(!char_sequence.empty() && char_sequence.top() == it->first) char_sequence.pop();
-                else return false;
-            }
-        }
-    }
-
-    return char_sequence.empty();
-}
-
+bool isValid(const std::string& source);
