7fd2ef86e3ea6e0b3d88e8d690629023b031d1cf
Tetiana Dobrovolska <TDobrovolska@luxoft.com>
2020-12-17 23:30:03 +0200

    unoredered associative container TDobrovolska

--- unordered_associative_task_1/student/src/impl.cpp
+++ unordered_associative_task_1/student/src/impl.cpp
@@ -1,1 +1,30 @@
+#include <unordered_map>
+
 #include "impl.h"
+
+bool is_anagram(const std::string& first, const std::string& second)
+{
+    std::unordered_map<char, int> map_converter;
+    if (first.length() != second.length())
+    {
+        return false;
+    }
+
+    for (const char& c : first)
+    {
+        map_converter[c]++;
+    }
+
+    for (const char& c : second)
+    {
+        if (map_converter.end() == map_converter.find(c))
+        {
+            return false;
+        }
+        if (0 == (--map_converter[c]))
+        {
+            map_converter.erase(c);
+        }
+    }
+    return map_converter.size() == 0;
+}
