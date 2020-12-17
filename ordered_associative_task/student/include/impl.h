6df57cedc46feb5a76b988905ad85cb4a7875a65
visper <zzzvisperzzz@yandex.ru>
2020-12-17 15:44:33 +0200

    ordered_associative_task

--- ordered_associative_task/student/include/impl.h
+++ ordered_associative_task/student/include/impl.h
@@ -15,4 +15,24 @@
  *
  * @return 1-5
  */
-size_t convertMarkTo5PointSystem(size_t markIn100Point);
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    std::map<size_t, size_t> conv = { {0, 1},
+                                 {29, 1},
+                                 {30, 2},
+                                 {49, 2},
+                                 {50, 3},
+                                 {69, 3},
+                                 {70, 4},
+                                 {89, 4},
+                                 {90, 5}
+                                };
+
+    auto lower_ = conv.lower_bound(markIn100Point);
+
+    if(lower_ != conv.end()) {
+        return lower_->second;
+    } else {
+        return 5;
+    }
+}
