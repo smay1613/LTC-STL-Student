79bba4ab28807a9bf05d7cdcf1b2daf3484cb21e
lexa <myrmik@gmail.com>
2020-12-18 09:20:27 +0200

    second

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,12 +1,14 @@
 #include "impl.h"
 
+#include <vector>
+
 std::string reverseWords(const std::string& input)
 {
     std::string ret;
     if(!input.empty())
     {
         std::stringstream in(input);
-        const std::deque<std::string> ds { std::istream_iterator<std::string>(in), std::istream_iterator<std::string>() };
+        const std::vector<std::string> ds { std::istream_iterator<std::string>(in), std::istream_iterator<std::string>() };
         std::stringstream out;
         std::copy(ds.rbegin(), ds.rend(), std::ostream_iterator<std::string>(out, " "));
         ret = out.str();
