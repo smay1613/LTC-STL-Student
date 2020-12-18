d1367affbf33ab2c97d8bba3bf0de8c616205c2e
Anna Molodchenkova <amolodchenkova@luxoft.com>
2020-12-18 05:22:14 +0200

    adaptor task

--- adaptors task/student/src/impl.cpp
+++ adaptors task/student/src/impl.cpp
@@ -1,8 +1,45 @@
 #include "impl.h"
 #include <vector>
+#include <algorithm>
+#include <stack>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    bool isValid = true;
+    std::stack<char> stack;
+    std::for_each(source.begin(),source.end(), [&stack,&isValid](const char& ch)
+    {
+       std::for_each(brackets.begin(),brackets.end(),[&ch,&stack,&isValid](const std::pair<char, char>& pair)
+       {
+          if(ch == pair.first)
+          {
+             stack.push(ch);
+          }
+
+          if(ch == pair.second)
+          {
+              if ( stack.empty() )
+              {
+                  isValid = false;
+                  return;
+              }
+              else if( stack.top() != pair.first )
+              {
+                  isValid = false;
+                  return;
+              }
+              else
+              {
+                  stack.pop();
+              }
+          }
+       });
+    });
+    return (isValid && stack.empty());
+}
