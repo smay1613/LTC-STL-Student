12a2a0ad2039fd58bb090c608abc5b615c9965c9
Ubuntu 20.04 <asg@ubuntu20.localdomain>
2020-12-19 20:26:00 +0200

    Comments implementation

--- unordered_associative_task_1/student/src/impl.cpp
+++ unordered_associative_task_1/student/src/impl.cpp
@@ -1,9 +1,9 @@
 #include "impl.h"
-#include "unordered_set"
+#include "set"
 
 bool is_anagram(const std::string& first, const std::string& second)
 {
-    const std::unordered_multiset<char> set_f(first.begin(), first.end());
-    const std::unordered_multiset<char> set_s(second.begin(), second.end());
-    return set_f == set_s;
+    const std::multiset<char> first_word(first.begin(), first.end());
+    const std::multiset<char> second_word(second.begin(), second.end());
+    return first_word == second_word;
 }
