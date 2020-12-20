d80428e3358b48ead8d69f41f42cb0c6744ffeb4
Ubuntu 20.04 <asg@ubuntu20.localdomain>
2020-12-20 21:31:01 +0200

    Some includes correction in tests

--- unordered_associative_task_2/student/tests/unit_tests/src/maintest.cpp
+++ unordered_associative_task_2/student/tests/unit_tests/src/maintest.cpp
@@ -4,19 +4,19 @@ using namespace ::testing;
 
 TEST_F(ThreeFiles, OneGroupThreeFiles)
 {
-    for (size_t i = 0; i < 3; i++) {
+    for (size_t i = 0; i < 3; i++)
+    {
         files[i] << "11";
     }
 
-    for (auto& file : files) {
+    for (auto& file : files)
+    {
         file.close();
     }
 
-    std::vector<std::vector<std::string>> groups { findDuplicates("sandbox") };
+    std::vector<std::vector<std::string>> groups {findDuplicates("sandbox")};
     EXPECT_EQ(groups.size(), 1);
     EXPECT_EQ(groups.front().size(), 3);
-
-    listFiles("sandbox");
 }
 
 TEST_F(ThreeFiles, ThreeGroupsOneFile)
@@ -25,11 +25,12 @@ TEST_F(ThreeFiles, ThreeGroupsOneFile)
     files[1] << "12";
     files[2] << "13";
 
-    for (auto& file : files) {
+    for (auto& file : files)
+    {
         file.close();
     }
 
-    std::vector<std::vector<std::string>> groups { findDuplicates("sandbox") };
+    std::vector<std::vector<std::string>> groups {findDuplicates("sandbox")};
     EXPECT_EQ(groups.size(), 0);
 }
 
@@ -39,11 +40,12 @@ TEST_F(ThreeFiles, Reversed)
     files[1] << "54321";
     files[2] << "12345";
 
-    for (auto& file : files) {
+    for (auto& file : files)
+    {
         file.close();
     }
 
-    std::vector<std::vector<std::string>> groups { findDuplicates("sandbox") };
+    std::vector<std::vector<std::string>> groups {findDuplicates("sandbox")};
     EXPECT_EQ(groups.size(), 1);
     EXPECT_EQ(groups.front().size(), 2);
 }
@@ -55,35 +57,40 @@ TEST_F(TwoFolders, TwoGroups)
     files[2] << "121";
     files[3] << "121";
 
-    for (auto& file : files) {
+    for (auto& file : files)
+    {
         file.close();
     }
 
-    std::vector<std::vector<std::string>> groups { findDuplicates("sandbox") };
+    std::vector<std::vector<std::string>> groups {findDuplicates("sandbox")};
     EXPECT_EQ(groups.size(), 2);
     EXPECT_EQ(groups.front().size(), 2);
 }
 
 TEST_F(NineFiles, ThreeGroupsThreeFiles)
 {
-    for (size_t i = 0; i < 3; i++) {
+    for (size_t i = 0; i < 3; i++)
+    {
         files[i] << "abcd";
     }
 
-    for (size_t i = 3; i < 6; i++) {
+    for (size_t i = 3; i < 6; i++)
+    {
         files[i] << "abcde";
     }
 
-    for (size_t i = 6; i < 9; i++) {
+    for (size_t i = 6; i < 9; i++)
+    {
         files[i] << "abcdg";
     }
 
-    for (auto& file : files) {
+    for (auto& file : files)
+    {
         file.close();
     }
 
-    std::vector<std::vector<std::string>> groups { findDuplicates("sandbox") };
+    std::vector<std::vector<std::string>> groups {findDuplicates("sandbox")};
     EXPECT_EQ(groups.size(), 3);
     EXPECT_TRUE(std::all_of(groups.begin(), groups.end(),
-        [](const std::vector<std::string>& subgroup) { return subgroup.size() == 3; }));
+                          [](const std::vector<std::string>& subgroup) {return subgroup.size() == 3;}));
 }
