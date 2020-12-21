9d9784795c09db1d671f0cdf2d945f25fdd19541
Verzhbytskyi <DVerzhbytskyi@kiev.luxoft.com>
2020-12-21 23:40:34 +0200

    resolving sequence task 3

--- sequence_task_3/student/src/impl.cpp
+++ sequence_task_3/student/src/impl.cpp
@@ -1,2 +1,17 @@
 #include "impl.h"
 
+std::string reverseWords(const std::string& input)
+{
+    if (input.empty()){
+        return input;
+    }
+    std::stringstream stream (input);
+    std::string streamOut;
+
+    for(std::string streamIn; stream>>streamIn; streamOut.insert(0, streamIn+' '));
+
+    streamOut.erase(streamOut.end() - 1);
+    if (*(streamOut.begin()) == ' ')
+        streamOut.erase(streamOut.begin());
+    return streamOut;
+}
