9d6f93fe0092bad9cf2187362fcdfc32fe69c06b
Denis Plotnikov <virtuallyworks@gmail.com>
2020-12-26 15:56:53 +0200

    unordered associative task (1) - Plotnikov

--- unordered associative task (1)/student/include/impl.h
+++ unordered associative task (1)/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <unordered_set>
 
 /**
  * @todo Implement function that will check if one string has the same characters
@@ -12,4 +13,15 @@
  * @param second - second word
  * @return true if first is anagram of second
  */
-bool is_anagram(const std::string& first, const std::string& second);
+bool is_anagram(const std::string& first, const std::string& second){
+  if (first.size() != second.size()) return false;
+  else {
+    std::unordered_multiset<char> lhs;
+    std::unordered_multiset<char> rhs;
+    for (size_t i = 0; i < first.size(); i++) {
+      lhs.insert(first[i]);
+      rhs.insert(second[i]);
+    }
+    return lhs == rhs;
+  }
+}
