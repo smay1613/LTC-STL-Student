615acda48a53df25698334ce64647d05a32730dd
Ubuntu 20.04 <asg@ubuntu20.localdomain>
2020-12-27 22:10:30 +0200

    Some corrections after comments

--- pseudocontainers_task/student/src/impl.cpp
+++ pseudocontainers_task/student/src/impl.cpp
@@ -6,15 +6,17 @@
 std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_map<worker_id, calendar>& workerVacations)
 {
     std::tuple<worker_id, worker_id, days> intersection(0, 0, 0);
-    for (auto iW = workerVacations.cbegin(); iW != workerVacations.cend(); ++iW) {
-        const std::bitset<daysInMonth> iVacation(iW->second, 0, daysInMonth, availableMarker, vacationMarker);
+    for (auto const& iWorker : workerVacations) {
+        const std::bitset<daysInMonth> iVacation(iWorker.second, 0, daysInMonth, availableMarker, vacationMarker);
 
-        for (auto jW = std::next(iW); jW != workerVacations.cend(); ++jW) {
-            const std::bitset<daysInMonth> jVacation(jW->second, 0, daysInMonth, availableMarker, vacationMarker);
-
+        for (auto const& jWorker : workerVacations) {
+            if (iWorker.first == jWorker.first) {
+                continue;
+            }
+            const std::bitset<daysInMonth> jVacation(jWorker.second, 0, daysInMonth, availableMarker, vacationMarker);
             auto ijIntersection = iVacation & jVacation;
             if (std::get<2>(intersection) < ijIntersection.count()) {
-                intersection = std::tuple<worker_id, worker_id, days>(iW->first, jW->first, ijIntersection.count());
+                intersection = std::make_tuple(iWorker.first, jWorker.first, ijIntersection.count());
             }
         }
     }
