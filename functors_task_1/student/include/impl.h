14eba52623ba49df0be14f45fe59267a8c2b43a5
Oleksandr Pichuryn <OPichuryn@luxoft.com>
2021-02-15 16:03:33 +0200

    [opichuryn] functors_task_1 implementation

--- functors_task_1/student/include/impl.h
+++ functors_task_1/student/include/impl.h
@@ -8,6 +8,26 @@
  */
 struct BeerOrganizer
 {
+    BeerBrand operator()()
+    {
+        constexpr size_t BEER_NUM = 6;
+        constexpr wall_shelf<BEER_NUM> brands {
+            BeerBrand::HoeGaarden,
+            BeerBrand::Corona,
+            BeerBrand::Carlsberg,
+            BeerBrand::Bud,
+            BeerBrand::ZlataPraha,
+            BeerBrand::Leffe
+        };
+
+        if (index >= brands.size()) {
+            index = 0;
+        }
+        return brands[index++];
+    }
+
+private:
+    size_t index = 0;
 };
 
 /**
@@ -16,16 +36,18 @@ struct BeerOrganizer
  *
  * @note Only Corona and HoeGaarden are expensive
  */
-bool isExpensiveBeer(/**???*/)
+bool isExpensiveBeer(BeerBrand brand)
 {
+    return brand == BeerBrand::Corona || brand == BeerBrand::HoeGaarden;
 }
 
 /**
  * @todo Implement lambda beer country equality comparator
  * @return true if beer county is the same, false otherwise
  */
-auto sameCountry = [](/**???*/)
+auto sameCountry = [](BeerBrand brand1, BeerBrand brand2)
 {
+    return getBeerCountry(brand1) == getBeerCountry(brand2);
 };
 
 struct MixingPolicy
@@ -40,9 +62,28 @@ struct MixingPolicy
      * Whiskey + SevenUp = SevenPlusSeven;
      * Others + Others = Oops;
      */
-    static Cocktail mix(/**???*/)
+    static Cocktail mix(AlcoholDrink alcohol, NonAlcoholDrink soft)
     {
+        switch (alcohol) {
+            case AlcoholDrink::Gin: {
+                switch (soft) {
+                    case NonAlcoholDrink::LimeJuice: return Cocktail::Gimlet;
+                    case NonAlcoholDrink::GrapefruitJuice: return Cocktail::Greyhount;
+                    default: break;
+                }
+            } break;
+
+            case AlcoholDrink::Whiskey: {
+                switch (soft) {
+                    case NonAlcoholDrink::SevenUp: return Cocktail::SevenPlusSeven;
+                    default: break;
+                }
+            } break;
+
+            default: break;
+        }
+        return Cocktail::Oops;
     }
 };
 
-std::function</**???*/> mixer {&MixingPolicy::mix};
+std::function<Cocktail(AlcoholDrink, NonAlcoholDrink)> mixer {&MixingPolicy::mix};
