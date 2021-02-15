de67424bd63ab36fd4963fcf06974b78c78c210f
Oleksandr Pichuryn <OPichuryn@luxoft.com>
2021-02-15 14:31:20 +0200

    [practice] iterators_task_1 implementation

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,2 +1,20 @@
 #include "impl.h"
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+string reverseWords(const string& input) {
+    istringstream iss {input};
+    vector<string> tokens{istream_iterator<string>{iss}, istream_iterator<string>{}};
+    ostringstream oss;
+
+    copy(tokens.rbegin(),
+         tokens.rend() - 1,
+         ostream_iterator<string>(oss, " "));
+
+    if (tokens.size() > 0) {
+        oss << tokens.front();
+    }
+    return oss.str();
+}
