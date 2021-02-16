3b4b1c0e56d632dba16880ca810d97ddff912c5d
Oleksandr Khudyk <OKhudyk@luxoft.com>
2021-02-16 20:22:17 +0200

    functors_task_1 Khudyk

--- functors_task_1/student/include/impl.h
+++ functors_task_1/student/include/impl.h
@@ -8,6 +8,14 @@
  */
 struct BeerOrganizer
 {
+    BeerBrand operator()() {
+        state = static_cast<BeerBrand>(static_cast<int>(state) + 1);
+        if(state == BeerBrand::Max)
+            state = BeerBrand::HoeGaarden;
+
+        return state;
+    }
+    BeerBrand state = BeerBrand::None;
 };
 
 /**
@@ -16,16 +24,25 @@ struct BeerOrganizer
  *
  * @note Only Corona and HoeGaarden are expensive
  */
-bool isExpensiveBeer(/**???*/)
+bool isExpensiveBeer(BeerBrand beer)
 {
+    if(beer == BeerBrand::Corona || beer == BeerBrand::HoeGaarden)
+        return true;
+    return false;
 }
 
 /**
  * @todo Implement lambda beer country equality comparator
  * @return true if beer county is the same, false otherwise
  */
-auto sameCountry = [](/**???*/)
+auto sameCountry = [](BeerBrand firstBeerBrand, BeerBrand secondBeerBrand)
 {
+    Country firstCountry = getBeerCountry(firstBeerBrand);
+    Country secondCountry = getBeerCountry(secondBeerBrand);
+
+    if (firstCountry == secondCountry)
+        return true;
+    return false;
 };
 
 struct MixingPolicy
@@ -40,9 +57,16 @@ struct MixingPolicy
      * Whiskey + SevenUp = SevenPlusSeven;
      * Others + Others = Oops;
      */
-    static Cocktail mix(/**???*/)
+    static Cocktail mix(AlcoholDrink alcoholDrink, NonAlcoholDrink nonAlcoholDrink)
     {
+        if(alcoholDrink == AlcoholDrink::Gin && nonAlcoholDrink == NonAlcoholDrink::LimeJuice)
+            return Cocktail::Gimlet;
+        if(alcoholDrink == AlcoholDrink::Gin && nonAlcoholDrink == NonAlcoholDrink::GrapefruitJuice)
+            return Cocktail::Greyhount;
+        if(alcoholDrink == AlcoholDrink::Whiskey && nonAlcoholDrink == NonAlcoholDrink::SevenUp)
+            return Cocktail::SevenPlusSeven;
+        return Cocktail::Oops;
     }
 };
 
-std::function</**???*/> mixer {&MixingPolicy::mix};
+std::function<Cocktail(AlcoholDrink, NonAlcoholDrink)> mixer {&MixingPolicy::mix};
