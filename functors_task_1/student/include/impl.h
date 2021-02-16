be05576bc88960ec0696285d85f722cd08d463b0
chizarus <chizarus@gmail.com>
2021-02-16 18:08:25 +0200

    functors_task_1

--- functors_task_1/student/include/impl.h
+++ functors_task_1/student/include/impl.h
@@ -8,6 +8,17 @@
  */
 struct BeerOrganizer
 {
+    BeerBrand operator()() {
+        if (m_currentIndex == m_maxIndex - 1) {
+            m_currentIndex = static_cast<size_t>(BeerBrand::None);
+        }
+
+        return static_cast<BeerBrand>(++m_currentIndex);
+    }
+
+private:
+    size_t m_currentIndex{static_cast<size_t>(BeerBrand::None)};
+    size_t m_maxIndex{static_cast<size_t>(BeerBrand::Max)};
 };
 
 /**
@@ -16,16 +27,18 @@ struct BeerOrganizer
  *
  * @note Only Corona and HoeGaarden are expensive
  */
-bool isExpensiveBeer(/**???*/)
+bool isExpensiveBeer(BeerBrand brand)
 {
+    return brand == BeerBrand::Corona || brand == BeerBrand::HoeGaarden;
 }
 
 /**
  * @todo Implement lambda beer country equality comparator
  * @return true if beer county is the same, false otherwise
  */
-auto sameCountry = [](/**???*/)
+auto sameCountry = [](BeerBrand brand1, BeerBrand brand2)
 {
+    return getBeerCountry(brand1) == getBeerCountry(brand2);
 };
 
 struct MixingPolicy
@@ -40,9 +53,19 @@ struct MixingPolicy
      * Whiskey + SevenUp = SevenPlusSeven;
      * Others + Others = Oops;
      */
-    static Cocktail mix(/**???*/)
+    static Cocktail mix(AlcoholDrink alcohol, NonAlcoholDrink nonAlcohol)
     {
+        if (alcohol == AlcoholDrink::Gin && nonAlcohol == NonAlcoholDrink::LimeJuice) {
+            return Cocktail::Gimlet;
+        }
+        if (alcohol == AlcoholDrink::Gin && nonAlcohol == NonAlcoholDrink::GrapefruitJuice) {
+            return Cocktail::Greyhount;
+        }
+        if (alcohol == AlcoholDrink::Whiskey && nonAlcohol == NonAlcoholDrink::SevenUp) {
+            return Cocktail::SevenPlusSeven;
+        }
+        return Cocktail::Oops;
     }
 };
 
-std::function</**???*/> mixer {&MixingPolicy::mix};
+std::function<Cocktail(AlcoholDrink, NonAlcoholDrink)> mixer {&MixingPolicy::mix};
