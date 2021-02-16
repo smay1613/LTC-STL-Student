d98fbfa39a885dfd69c50b622ebcb9aaf90ad074
Vadym Ostanin <VOOstanin@luxoft.com>
2021-02-16 14:54:17 +0200

    functors_task_1_-_Ostanin

--- functors_task_1/student/include/impl.h
+++ functors_task_1/student/include/impl.h
@@ -8,6 +8,31 @@
  */
 struct BeerOrganizer
 {
+    BeerOrganizer()
+    {
+    }
+    BeerBrand operator()()
+    {
+        auto result = BeerBrand::None;
+        do
+        {
+            if(0 == mBrands.size())
+            {
+                break;
+            }
+            result = *(std::begin(mBrands) + mBrandIndex);
+            ++mBrandIndex;
+            if(mBrands.size() <= mBrandIndex)
+            {
+                mBrandIndex = 0;
+            }
+        }while(false);
+        return result;
+    }
+private:
+    typedef std::array<BeerBrand, (int)(BeerBrand::Max) - 1> BeerBrandsType;
+    BeerBrandsType mBrands = {BeerBrand::HoeGaarden, BeerBrand::Corona, BeerBrand::Carlsberg, BeerBrand::Bud, BeerBrand::ZlataPraha, BeerBrand::Leffe};
+    uint mBrandIndex{0};
 };
 
 /**
@@ -16,16 +41,19 @@ struct BeerOrganizer
  *
  * @note Only Corona and HoeGaarden are expensive
  */
-bool isExpensiveBeer(/**???*/)
+bool isExpensiveBeer(BeerBrand brand)
 {
+    return (brand == BeerBrand::HoeGaarden ||
+            brand == BeerBrand::Corona) ? true : false;
 }
 
 /**
  * @todo Implement lambda beer country equality comparator
  * @return true if beer county is the same, false otherwise
  */
-auto sameCountry = [](/**???*/)
+auto sameCountry = [](BeerBrand lhs, BeerBrand rhs)
 {
+    return getBeerCountry(lhs) == getBeerCountry(rhs);
 };
 
 struct MixingPolicy
@@ -40,9 +68,23 @@ struct MixingPolicy
      * Whiskey + SevenUp = SevenPlusSeven;
      * Others + Others = Oops;
      */
-    static Cocktail mix(/**???*/)
+    static Cocktail mix(AlcoholDrink lhs, NonAlcoholDrink rhs)
     {
+        Cocktail result = Cocktail::Oops;
+        if(AlcoholDrink::Gin == lhs && NonAlcoholDrink::LimeJuice == rhs)
+        {
+            result = Cocktail::Gimlet;
+        }
+        else if(AlcoholDrink::Gin == lhs && NonAlcoholDrink::GrapefruitJuice == rhs)
+        {
+            result = Cocktail::Greyhount;
+        }
+        else if(AlcoholDrink::Whiskey == lhs && NonAlcoholDrink::SevenUp == rhs)
+        {
+            result = Cocktail::SevenPlusSeven;
+        }
+        return result;
     }
 };
 
-std::function</**???*/> mixer {&MixingPolicy::mix};
+std::function<Cocktail(AlcoholDrink, NonAlcoholDrink)> mixer {&MixingPolicy::mix};
