267432593d6e2a2d22b7ddb2bd1c7bed5532e5d8
Oleksandr Minenko <ominenko@luxoft.com>
2021-02-18 16:51:33 +0200

    [ominenko] iterators_task_1

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,2 +1,13 @@
 #include "impl.h"
+#include "vector"
 
+std::string reverseWords(const std::string& input){
+    std::istringstream stream {input};
+    std::vector<std::string> words{std::istream_iterator<std::string> {stream},
+                                   std::istream_iterator<std::string> {}};
+    std::ostringstream out;
+    std::copy(words.rbegin(), words.rend(), std::ostream_iterator<std::string>{out, " "});
+    auto result=out.str();
+    result.pop_back();
+    return result;
+}
