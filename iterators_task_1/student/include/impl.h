885c2298e363ea19e6e40dbeae4cbb85644e8fec
Serhii Starodumov <SStarodumov@luxoft.com>
2021-02-18 02:15:32 +0200

    iterators_task_1_Serhii_Starodumov

--- iterators_task_1/student/include/impl.h
+++ iterators_task_1/student/include/impl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <sstream>
 #include <iterator>
+#include <string>
 
 /**
  * @todo Implement function that will reverse words in a sentence
@@ -11,3 +12,17 @@
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
 std::string reverseWords(const std::string& input);
+
+class TokenString{
+public:
+    const std::string& getToken() const;
+    std::string& getToken();
+    bool isEmpty() const;
+private:
+    std::string m_data;
+};
+
+std::istream& operator>>(std::istream& stream, TokenString& token){
+    std::getline(stream, token.getToken(), ' ');
+    return stream;
+}
--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,2 +1,39 @@
 #include "impl.h"
+#include <vector>
+#include <algorithm>
 
+std::string reverseWords(const std::string& input){
+    std::stringstream sstream(input);
+    std::vector<TokenString> tokens {std::istream_iterator<TokenString>{sstream}, std::istream_iterator<TokenString>{}};
+
+
+    tokens.erase(std::remove_if(tokens.begin(), tokens.end(), [](TokenString token){
+        return token.isEmpty();
+    }), tokens.end());
+
+    std::vector<std::string> reversedTokens;
+    std::transform(tokens.rbegin(), tokens.rend(), std::back_inserter(reversedTokens), [](const TokenString& token){
+        return token.getToken();
+    });
+
+    sstream = std::stringstream();
+    std::copy(reversedTokens.begin(), reversedTokens.end(), std::ostream_iterator<std::string>{sstream, " "});
+
+    std::string result{sstream.str()};
+    if (!result.empty()){
+        result.pop_back();
+    }
+    return result;
+}
+
+const std::string& TokenString::getToken() const {
+    return m_data;
+}
+
+std::string &TokenString::getToken() {
+    return m_data;
+}
+
+bool TokenString::isEmpty() const {
+    return m_data.empty();
+}
