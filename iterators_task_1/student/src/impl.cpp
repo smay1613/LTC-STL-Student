8219bff9daadc99bd468486d945e67a7bd24f827
Kostiantyn Kostiuchenko <kakostiuchenko@luxoft.com>
2021-02-18 21:05:25 +0200

    iterators_task_1_kostiuchenko

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,11 +1,11 @@
 #include "impl.h"
-#include <list>
+#include <deque>
 
 std::string reverseWords(const std::string& input)
 {
     std::stringstream inputStream{input};
     std::stringstream outputStream;
-    std::list<std::string> inputStrList;
+    std::deque<std::string> inputStrList;
 
     std::copy(std::istream_iterator<std::string>{inputStream}, std::istream_iterator<std::string>{}, std::front_inserter(inputStrList));
     std::copy(inputStrList.begin(), inputStrList.end(), std::ostream_iterator<std::string>{outputStream, " "});
