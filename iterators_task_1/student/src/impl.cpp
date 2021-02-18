cecd0d5dcd75c29f4d47dab7e5177ca017faef9c
veem <>
2021-02-18 10:41:26 +0200

    iterators_task_1_verna 2

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 #include <iterator>
 #include <algorithm>
-#include <list>
+#include <vector>
 
 
 std::string reverseWords(const std::string& input)
@@ -14,49 +14,19 @@ std::string reverseWords(const std::string& input)
     }
 
     std::istringstream inputStream {input};
-    std::ostringstream outputStream;
-    std::list<std::string> container = {};
-
-    std::copy(std::istream_iterator<std::string>(inputStream),
-              std::istream_iterator<std::string>(),
-              std::front_inserter(container));
+    std::vector<std::string> container { std::istream_iterator<std::string>(inputStream),
+                                         std::istream_iterator<std::string>() };
 
     if (container.empty())
     {
         return {};
     }
 
-    auto begin = std::begin(container);
-    auto end = std::end(container);
-    std::copy(begin, std::prev(end), std::ostream_iterator<std::string>(outputStream, " "));
-    std::copy(std::prev(end), end, std::ostream_iterator<std::string>(outputStream));
+    std::ostringstream outputStream;
+    auto rbegin = container.rbegin();
+    auto rend = container.rend();
+    std::copy(rbegin, std::prev(rend), std::ostream_iterator<std::string>(outputStream, " "));
+    outputStream << container.front();
 
     return outputStream.str();
 }
-
-//#include <vector>
-
-//std::string reverseWords(const std::string& input)
-//{
-//    if (input.empty())
-//    {
-//        return {};
-//    }
-
-//    std::istringstream inputStream {input};
-//    std::ostringstream outputStream;
-//    std::vector<std::string> container { std::istream_iterator<std::string>(inputStream),
-//                                         std::istream_iterator<std::string>() };
-
-//    if (container.empty())
-//    {
-//        return {};
-//    }
-
-//    auto rbegin = container.rbegin();
-//    auto rend = container.rend();
-//    std::copy(rbegin, std::prev(rend), std::ostream_iterator<std::string>(outputStream, " "));
-//    std::copy(std::prev(rend), rend, std::ostream_iterator<std::string>(outputStream));
-
-//    return outputStream.str();
-//}
