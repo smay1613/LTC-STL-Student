faba6158e16437ef91c7950ccf95ca5723f6d1aa
Serhii Starodumov <SStarodumov@luxoft.com>
2021-02-19 11:30:35 +0200

    iterators_task_2_Serhii_Starodumov

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -2,7 +2,7 @@
 #include <iterator>
 
 template <class Container>
-struct accumulator
+class accumulator : public std::iterator<std::output_iterator_tag, typename Container::value_type>
 {
     /** @todo Iterator traits*/
     /** @todo Constructor with container*/
@@ -10,6 +10,26 @@ struct accumulator
     /** @todo rvalue writing operator*/
 
     /** @todo Iterator operations */
+
+public:
+    using ValueType = typename Container::value_type;
+
+    accumulator operator*() const{
+        return *this;
+    }
+
+    const accumulator& operator++() const {
+        return *this;
+    }
+    accumulator& operator++() {
+        return *this;
+    }
+    accumulator& operator=(const ValueType& value){
+        container->push_back(value);
+        return *this;
+    }
+private:
+    Container* container;
 };
 
 /**
