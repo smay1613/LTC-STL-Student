607d2f222f7309d27ff16f966f7e878da01270e5
Vladyslav Fedorovskyi <VFedorovskyi@luxoft.com>
2021-02-19 09:39:51 +0200

    [Fedorvoskyi] iterator_task_1

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,2 +1,21 @@
 #include "impl.h"
+#include <vector>
+#include <algorithm>
 
+
+std::string reverseWords(const std::string &input)
+{
+    std::istringstream iss{input};
+    std::vector<std::string> tokens{std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{}};
+
+    if(tokens.empty())
+        return "";
+
+    std::ostringstream oss;
+    std::copy(tokens.rbegin(), tokens.rend(), std::ostream_iterator<std::string>(oss, " "));
+
+    std::string reverseString{oss.str()};
+    reverseString.pop_back();
+
+    return reverseString;
+}
