24a60c628efb3edd1e4d85b741b4dd4f4fc90000
mrassokha <MRassokha@luxoft.com>
2021-02-19 14:51:58 +0200

    [Maksym Rassokha] Iterator_task_1

--- iterators_task_1/student/include/impl.h
+++ iterators_task_1/student/include/impl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <sstream>
 #include <iterator>
+#include <vector>
 
 /**
  * @todo Implement function that will reverse words in a sentence
@@ -10,4 +11,21 @@
  *
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
-std::string reverseWords(const std::string& input);
+
+std::string reverseWords(const std::string& input)
+{
+    std::stringstream streamInput(input);
+    std::vector<std::string> tokenized(std::istream_iterator<std::string>{streamInput},
+                                       std::istream_iterator<std::string>{});
+
+    std::stringstream streamOutput{};
+    std::copy(tokenized.rbegin(),tokenized.rend(),
+                 std::ostream_iterator<std::string>{streamOutput," "});
+
+    std::string merged{streamOutput.str()};
+
+    if (!merged.empty())
+        merged.pop_back();
+
+    return merged;
+};
