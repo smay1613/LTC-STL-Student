4227cf6a21e258ccbb1d47fe89c5fe472e621e43
Oleksandr Pichuryn <OPichuryn@luxoft.com>
2021-02-19 13:32:30 +0200

    [opichuryn] iterators_task_2

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -1,21 +1,52 @@
 #pragma once
 #include <iterator>
+#include <algorithm>
 
 template <class Container>
 struct accumulator
 {
-    /** @todo Iterator traits*/
-    /** @todo Constructor with container*/
-    /** @todo lvalue writing operator*/
-    /** @todo rvalue writing operator*/
+    using value_type = void;
+    using difference_type = void;
+    using pointer = void;
+    using reference = void;
+    using iterator_category = std::output_iterator_tag;
 
-    /** @todo Iterator operations */
+    accumulator(Container& container)
+        : m_ref(container)
+    {}
+
+    accumulator(const accumulator<Container>& other) = default;
+    accumulator<Container>& operator=(const accumulator<Container>& other) = default;
+
+    template <typename AnotherContainer>
+    accumulator& operator=(const AnotherContainer& container)
+    {
+        std::copy(container.cbegin(), container.cend(), std::back_inserter(m_ref.get()));
+        return *this;
+    }
+
+    template <typename AnotherContainer>
+    accumulator& operator=(AnotherContainer&& container){
+        std::move(container.begin(), container.end(), std::back_inserter(m_ref.get()));
+        return *this;
+    }
+
+    accumulator& operator++()
+    {
+        return *this;
+    }
+
+    accumulator<Container>& operator*()
+    {
+        return *this;
+    }
+
+private:
+    std::reference_wrapper<Container> m_ref;
 };
 
-/**
- *  @todo Implement accumulator object creator
- */
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>{container};
 }
