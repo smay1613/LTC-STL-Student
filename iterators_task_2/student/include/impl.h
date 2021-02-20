80a754ab0bda359b6cbc4fcd64cf9e4687676490
mrassokha <MRassokha@luxoft.com>
2021-02-20 11:46:07 +0200

    [Maksym Rassokha] Iterator_task_2

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -3,6 +3,7 @@
 
 template <class Container>
 struct accumulator
+        : public std::iterator<std::output_iterator_tag, void, void, void, void>
 {
     /** @todo Iterator traits*/
     /** @todo Constructor with container*/
@@ -10,6 +11,32 @@ struct accumulator
     /** @todo rvalue writing operator*/
 
     /** @todo Iterator operations */
+public:
+    explicit accumulator(Container& _x) {container = &_x;}
+
+    template<class rhsContainer>
+    accumulator<Container>& operator = (rhsContainer&& rhs) {
+        std::copy(std::make_move_iterator(rhs.begin()),
+                  std::make_move_iterator(rhs.end()),
+                  std::back_inserter(*container));
+    }
+
+    accumulator& operator = (const typename Container::value_type & _value) {
+        container->insert(_value);
+        return *this;
+    }
+
+    accumulator& operator = (const typename Container::value_type && _value) {
+        container->insert(std::move(_value));
+        return *this;
+    }
+
+    accumulator& operator*() {return *this;}
+    accumulator& operator++() {return *this;}
+    accumulator operator++(int) {return *this;}
+
+private:
+    Container *container;
 };
 
 /**
@@ -18,4 +45,6 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    accumulator<Container> accumulator{container};
+    return accumulator;
 }
