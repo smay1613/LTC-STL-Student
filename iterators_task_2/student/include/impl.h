350a6215d85f78eecf6fc0e7d66a1328a851fe0f
Vladyslav Fedorovskyi <VFedorovskyi@luxoft.com>
2021-02-21 19:03:43 +0200

    [Fedorovskyi] iterator_task_2

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -1,15 +1,25 @@
 #pragma once
 #include <iterator>
 
+// https://ideone.com/Vp22H3
 template <class Container>
 struct accumulator
 {
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::output_iterator_tag;
     /** @todo Iterator traits*/
     /** @todo Constructor with container*/
     /** @todo lvalue writing operator*/
     /** @todo rvalue writing operator*/
 
     /** @todo Iterator operations */
+
+    accumulator(Container conatiner) :
+        m_container{conatiner}
+    {}
+
+private:
+    Container m_container;
 };
 
 /**
