66c0f2a12c9f7a55f5347508b2a686db446c771a
Oleksandr Khudyk <OKhudyk@luxoft.com>
2021-02-21 20:32:49 +0200

    iterators_task_1 Khudyk

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,2 +1,21 @@
 #include "impl.h"
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <vector>
 
+std::string reverseWords(const std::string& input) {
+    std::istringstream iss(input);
+    std::vector<std::string> vStr{std::istream_iterator<std::string>{iss},
+                                 std::istream_iterator<std::string>()};
+    std::reverse(std::begin(vStr), std::end(vStr));
+    std::string str {};
+
+    std::all_of(vStr.begin(), vStr.end() - 1,[&str](const std::string& s) {
+                                           str += s;
+                                           str += " ";
+                                           return true;
+                                       });
+    str += vStr.back();
+    return str;
+}
