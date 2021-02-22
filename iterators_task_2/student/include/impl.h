ff48ff5effebf94686cb6fa5e7291526ab4d6ee8
Olha Kosiakova <okosiakova@luxoft.com>
2021-02-22 20:10:11 +0200

    iterators_task_2_Kosiakova

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -3,13 +3,36 @@
 
 template <class Container>
 struct accumulator
+        : std::iterator<std::output_iterator_tag, void, void, void, void>
 {
+public:
     /** @todo Iterator traits*/
     /** @todo Constructor with container*/
+    explicit accumulator(Container& container): m_container(container) {};
+
     /** @todo lvalue writing operator*/
+    template<class differentContainer>
+    accumulator& operator=(differentContainer& container) {
+        std::copy(container.begin(), container.end(), std::back_inserter(m_container));
+
+        return *this;
+    }
+
     /** @todo rvalue writing operator*/
+    template<class differentContainer>
+    accumulator& operator=(differentContainer&& container) {
+        std::move(container.begin(), container.end(), std::back_inserter(m_container));
+
+        return *this;
+    }
 
     /** @todo Iterator operations */
+    accumulator& operator*() { return *this; }
+    accumulator& operator++() { return *this; }
+    accumulator operator++(int) { return *this; }
+
+private:
+    Container &m_container;
 };
 
 /**
@@ -18,4 +41,5 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>(container);
 }
