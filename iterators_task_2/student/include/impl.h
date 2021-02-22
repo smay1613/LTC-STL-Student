124658f824150fb60a62c0d2785aaa9e3d037f4f
Kostiantyn Kostiuchenko <kakostiuchenko@luxoft.com>
2021-02-22 14:03:40 +0200

    iterators_task_2_kostiuchenko

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <iterator>
+#include <deque>
 
 template <class Container>
-struct accumulator
+struct accumulator : public std::iterator<std::output_iterator_tag, void, void, void, void>
 {
     /** @todo Iterator traits*/
     /** @todo Constructor with container*/
@@ -10,6 +11,39 @@ struct accumulator
     /** @todo rvalue writing operator*/
 
     /** @todo Iterator operations */
+
+protected:
+    Container* container;
+public:
+    explicit accumulator(Container& con): container(&con) {};
+
+    accumulator& operator=(Container& conLR) {
+        container->insert(container->end(), conLR.begin(), conLR.end());
+        return *this;
+    }
+
+    accumulator& operator=(Container&& conRR) {
+        container->insert(container->end(),
+                          std::make_move_iterator(conRR.begin()),
+                          std::make_move_iterator(conRR.end()));
+        return *this;
+    }
+
+    accumulator& operator=(const std::deque<typename Container::value_type>& decqu) {
+        container->insert(container->end(), decqu.begin(), decqu.end());
+        return *this;
+    }
+
+    accumulator& operator=(const typename Container::value_type& value) {
+        container->insert(container->end(), value);
+        return *this;
+    }
+    accumulator& operator=(typename Container::value_type&& value) {
+        container->insert(container->end(), std::move(value));
+        return *this;
+    }
+    accumulator& operator*() { return *this; }
+    accumulator& operator++() { return *this; }
 };
 
 /**
@@ -18,4 +52,5 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>(container);
 }
