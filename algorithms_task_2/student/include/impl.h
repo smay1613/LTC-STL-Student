7ad6ca30d85db31459daf914880273124fc4b17e
mrassokha <MRassokha@luxoft.com>
2021-02-22 11:20:04 +0200

    [Maksym Rassokha] Algorithm_Task_2

--- algorithms_task_2/student/include/impl.h
+++ algorithms_task_2/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <algorithm>
 
 /**
  * @todo Check if strings starts with another string
@@ -9,4 +10,15 @@
  *
  * @return true if source starts from a prefix
  */
-bool startsWith(const std::string& source, const std::string& prefix);
+bool startsWith(const std::string& source, const std::string& prefix)
+{
+// possible solution:
+//    int i = 0;
+//    while (i < prefix.size() && prefix[i] == source[i]){++i;}
+//    return i == prefix.length();
+
+  if (prefix.size() > source.size())
+      return false;
+
+  return std::equal(prefix.begin(), prefix.end(), source.begin());
+}
