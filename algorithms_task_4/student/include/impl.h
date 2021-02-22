8161153095b87b6d32ade112769b5e750c2808dd
Olha Kosiakova <okosiakova@luxoft.com>
2021-02-23 00:04:00 +0200

    algorithms_task_4_Kosiakova

--- algorithms_task_4/student/include/impl.h
+++ algorithms_task_4/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <iterator>
 
 /**
  * @todo Implement sliding algorithm (left/right shift of subsequence to a position)
@@ -12,4 +13,20 @@
  * @return new slided range position
  */
 template <typename RandomAccessIterator>
-std::pair<RandomAccessIterator, RandomAccessIterator> slide(RandomAccessIterator begin, RandomAccessIterator last, RandomAccessIterator pos);
+std::pair<RandomAccessIterator, RandomAccessIterator> slide(RandomAccessIterator begin, RandomAccessIterator last, RandomAccessIterator pos)
+{
+    if (pos == begin || pos == last) {
+        return { begin, last };
+    }
+
+    bool isRightShift =  pos > last;
+    if (isRightShift) {
+        auto newBegin = std::rotate(begin, last, pos);
+        return { newBegin, pos };
+    } else {
+        using rev_it = std::reverse_iterator<RandomAccessIterator>;
+
+        auto newEnd = std::rotate(rev_it(last), rev_it(begin), rev_it(pos));
+        return { pos, newEnd.base() };
+    }
+}
