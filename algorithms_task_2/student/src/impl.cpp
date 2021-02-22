4dd8f0c34b2e6026dfc5b9277e9b5bdfd85f8f75
Vladyslav Fedorovskyi <VFedorovskyi@luxoft.com>
2021-02-22 10:42:17 +0200

    algorithm_task_2

--- algorithms_task_2/student/src/impl.cpp
+++ algorithms_task_2/student/src/impl.cpp
@@ -1,1 +1,10 @@
 #include "impl.h"
+#include <algorithm>
+
+bool startsWith(const std::string &source, const std::string &prefix)
+{
+    if(prefix.size() > source.size())
+        return false;
+
+    return std::equal(begin(prefix), end(prefix), begin(source));
+}
