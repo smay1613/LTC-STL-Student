674b19287a1507e93a9291b0e09426b953ce9b66
Olha Kosiakova <okosiakova@luxoft.com>
2021-02-22 09:53:46 +0200

    iterators_task_1_Kosiakova

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,2 +1,25 @@
 #include "impl.h"
 
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+std::string reverseWords(const std::string& input)
+{
+    using ii_string = std::istream_iterator<std::string>;
+    using oi_string = std::ostream_iterator<std::string>;
+
+    std::istringstream iss {input};
+    std::vector<std::string> words {ii_string{iss}, ii_string{}};
+
+    if (words.empty()) {
+        return "";
+    }
+
+    std::stringstream result;
+    std::copy(words.rbegin(), words.rend(), oi_string(result, " "));
+    std::string str_result {result.str()};
+    str_result.pop_back();
+
+    return str_result;
+}
