a2f70f228f900f4fb2e941216ff0c378559a60b4
chizarus <chizarus@gmail.com>
2021-02-22 09:38:49 +0200

    iterators_task_2

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -2,14 +2,46 @@
 #include <iterator>
 
 template <class Container>
-struct accumulator
+struct accumulator : public std::iterator<std::output_iterator_tag, void,void,void,void>
 {
-    /** @todo Iterator traits*/
+
     /** @todo Constructor with container*/
+    accumulator(Container& container) : m_cotainer(container)
+    {}
+
+    accumulator(const accumulator<Container>& other) = default;
+
     /** @todo lvalue writing operator*/
+    template <typename OtherContainer>
+    accumulator& operator=(const OtherContainer& __value) {
+        std::copy(__value.cbegin(), __value.cend(), std::back_inserter(m_cotainer));
+
+        return *this;
+    }
+
     /** @todo rvalue writing operator*/
+    template <typename OtherContainer>
+    accumulator& operator=(OtherContainer&& __value) {
+        std::move(__value.begin(), __value.end(), std::back_inserter(m_cotainer));
+
+        return *this;
+    }
 
     /** @todo Iterator operations */
+    accumulator& operator*() {
+        return *this;
+    }
+
+    accumulator& operator++() {
+        return *this;
+    }
+
+    accumulator operator++(int) {
+        return *this;
+    }
+
+private:
+    Container &m_cotainer;
 };
 
 /**
@@ -18,4 +50,5 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>(container);
 }
