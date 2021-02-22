7cd1ea7d793983da659097ccc7d924172719fc85
Olha Kosiakova <okosiakova@luxoft.com>
2021-02-22 20:39:52 +0200

    algorithms_task_1_Kosiakova

--- algorithms_task_1/student/include/impl.h
+++ algorithms_task_1/student/include/impl.h
@@ -11,4 +11,16 @@
   * @return consecutive range
 */
 template<class FI, class Comparator = std::equal_to<typename std::iterator_traits<FI>::value_type>>
-std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {});
+std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {})
+{
+    auto firstIter = std::adjacent_find(start, end, comp);
+
+    if (firstIter == end) {
+        return std::make_pair(end, end);
+    }
+
+    using value_type = typename std::iterator_traits<FI>::value_type;
+    auto secondIter = std::find_if_not(firstIter, end,
+                                       [&](value_type elem){ return comp(elem, *firstIter); });
+    return std::make_pair(firstIter, secondIter);
+}
