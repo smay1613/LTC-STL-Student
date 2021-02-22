2a20316369d70603952273acf709e2f3f8213d6f
Oleksandr Khudyk <OKhudyk@luxoft.com>
2021-02-22 23:11:13 +0200

    iterators_task_2 Khudyk

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -2,7 +2,7 @@
 #include <iterator>
 
 template <class Container>
-struct accumulator
+struct accumulator : public std::iterator<std::output_iterator_tag, void, void, void, void>
 {
     /** @todo Iterator traits*/
     /** @todo Constructor with container*/
@@ -10,6 +10,27 @@ struct accumulator
     /** @todo rvalue writing operator*/
 
     /** @todo Iterator operations */
+
+    protected:
+        Container* container;
+    public:
+        explicit accumulator(Container& __x) : container(&__x){}
+
+        template<typename Input>
+        accumulator& operator=(const Input& __value) {
+            std::copy(__value.cbegin(), __value.cend(), std::back_inserter(*container));
+            return *this;
+        }
+
+        template<typename Input>
+        accumulator& operator=(Input&& __value) {
+            std::move(__value.begin(), __value.end(), std::back_inserter(*container));
+            return *this;
+        }
+
+        accumulator& operator*() { return *this; }
+        accumulator& operator++() { return *this; }
+        accumulator operator++(int) { return *this; }
 };
 
 /**
@@ -18,4 +39,5 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>{container};
 }
