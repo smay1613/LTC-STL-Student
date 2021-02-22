0ac3ee189819fd1f324523e7204406207efc74b3
Olha Kosiakova <okosiakova@luxoft.com>
2021-02-22 10:47:56 +0200

    Algorithms_task_2_Kosiakova

--- algorithms_task_2/student/src/impl.cpp
+++ algorithms_task_2/student/src/impl.cpp
@@ -1,1 +1,17 @@
 #include "impl.h"
+#include <algorithm>
+
+bool startsWith(const std::string& source, const std::string& prefix)
+{
+    /*
+     * substr
+     * strncmp
+     * rfind
+     * search
+     */
+    if (prefix.size() > source.size()) {
+        return false;
+    }
+
+    return std::equal(prefix.cbegin(), prefix.cend(), source.cbegin());
+}
