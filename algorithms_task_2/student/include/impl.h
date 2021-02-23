ff2b4378ca84d1ac28a7a518782691260ceb8833
Kostiantyn Kostiuchenko <kakostiuchenko@luxoft.com>
2021-02-23 08:11:04 +0200

    algorithms_task_2_kostiuchenko

--- algorithms_task_2/student/include/impl.h
+++ algorithms_task_2/student/include/impl.h
@@ -9,4 +9,12 @@
  *
  * @return true if source starts from a prefix
  */
-bool startsWith(const std::string& source, const std::string& prefix);
+bool startsWith(const std::string& source, const std::string& prefix)
+{
+    if (prefix.size() > source.size()) return false;
+    return prefix.cend() == std::mismatch(prefix.cbegin(), prefix.cend(), source.cbegin()).first;
+
+    //alternative
+//    if (prefix.size() > source.size()) return false;
+//    return std::equal(prefix.cbegin(), prefix.cend(), source.cbegin());
+}
