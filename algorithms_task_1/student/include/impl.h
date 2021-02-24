7bd32b19d653d8a8f9ef099a2301c4c2faf8cc42
veem <>
2021-02-25 00:23:44 +0200

    algorithms_task_1_verna

--- algorithms_task_1/student/include/impl.h
+++ algorithms_task_1/student/include/impl.h
@@ -11,4 +11,21 @@
   * @return consecutive range
 */
 template<class FI, class Comparator = std::equal_to<typename std::iterator_traits<FI>::value_type>>
-std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {});
+std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {})
+{
+    const auto groupStart = std::adjacent_find(start, end, comp);
+
+    if (groupStart == end)
+    {
+        return {end, end};
+    }
+
+    const auto& groupValue = *groupStart;
+    auto groupEnd = std::find_if_not(groupStart,
+                                     end,
+                                     [&groupValue, &comp] (decltype(groupValue) value) {
+                                        return comp(groupValue, value);
+                                     });
+
+    return {groupStart, groupEnd};
+}
