8ebbd7b805071814ea0baf70064dbfef2cf0b389
Vladyslav Fedorovskyi <VFedorovskyi@luxoft.com>
2021-02-24 11:16:04 +0200

    algorithm_task_5

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -10,4 +10,11 @@
  * @return gathered range
  */
 template <typename BiIt, typename UnaryPredicate>
-std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate);
+std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate)
+{
+    using constValueTypeReference = const typename std::iterator_traits<BiIt>::value_type&;
+    auto newBegin = std::partition(begin, position, [predicate](constValueTypeReference value)
+    { return !predicate(value); });
+    auto newEnd = std::partition(position, end, predicate);
+    return {newBegin, newEnd};
+}
