a752b3ada0c14d8ccfd351fcf14324cffbeede51
Oleksandr Minenko <ominenko@luxoft.com>
2021-02-24 12:03:48 +0200

    upd

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -11,8 +11,8 @@
  */
 template <typename BiIt, typename UnaryPredicate>
 std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate){
-    const auto _end= std::stable_partition(position,end,predicate);
-    const auto _beg =std::stable_partition(begin,position, [&](typename std::iterator_traits<BiIt>::value_type val){return !predicate(val);});
-
-    return std::pair<BiIt, BiIt>(_beg,_end);
+    const auto &_end= std::partition(position,end,predicate);
+    const auto &_beg =std::partition(begin,position,
+                                     [&](typename std::iterator_traits<BiIt>::value_type val){return !predicate(val);});
+    return std::make_pair(_beg,_end);
 }
