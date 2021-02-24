51115edae2c0a7cac89d218ba3df3418ceede8e6
Serhii Starodumov <SStarodumov@luxoft.com>
2021-02-24 10:58:29 +0200

    algorithms_task_5_Serhii_Starodumov

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -10,4 +10,10 @@
  * @return gathered range
  */
 template <typename BiIt, typename UnaryPredicate>
-std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate);
+std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate){
+    auto GBegin = std::partition(begin, position, [&predicate](const typename BiIt::value_type& item){
+        return !predicate(item);
+    });
+    auto GEnd = std::partition(position, end, predicate);
+    return {GBegin, GEnd};
+}
