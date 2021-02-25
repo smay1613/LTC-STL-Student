b7b1b27434b09e039d2cb5dbfc1732702facbcab
Oleksandr Khudyk <OKhudyk@luxoft.com>
2021-02-25 15:19:48 +0200

    algorithms_task_2 Khudyk

--- algorithms_task_2/student/src/impl.cpp
+++ algorithms_task_2/student/src/impl.cpp
@@ -1,1 +1,10 @@
 #include "impl.h"
+
+
+
+bool startsWith(const std::string& source, const std::string& prefix) {
+    if (prefix.size() > source.size())
+        return false;
+
+    return std::equal(prefix.cbegin(), prefix.cend(), source.cbegin());
+}
