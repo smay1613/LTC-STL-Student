ffeb06086304d9ecb260f5775e7cd8414ea9043e
chizarus <chizarus@gmail.com>
2021-02-26 11:49:04 +0200

    algorithms_task_7

--- algorithms_task_7/student/include/impl.h
+++ algorithms_task_7/student/include/impl.h
@@ -1,10 +1,20 @@
 #pragma once
 #include <numeric>
+#include <vector>
 #include <array>
+#include <iostream>
+#include <functional>
+#include <iterator>
 
 /**
  * @todo Implement function that will return N first fibonnaci numbers, started from 1
  * @return N first fibonacci numbers
  */
 template<size_t N>
-std::array<size_t, N> fibonacci();
+std::array<size_t, N> fibonacci() {
+    using namespace std;
+    array<size_t, N> fibonacciNumbers {1};
+    adjacent_difference(begin(fibonacciNumbers), prev(end(fibonacciNumbers)), next(begin(fibonacciNumbers)),
+                        plus<size_t> {});
+    return fibonacciNumbers;
+}
