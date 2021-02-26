8e630f0209723d280b88b75d09d53798500d0cb5
Kostiantyn Kostiuchenko <kakostiuchenko@luxoft.com>
2021-02-26 15:57:43 +0200

    algorithms_task_7_kostiuchenko

--- algorithms_task_7/student/include/impl.h
+++ algorithms_task_7/student/include/impl.h
@@ -7,4 +7,16 @@
  * @return N first fibonacci numbers
  */
 template<size_t N>
-std::array<size_t, N> fibonacci();
+std::array<size_t, N> fibonacci()
+{
+    //adjacent_difference
+    std::array<size_t, N> arr{1};
+    std::adjacent_difference(arr.begin(), std::prev(arr.end()), std::next(arr.begin()), std::plus<size_t>());
+    return arr;
+
+    // partial_sum
+//    std::array<size_t, N> arr{2, 1};
+//    std::partial_sum(arr.begin(), std::prev(arr.end(), 2), std::next(arr.begin(), 2));
+//    arr[0] = 1;
+//    return arr;
+}
