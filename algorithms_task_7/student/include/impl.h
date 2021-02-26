616c63b6ab684e3276f50ee0279cf5ba0decde10
Serhii Starodumov <SStarodumov@luxoft.com>
2021-02-26 12:16:27 +0200

    algorithms_task_7_Serhii_Starodumov

--- algorithms_task_7/student/include/impl.h
+++ algorithms_task_7/student/include/impl.h
@@ -7,4 +7,10 @@
  * @return N first fibonacci numbers
  */
 template<size_t N>
-std::array<size_t, N> fibonacci();
+std::array<size_t, N> fibonacci(){
+    std::array<size_t, N> arr_out={1};
+    std::adjacent_difference(arr_out.begin(), arr_out.end()-1, arr_out.begin()+1, [](size_t sum, size_t value){
+        return sum + value;
+    });
+    return arr_out;
+}
