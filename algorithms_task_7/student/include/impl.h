32b00862412b876fa35bf4f294442edf5ee21325
mrassokha <MRassokha@luxoft.com>
2021-02-26 12:21:32 +0200

    [Maksym Rassokha] Algorithms_task_7

--- algorithms_task_7/student/include/impl.h
+++ algorithms_task_7/student/include/impl.h
@@ -7,4 +7,10 @@
  * @return N first fibonacci numbers
  */
 template<size_t N>
-std::array<size_t, N> fibonacci();
+std::array<size_t, N> fibonacci() {
+    std::array<size_t, N> result = {1};
+    std::adjacent_difference(std::begin(result), std::prev(std::end(result)),
+                             std::next(std::begin(result)), std::plus<size_t>());
+
+    return result;
+};
