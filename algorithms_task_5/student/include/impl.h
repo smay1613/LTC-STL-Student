c1b482370905c4c0a4eb67e8905e6f996a2131b0
Olha Kosiakova <okosiakova@luxoft.com>
2021-02-28 15:29:02 +0200

    Algorithms_task_5_Kosiakova

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -10,4 +10,11 @@
  * @return gathered range
  */
 template <typename BiIt, typename UnaryPredicate>
-std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate);
+std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate)
+{
+    using value_type = typename std::iterator_traits<BiIt>::value_type;
+    auto notPredicate = [&] (const value_type& value) { return !predicate(value); };
+
+    return { std::partition(begin, position, notPredicate),
+             std::partition(position, end, predicate) };
+}
