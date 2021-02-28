5705dd2707dbc38e35381adc5e53499aff30e0ba
Olha Kosiakova <okosiakova@luxoft.com>
2021-02-28 15:56:12 +0200

    Algorithms_task_7_Kosiakova

--- algorithms_task_7/student/include/impl.h
+++ algorithms_task_7/student/include/impl.h
@@ -7,4 +7,9 @@
  * @return N first fibonacci numbers
  */
 template<size_t N>
-std::array<size_t, N> fibonacci();
+std::array<size_t, N> fibonacci()
+{
+    std::array<size_t, N> result {1};
+    std::adjacent_difference(result.begin(), std::prev(result.end()), std::next(result.begin()), std::plus<size_t>{});
+    return result;
+}
