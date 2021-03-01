c73776a552b1bf1f7b2d5f87bf24587d57578706
Vladyslav Pysarenko <vpysarenko@luxoft.com>
2021-03-01 16:02:27 +0200

    iterators task (1) - Pysarenko
    Fixed findings

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -2,16 +2,16 @@
 
 std::string reverseWords(const std::string& input)
 {
-    std::stringstream stream{input};
+    std::stringstream inputStream{input};
 
-    std::vector<std::string> parsed{std::istream_iterator<std::string>(stream),
+    std::vector<std::string> parsed{std::istream_iterator<std::string>(inputStream),
                 std::istream_iterator<std::string>()};
 
-    stream = std::stringstream();
-    std::reverse_copy(parsed.begin(), parsed.end(), std::ostream_iterator<std::string>{stream, " "});
+    std::stringstream outputStream;
+    std::reverse_copy(parsed.begin(), parsed.end(), std::ostream_iterator<std::string>{outputStream, " "});
 
-    std::string merged{stream.str()};
-    if(merged.length())
+    std::string merged{outputStream.str()};
+    if(!merged.empty())
         merged.pop_back();
 
     return merged;
