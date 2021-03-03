492800cc05fef127ecd31b8863dab54d0b67aeb5
Vladyslav Pysarenko <vpysarenko@luxoft.com>
2021-03-03 12:00:21 +0200

    algorithms task (2) - Pysarenko

--- algorithms_task_2/student/src/impl.cpp
+++ algorithms_task_2/student/src/impl.cpp
@@ -1,1 +1,8 @@
 #include "impl.h"
+
+bool startsWith(const std::string& source, const std::string& prefix)
+{
+    auto result = std::mismatch(std::begin(source), std::end(source), std::begin(prefix));
+
+    return result.second == std::end(prefix);
+}
