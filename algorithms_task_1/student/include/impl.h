41b011ee14d6a8035753c56f8fee24922b372db5
Vladyslav Pysarenko <vpysarenko@luxoft.com>
2021-03-03 11:28:46 +0200

    algorithms task (1) - Pysarenko

--- algorithms_task_1/student/include/impl.h
+++ algorithms_task_1/student/include/impl.h
@@ -11,4 +11,12 @@
   * @return consecutive range
 */
 template<class FI, class Comparator = std::equal_to<typename std::iterator_traits<FI>::value_type>>
-std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {});
+std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {})
+{
+    auto first = std::adjacent_find(start, end);
+    auto last = std::find_if(first, end, [&first](const typename std::iterator_traits<FI>::value_type& current){
+        return *first != current;
+    });
+
+    return std::make_pair(first, last);
+}
