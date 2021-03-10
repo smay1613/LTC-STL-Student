3a4c8451f1ab1ba185933bbdb0cf66f70d8063d2
Vladyslav Pysarenko <vpysarenko@luxoft.com>
2021-03-10 18:49:12 +0200

    algorithms task (5) - Pysarenko

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <functional>
 
 /**
  * @todo Implement function that will bring all values satisfying predicate to specified position
@@ -10,4 +11,18 @@
  * @return gathered range
  */
 template <typename BiIt, typename UnaryPredicate>
-std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate);
+std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate)
+{
+    std::function<bool(const typename std::iterator_traits<BiIt>::value_type&)> pred = predicate;
+    auto pivot = std::partition(begin, end, std::not1(pred));
+    auto newBegin = pivot;
+    auto newEnd = end;
+
+    if (std::distance(begin, position) < std::distance(begin, pivot))
+    {
+        newBegin = position;
+        newEnd = std::rotate(position, pivot, end);
+    }
+
+    return std::make_pair(newBegin, newEnd);
+}
