1023c82ea647f223e474f85d343ba9ecd1791427
Oleksandr Pichuryn <OPichuryn@luxoft.com>
2021-03-17 14:11:44 +0200

    [opichuryn] sequence_task_3

--- sequence_task_3/student/include/impl.h
+++ sequence_task_3/student/include/impl.h
@@ -7,46 +7,102 @@ template<class Container, class Song_t>
 class DynamicPlaylist
 {
 public:
-    /** @todo Member traits */
+    /** Member traits */
+    using value_type = typename Container::value_type;
+    using reference = typename Container::reference;
+    using const_reference = typename Container::const_reference;
+    using iterator = typename Container::iterator;
+    using const_iterator = typename Container::const_iterator;
+    using difference_type = typename Container::difference_type;
+    using size_type = typename Container::size_type;
+    using self_type = DynamicPlaylist<Container, Song_t>;
 
-    /** @todo Iterators */
+    /** Iterators */
+    const_iterator begin() const {
+        return m_tracklist.cbegin();
+    }
+
+    const_iterator end() const {
+        return m_tracklist.cend();
+    }
 
     DynamicPlaylist() = default;
+    DynamicPlaylist(const self_type& other) = default;
+    DynamicPlaylist(self_type&& other) = default;
+    self_type& operator=(const self_type& rhs) = default;
+    self_type& operator=(self_type&& rhs) = default;
 
-    /** @todo Constructor from two iterators*/
+    /** Constructor from two iterators*/
+    template <class Iter>
+    DynamicPlaylist(Iter iterBegin, Iter iterEnd)
+         : m_tracklist(iterBegin, iterEnd)
+     {}
 
-    /** @todo Set track as currently played from initializer */
+    /** Set track as currently played from initializer */
     template<class... Args>
-    const Song_t& play(Args&&... songData);
+    const Song_t& play(Args&&... songData) {
+        m_tracklist.emplace_front(std::forward<Args>(songData)...);
+        return current();
+    }
 
-    /** @todo Set track as currently played from lvalue */
-    const Song_t& play(const Song_t& song);
+    /** Set track as currently played from lvalue */
+    const Song_t& play(const Song_t& song) {
+        m_tracklist.push_front(song);
+        return current();
+    }
 
-    /** @todo Set track to be played next from initializer */
+    /** Set track to be played next from initializer */
     template<class... Args>
-    void playNext(Args&&... songData);
+    void playNext(Args&&... songData) {
+        if (!hasTracks()) {
+            play(std::forward<Args>(songData)...);
+            return;
+        }
+        m_tracklist.emplace(std::next(m_tracklist.begin()), std::forward<Args>(songData)...);
+    }
 
-    /** @todo Set track to be played next from lvalue */
-    void playNext(const Song_t& song);
+    /** Set track to be played next from lvalue */
+    void playNext(const Song_t& song) {
+        if (!hasTracks()) {
+            play(song);
+            return;
+        }
+        m_tracklist.insert(std::next(m_tracklist.begin()), song);
+    }
 
-    /** @todo Add track to the end of the queue from initializer */
+    /** Add track to the end of the queue from initializer */
     template<class... Args>
-    void add(Args&&... songData);
+    void add(Args&&... songData) {
+        m_tracklist.emplace_back(std::forward<Args>(songData)...);
+    }
 
-    /** @todo Add track to the end of the queue from lvalue */
-    void add(const Song_t& song);
+    /** Add track to the end of the queue from lvalue */
+    void add(const Song_t& song) {
+        m_tracklist.push_back(song);
+    }
 
-    /** @todo Get first track in playlist queue */
-    const Song_t& current() const;
+    /** Get first track in playlist queue */
+    const Song_t& current() const {
+        return m_tracklist.front();
+    }
 
-    /** @todo Skip to the next track in playlist, remove current */
-    void switchNext();
+    /** Skip to the next track in playlist, remove current */
+    void switchNext() {
+        if (!hasTracks()) {
+            throw  std::out_of_range("switchNext");
+        }
+        m_tracklist.pop_front();
+    }
 
-    /** @todo Amount of tracks in playlist */
-    size_type count() const;
+    /** Amount of tracks in playlist */
+    size_type count() const {
+        return m_tracklist.size();
+    }
 
-    /** @todo Checks if playlist has any playable tracks */
-    bool hasTracks() const;
+    /** Checks if playlist has any playable tracks */
+    bool hasTracks() const {
+        return count() > 0;
+    }
 
 private:
     Container m_tracklist;
