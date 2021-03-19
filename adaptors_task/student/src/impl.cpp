3faac431d4d0e6bb75d4d623a924815bc261415c
Oleksandr Pichuryn <OPichuryn@luxoft.com>
2021-03-19 14:09:59 +0200

    [opichuryn] adaptors_task_1

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,44 @@
 #include "impl.h"
-#include <vector>
 
-const std::vector<std::pair<char, char>> brackets {
+#include <stack>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+namespace {
+
+using BracketsPair = std::pair<char, char>;
+using BracketsDictionary = std::vector<BracketsPair>;
+const BracketsDictionary brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+} // namespace
+
+bool isValid(const std::string& source) {
+
+    std::stack<char> s;
+    for (auto c : source) {
+        auto iter = std::find_if(brackets.cbegin(), brackets.cend(), [c](const BracketsPair& p) {
+            return p.first == c || p.second == c;
+        });
+
+        if (iter == brackets.end()) {
+            continue;
+        }
+
+        if (iter->first == c) {
+            s.push(c);
+            continue;
+        }
+
+        if (s.empty() || iter->first != s.top()) {
+            return false;
+        }
+
+        s.pop();
+    }
+    return s.empty();
+}
