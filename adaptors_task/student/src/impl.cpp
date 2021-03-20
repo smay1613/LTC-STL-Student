edfe57b2bdd8cfe3f8679a1c91637a69932d2f1c
Kostiantyn Kostiuchenko <kakostiuchenko@luxoft.com>
2021-03-20 13:09:46 +0200

    adaptors_task_kostiuchenko

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,42 @@
 #include "impl.h"
 #include <vector>
+#include <algorithm>
+#include <stack>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    std::stack<char> openBrackets;
+
+    for (char ch : source)
+    {
+        auto pairIt = std::find_if(brackets.cbegin(), brackets.cend(),
+                                   [ch] (const std::pair<char, char>& p)
+                                        {
+                                            return p.first == ch || p.second == ch;
+                                        });
+
+        if (pairIt != brackets.cend())
+        {
+            if (pairIt->first == ch)
+            {
+                openBrackets.push(ch);
+            }
+            else
+            {
+                if (openBrackets.empty() || pairIt->first != openBrackets.top())
+                {
+                    return false;
+                }
+                openBrackets.pop();
+            }
+        }
+    }
+
+    return openBrackets.empty();
+}
