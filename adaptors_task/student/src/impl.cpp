e304c2552d668568096106be30b01a689f7f9229
Olha Kosiakova <okosiakova@luxoft.com>
2021-03-22 20:43:25 +0200

    Adaptors_task_Kosiakova

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,44 @@
 #include "impl.h"
-#include <vector>
+#include <map>
+#include <algorithm>
+#include <stack>
 
-const std::vector<std::pair<char, char>> brackets {
+const std::map<char, char> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    std::stack<char> checkStack;
+
+    auto isCloseBracket = [](const char symbol) {
+        auto result =std::find_if(brackets.begin(), brackets.end(), [symbol](std::pair<char, char>pair) {
+                return pair.second == symbol;
+    });
+        return result != brackets.end();
+    };
+
+    for (auto bracket : source) {
+        auto currentBracketPair = brackets.find(bracket);
+        if (currentBracketPair != brackets.end()) {
+            checkStack.emplace(bracket);
+        } else {
+            if (isCloseBracket(bracket)) {
+                if (checkStack.empty()) {
+                    return false;
+                } else {
+                    auto currentOpen = brackets.find(checkStack.top());
+                    if (currentOpen->second == bracket) {
+                        checkStack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return checkStack.empty();
+}
