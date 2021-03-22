0080c3ad3c08893717083bde9abb4faab4741486
Vladyslav Fedorovskyi <VFedorovskyi@luxoft.com>
2021-03-22 13:06:55 +0200

    [Fedorovskyi] ordered_associative_task

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -1,1 +1,15 @@
 #include "impl.h"
+#include <iostream>
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    static const std::map<size_t, size_t> marks {
+        {29, 1},
+        {49, 2},
+        {69, 3},
+        {89, 4},
+        {-1, 5},
+    };
+
+    auto it = marks.lower_bound(markIn100Point);
+    return it->second;
+}
