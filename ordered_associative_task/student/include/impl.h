4771c0e5a8e0ff3c4578fc694f5eb6d9ccd047e7
Oleksandr Minenko <ominenko@luxoft.com>
2021-03-22 12:40:06 +0200

    ordered_associative_task_ominenko

--- ordered_associative_task/student/include/impl.h
+++ ordered_associative_task/student/include/impl.h
@@ -15,4 +15,11 @@
  *
  * @return 1-5
  */
-size_t convertMarkTo5PointSystem(size_t markIn100Point);
+size_t convertMarkTo5PointSystem(size_t markIn100Point){
+    std::map<size_t,size_t> ranges={{29,1},{49,2},{69,3},{89,4}};
+     const auto it=ranges.lower_bound(markIn100Point);
+     if(ranges.cend()==it){
+         return 5;
+     }
+     return it->second;
+}
