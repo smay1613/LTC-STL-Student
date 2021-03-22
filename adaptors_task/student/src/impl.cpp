17f2b6047314b7021c5e71fc2818883e027d8a3d
Vladyslav Pysarenko <vpysarenko@luxoft.com>
2021-03-22 14:19:53 +0200

    adaptors task - Pysarenko

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,40 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
+#include <algorithm>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source)
+{
+    std::stack<char> bracketStack;
+
+    for (auto ch : source)
+    {
+        for (auto br : brackets)
+        {
+            if (ch == br.first)
+            {
+                bracketStack.emplace(ch);
+                break;
+            }
+            else if (ch == br.second)
+            {
+                if (bracketStack.empty() ||
+                    std::end(brackets) == std::find(std::begin(brackets), std::end(brackets), std::make_pair(bracketStack.top(), ch)))
+                {
+                    return false;
+                }
+
+                bracketStack.pop();
+                break;
+            }
+        }
+    }
+
+    return bracketStack.empty();
+}
