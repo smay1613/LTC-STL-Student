3433e693391377d10868bd56b5c33d7a52e464b7
Kostiantyn Kostiuchenko <kakostiuchenko@luxoft.com>
2021-03-22 13:10:17 +0200

    ordered_associative_task_kostiuchenko

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -1,1 +1,14 @@
 #include "impl.h"
+
+const std::map<size_t, size_t> bounds {
+  {0, 1},
+  {30, 2},
+  {50, 3},
+  {70, 4},
+  {90, 5}
+};
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    return (--bounds.upper_bound(markIn100Point))->second;
+}
