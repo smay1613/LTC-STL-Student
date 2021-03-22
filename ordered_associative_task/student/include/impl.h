558c82ef6df021b2f030055a4cbe3db235cf58e2
Olha Kosiakova <okosiakova@luxoft.com>
2021-03-22 19:17:27 +0200

    Ordered_associative_task_Kosiakova

--- ordered_associative_task/student/include/impl.h
+++ ordered_associative_task/student/include/impl.h
@@ -15,4 +15,19 @@
  *
  * @return 1-5
  */
-size_t convertMarkTo5PointSystem(size_t markIn100Point);
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    const std::map<size_t, size_t> marksMap {{0, 1},
+                                             {30, 2},
+                                             {50, 3},
+                                             {70, 4},
+                                             {90, 5}};
+
+    auto nearestMarks = marksMap.equal_range(markIn100Point);
+
+    if (nearestMarks.first == nearestMarks.second) {
+        return (*(--nearestMarks.first)).second;
+    } else {
+        return nearestMarks.first->second;
+    }
+}
