48cc80fd520bcc1d6ea94651989a835d0779505a
Vladyslav Pysarenko <vpysarenko@luxoft.com>
2021-03-22 14:42:30 +0200

    ordered associative task - Pysarenko

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -1,1 +1,19 @@
 #include "impl.h"
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point)
+{
+    std::map<size_t, size_t> markMap { {0, 1},
+                                       {30, 2},
+                                       {50, 3},
+                                       {70, 4},
+                                       {90, 5} };
+
+    auto markRange = markMap.equal_range(markIn100Point);
+
+    if (markRange.first == markRange.second)
+    {
+        --markRange.first;
+    }
+
+    return markRange.first->second;
+}
