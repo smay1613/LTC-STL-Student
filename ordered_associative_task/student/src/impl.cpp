bf3234a3b8b2335fedf41840fed56c8ba0a24e5f
Oleksandr Khudyk <OKhudyk@luxoft.com>
2021-03-22 19:31:00 +0200

    ordered_associative_task Khudyk

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -1,1 +1,11 @@
 #include "impl.h"
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point) {
+
+    const std::map<size_t, size_t> mapOfMarks { {0, 1}, {30, 2}, {50, 3}, {70, 4}, {90, 5} };
+
+    auto it = mapOfMarks.upper_bound(markIn100Point);
+
+    size_t res = it == mapOfMarks.end() ? 5 : (--it)->second;
+    return res;
+}
