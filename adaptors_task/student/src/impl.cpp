12983f8095162c323f8d09b335ac856382e40115
Vladyslav Fedorovskyi <VFedorovskyi@luxoft.com>
2021-03-23 22:09:26 +0200

    [Fedorovskyi] adaptors_task

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,37 @@
 #include "impl.h"
-#include <vector>
+#include <map>
+#include <stack>
+#include <algorithm>
 
-const std::vector<std::pair<char, char>> brackets {
+const std::map<char, char> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string &source)
+{
+    std::stack<char> checkBrackets;
+
+    auto isWrongPair = [](const std::map<char, char>& brackets, const char& symbol)
+    {
+        return std::any_of(brackets.begin(), brackets.end(),
+                                         [&symbol](const std::pair<const char, char>& pair) { return symbol == pair.second ? true : false; });
+    };
+
+    for(const auto& symbol : source)
+    {
+        if(brackets.find(symbol) != brackets.end())
+            checkBrackets.emplace(symbol);
+        else
+        {
+            if(!checkBrackets.empty() && symbol == brackets.at(checkBrackets.top()))
+                checkBrackets.pop();
+            else if ( isWrongPair(brackets, symbol) )
+                return false;
+
+        }
+    }
+
+    return checkBrackets.empty();
+}
