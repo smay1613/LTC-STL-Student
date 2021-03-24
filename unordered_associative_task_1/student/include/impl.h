3c52c0a67d192836a108b28c8f2fc7c3e9202793
chizarus <chizarus@gmail.com>
2021-03-24 17:50:51 +0200

    unordered_associative_task_1

--- unordered_associative_task_1/student/include/impl.h
+++ unordered_associative_task_1/student/include/impl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 struct Song
 {
@@ -16,6 +17,9 @@ public:
     }
 
     bool operator<(const Song&) = delete;
+    bool operator==(const Song& otherSong) const {
+        return track_name == otherSong.track_name;
+    }
 
 private:
     std::string track_name;
@@ -32,4 +36,15 @@ using playlist = std::vector<Song>;
  *
  * @return true if first playlist is anagram of second
  */
-bool is_same_content(const playlist& first_playlist, const playlist& second_playlist);
+bool is_same_content(const playlist& first_playlist, const playlist& second_playlist) {
+    auto hashSong = [](const Song& song){
+        return std::hash<std::string> {}(song.name());
+    };
+
+    std::unordered_multiset<Song, decltype(hashSong)> list1(first_playlist.begin(), first_playlist.end(),
+                                                                            first_playlist.size(), hashSong);
+    std::unordered_multiset<Song, decltype(hashSong)> list2(second_playlist.begin(), second_playlist.end(),
+                                                                            second_playlist.size(), hashSong);
+
+    return list1 == list2;
+}
