99b57bd29b8ac5bc5cbd05ad39e432a7bc327083
Oleksandr Khudyk <OKhudyk@luxoft.com>
2021-03-24 17:15:00 +0200

    unordered_associative_task_1 Khudyk

--- unordered_associative_task_1/student/src/impl.cpp
+++ unordered_associative_task_1/student/src/impl.cpp
@@ -1,1 +1,8 @@
 #include "impl.h"
+#include <unordered_set>
+
+bool is_anagram(const std::string& first, const std::string& second) {
+
+    return std::unordered_multiset<char>(first.cbegin(), first.cend()) ==
+           std::unordered_multiset<char>(second.cbegin(), second.cend());
+}
