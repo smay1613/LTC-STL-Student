260523e858b56b7f72fac4b435df80b7ae7ccdc3
Olha Kosiakova <okosiakova@luxoft.com>
2021-03-24 11:55:58 +0200

    Unordered_associative_task_1_Kosiakova

--- unordered_associative_task_1/student/src/impl.cpp
+++ unordered_associative_task_1/student/src/impl.cpp
@@ -1,1 +1,10 @@
 #include "impl.h"
+#include <unordered_set>
+
+bool is_anagram(const std::string& first, const std::string& second)
+{
+    std::unordered_multiset<char> firstSet(first.begin(), first.end(), first.size());
+    std::unordered_multiset<char> secondSet(second.begin(), second.end(), second.size());
+
+    return firstSet == secondSet;
+}
