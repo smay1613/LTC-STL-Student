75ef6955b99f3e626b69d63dd957d9e442731cdd
Kostiantyn Kostiuchenko <kakostiuchenko@luxoft.com>
2021-03-26 19:40:47 +0200

    pseudocontainers_task_kostiuchenko

--- pseudocontainers_task/student/include/impl.h
+++ pseudocontainers_task/student/include/impl.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <map>
 #include <unordered_map>
+#include <algorithm>
+#include <bitset>
 
 using calendar = std::string; // format: o - available, x - vacation
 using worker_id = size_t;
@@ -16,4 +18,29 @@ constexpr char vacationMarker = 'x';
  * @param workerVacations - pairs of worker and his calendar
  * @return Max intersection of two workers and intersection days count
  */
-std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_map<worker_id, calendar>& workerVacations);
+std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_map<worker_id, calendar>& workerVacations)
+{
+    std::tuple<worker_id, worker_id, days> result;
+
+    auto getCountOfIntersectedVacationDays = [](const calendar& calendar1, const calendar& calendar2)
+    {
+        const std::bitset<daysInMonth> bitCalendar1(calendar1, 0, daysInMonth, availableMarker, vacationMarker);
+        const std::bitset<daysInMonth> bitCalendar2(calendar2, 0, daysInMonth, availableMarker, vacationMarker);
+        return (bitCalendar1 & bitCalendar2).count();
+    };
+
+    for (const auto& workerVacation : workerVacations)
+    {
+        std::for_each(++workerVacations.find(workerVacation.first), workerVacations.end(),
+                      [&](const std::pair<worker_id, calendar>& nextWorkerVacation)
+        {
+            const days countOfIntersectedVacationDays = getCountOfIntersectedVacationDays(workerVacation.second, nextWorkerVacation.second);
+            if (countOfIntersectedVacationDays > std::get<2>(result))
+            {
+                result = std::make_tuple(workerVacation.first, nextWorkerVacation.first, countOfIntersectedVacationDays);
+            }
+        });
+    }
+
+    return result;
+}
