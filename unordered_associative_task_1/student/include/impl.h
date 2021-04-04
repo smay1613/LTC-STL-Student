3beff045f34bfb64d9a34aa2e07018656573e51c
mrassokha <MRassokha@luxoft.com>
2021-04-04 14:15:54 +0300

    [Maksym Rassokha] Unordered_associative_task_1

--- unordered_associative_task_1/student/include/impl.h
+++ unordered_associative_task_1/student/include/impl.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <unordered_set>
+#include <algorithm>
 
 struct Song
 {
@@ -21,6 +23,19 @@ private:
     std::string track_name;
 };
 
+struct SongHash
+{
+    size_t operator ()(const Song & song) const
+    {
+        return  std::hash<std::string>{}(song.name());
+    }
+};
+
+bool operator ==(const Song& songLhs, const Song& songRhs)
+{
+    return songLhs.name() == songRhs.name();
+}
+
 using playlist = std::vector<Song>;
 
 /**
@@ -32,4 +47,14 @@ using playlist = std::vector<Song>;
  *
  * @return true if first playlist is anagram of second
  */
-bool is_same_content(const playlist& first_playlist, const playlist& second_playlist);
+bool is_same_content(const playlist& first_playlist, const playlist& second_playlist) {
+    if (first_playlist.size() != second_playlist.size())
+        return false;
+
+    std::unordered_set<Song, SongHash>
+            playSetFirst(first_playlist.begin(), first_playlist.end());
+    std::unordered_set<Song, SongHash>
+            playSetSecond(second_playlist.begin(), second_playlist.end());
+
+    return playSetFirst == playSetSecond;
+}
