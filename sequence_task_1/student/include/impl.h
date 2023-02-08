1ee26396cf04af4548dd26660dc767685f4c22cf
Ionut Demsa <demsa_ionut@yahoo.com>
2023-02-08 18:14:06 +0200

    Solve task

--- sequence_task_1/student/include/impl.h
+++ sequence_task_1/student/include/impl.h
@@ -8,33 +8,71 @@ class StaticPlaylist
 {
 public:
     /** @todo Member traits */
+    typedef Song_t value_type;
+    typedef Song_t& reference;
+    typedef const Song_t& const_reference;
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+    typedef ptrdiff_t difference_type;
+    typedef size_t size_type;
 
     /** @todo Iterators */
+    const_iterator begin() const {
+        return m_tracklist.cbegin();
+    }
+    const_iterator end() const {
+        return m_tracklist.cend();
+    }
 
     StaticPlaylist() = default;
 
     /** @todo Constructor from any reversible sequence container */
-
+    template<class T>
+    StaticPlaylist(const T container_) {
+        m_tracklist.assign(container_.rbegin(), container_.rend());
+    }
     /** @todo Assignment from any reversible sequence container */
+    template<class T>
+    StaticPlaylist<Container, Song_t>& operator=(const T container_){
+        Container trackList;
+        trackList.assign(container_.rbegin(), container_.rend());
 
+        m_tracklist.swap(trackList);
+        return *this;
+    }
     /** @todo Add track from initializer */
     template<class... Args>
-    const Song_t& play(Args&&... songData);
+    const Song_t& play(Args&&... songData) {
+        m_tracklist.emplace_back(std::forward<Args>(songData)...);
+        return current();
+    }
 
     /** @todo Add track */
-    const Song_t& play(const Song_t& song);
+    const Song_t& play(const Song_t& song) {
+        m_tracklist.emplace_back(song);
+        return current();
+    }
 
     /** @todo Get first track in playlist stack */
-    const Song_t& current() const;
+    const Song_t& current() const {
+        return *std::prev(end());
+    }
 
     /** @todo Skip to the next track in playlist, remove current */
-    void switchNext();
+    void switchNext() {
+        if (!hasTracks()) throw std::out_of_range("Track list is empty");
+        m_tracklist.erase(std::prev(end()));
+    }
 
     /** @todo Amount of tracks in playlist */
-    size_type count() const;
+    size_type count() const {
+        return m_tracklist.size();
+    }
 
     /** @todo Checks if playlist has any playable tracks */
-    bool hasTracks() const;
+    bool hasTracks() const {
+        return !m_tracklist.empty();
+    }
 
 private:
     Container m_tracklist;
