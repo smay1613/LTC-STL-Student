22327b00e467374bb42c0750c845c1288ffd1456
Belaidi <sbelaidi@luxoft.com>
2023-02-09 11:38:01 +0100

    sequence_task_2

--- sequence_task_2/student/include/impl.h
+++ sequence_task_2/student/include/impl.h
@@ -14,4 +14,9 @@
  * @note See https://en.cppreference.com/w/cpp/algorithm/rotate
  */
 template<class T, class BI>
-void rotate(std::list<T>& list, BI begin, BI new_begin, BI end);
+void rotate(std::list<T>& list, BI begin, BI new_begin, BI end){
+    auto it = begin;
+    while (it != end){
+        list.splice(new_begin, list, it++);
+    }
+}
