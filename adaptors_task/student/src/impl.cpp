9f5e827200e6c01cac3a56e4c1ebe3ae824dd186
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-10 12:16:44 +0100

    adaptor_task completed

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,34 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source){
+	std::stack<char> stack;
+	auto find_pair = [](char c){
+		for (auto it = brackets.cbegin() ; it != brackets.cend(); it++) {
+		if (c == it->first || c == it->second)
+			return it;
+		}
+		return brackets.cend();
+	};
+	
+	for (auto c : source){
+		auto check = find_pair(c);
+		if (check != brackets.cend())
+		{
+			if (check->first == c)
+				stack.push(c);
+			else if (!stack.empty() && stack.top() == check->first && c == check->second)
+				stack.pop();
+			else
+				return false;
+		}
+	}
+	return stack.empty();
+}
