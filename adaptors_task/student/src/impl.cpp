346e29a7f0da90b1074a1882a94fafba88a2c467
Alberto Icardi <alberto.icardi@dxc.com>
2023-02-10 11:29:05 +0100

    adaptors task completed

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,41 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source){
+    std::stack<char> valid;
+
+    for(const char & c : source){
+        
+        for(const auto & b : brackets ){
+            if(c==b.second && valid.empty()){
+                return false;
+            }
+
+            if(c==b.first){
+                valid.push(c);
+                break;
+            }
+
+            if(c==b.second && !valid.empty() ){
+                auto popped = valid.top();
+                if(popped!=b.first){
+                    return false;
+                }
+                valid.pop();
+            }
+            
+        }
+    
+    }
+
+    return valid.empty();
+
+
+}
