ff88802d2f177d4ed9503bda34832918deb33ae2
carina-eliza-ganea <carinaeliza.ganea@dxc.com>
2023-02-10 13:23:08 +0200

    Finished adapters_task
    Modified impl.cpp. All tests pass.

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,37 @@
 #include "impl.h"
 #include <vector>
-
+#include <stack>
+#include <iostream>
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source){
+    std::stack<char> notClosed;
+
+    for(auto i = 0; i < source.size(); i++){
+        bool closed = false;
+
+        if(!notClosed.empty()){
+            for(auto b : brackets){
+                if(std::make_pair(notClosed.top(), source[i]) == b){
+                    notClosed.pop();
+                    closed = true;
+                    break;
+                }  
+            }
+        }
+        
+        if(!closed){
+            for( auto b : brackets){
+                if(source[i] == b.first || source[i] == b.second){
+                    notClosed.push(source[i]);
+                }
+            }
+        }
+    }
+
+    return notClosed.empty();
+}
