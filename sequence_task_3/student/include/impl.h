b69e991e569c26625dcb23f9594ca0e197d5a464
DariusAndreiParaschiv <paraschivdarius1@gmail.com>
2023-02-11 15:42:25 +0200

    task complete

--- sequence_task_3/student/include/impl.h
+++ sequence_task_3/student/include/impl.h
@@ -8,45 +8,100 @@ class DynamicPlaylist
 {
 public:
     /** @todo Member traits */
-
+    using value_type = typename Container::value_type;
+    using reference = typename Container::reference;
+    using const_reference = typename Container::const_reference;
+    using iterator = typename Container::iterator;
+    using const_iterator = typename Container::const_iterator;
+    using difference_type = typename Container::difference_type;
+    using size_type = typename Container::size_type;
     /** @todo Iterators */
+    const_iterator begin() const {
+        return m_tracklist.cbegin();
+    }
+
+    const_iterator end() const {
+        return m_tracklist.cend();
+    }
 
     DynamicPlaylist() = default;
 
     /** @todo Constructor from two iterators*/
-
+    template<typename iterator>
+    DynamicPlaylist(iterator first, iterator last)
+    {
+        m_tracklist.assign(first, last);
+    };
     /** @todo Set track as currently played from initializer */
     template<class... Args>
-    const Song_t& play(Args&&... songData);
+    const Song_t& play(Args&&... songData)
+    {
+        m_tracklist.emplace(begin(), std::forward<Args>(songData)...);
+        return current();
+    };
 
     /** @todo Set track as currently played from lvalue */
-    const Song_t& play(const Song_t& song);
+    const Song_t& play(const Song_t& song)
+    {
+        return m_tracklist.insert(begin(), song);
+        return current();
+    };
 
     /** @todo Set track to be played next from initializer */
     template<class... Args>
-    void playNext(Args&&... songData);
+    void playNext(Args&&... songData)
+    {
+        int pos = hasTracks() ? 1:0;
+        m_tracklist.emplace(begin()+pos, std::forward<Args>(songData)...);
+    };
 
     /** @todo Set track to be played next from lvalue */
-    void playNext(const Song_t& song);
+    void playNext(const Song_t& song)
+    {
+        int pos = hasTracks() ? 1:0;
+        m_tracklist.insert(begin()+pos, 1, song);
+    };
 
     /** @todo Add track to the end of the queue from initializer */
     template<class... Args>
-    void add(Args&&... songData);
+    void add(Args&&... songData)
+    {
+        m_tracklist.emplace_back(std::forward<Args>(songData)...);
+    };
 
     /** @todo Add track to the end of the queue from lvalue */
-    void add(const Song_t& song);
+    void add(const Song_t& song)
+    {
+        m_tracklist.insert(end(), song);
+    };
 
     /** @todo Get first track in playlist queue */
-    const Song_t& current() const;
+    const Song_t& current() const
+    {
+        return m_tracklist.front();
+    }
 
     /** @todo Skip to the next track in playlist, remove current */
-    void switchNext();
+    void switchNext()
+    {
+        if(m_tracklist.empty())
+        {
+            throw std::out_of_range("Out of Range");;
+        }
+        m_tracklist.pop_front();
+    };
 
     /** @todo Amount of tracks in playlist */
-    size_type count() const;
+    size_type count() const
+    {
+        return m_tracklist.size();
+    };
 
     /** @todo Checks if playlist has any playable tracks */
-    bool hasTracks() const;
+    bool hasTracks() const
+    {
+        return !m_tracklist.empty();
+    };
 
 private:
     Container m_tracklist;
