ce88db189d97ff90424d5deb172618044283fc75
Ionut Demsa <demsa_ionut@yahoo.com>
2023-02-13 21:15:54 +0200

    Solve task

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,43 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string &source) {
+    std::stack<char> notClosedBr;
+    for (char c : source) {
+        switch(c) {
+            case '(':
+            case '[':
+            case '{':
+                notClosedBr.push(c);
+                break;
+            case ')':
+                if(notClosedBr.empty() || notClosedBr.top() != brackets.at(0).first) {
+                    return false;
+                }
+                notClosedBr.pop();
+                break;
+            case ']':
+                if(notClosedBr.empty() || notClosedBr.top() != brackets.at(1).first) {
+                    return false;
+                }
+                notClosedBr.pop();
+                break;
+            case '}':
+                if (notClosedBr.empty() || notClosedBr.top() != brackets.at(2).first) {
+                    return false;
+                }
+                notClosedBr.pop();
+                break;
+            default:
+                break;
+        }
+    }
+    return notClosedBr.empty();
+}
