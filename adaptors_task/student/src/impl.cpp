cb1db12044fa07bdacdc4624e73c8a4b20238aca
carina-eliza-ganea <carinaeliza.ganea@dxc.com>
2023-02-13 13:04:52 +0200

    Corrected impl.cpp

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -2,36 +2,43 @@
 #include <vector>
 #include <stack>
 #include <iostream>
+#include <algorithm>
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
 };
 
+struct isBracket
+{
+  isBracket(char _c) : c(_c) {}
+  bool operator()(const std::pair<char,char>& bracketPair) const {
+    return c == bracketPair.first || c == bracketPair.second;
+  }
+  private:
+    char c;
+};
+
 bool isValid(const std::string& source){
-    std::stack<char> notClosed;
+    std::stack<char> openBrackets;
 
-    for(auto i = 0; i < source.size(); i++){
-        bool closed = false;
+    for(auto c : source){
+        const bool closeable_with_current_character = !openBrackets.empty() && 
+        std::find(brackets.begin(), brackets.end(), std::make_pair(openBrackets.top(), c)) != brackets.end();
 
-        if(!notClosed.empty()){
-            for(auto b : brackets){
-                if(std::make_pair(notClosed.top(), source[i]) == b){
-                    notClosed.pop();
-                    closed = true;
-                    break;
-                }  
+        if(openBrackets.empty() || !closeable_with_current_character){
+            
+            if(std::find_if(brackets.begin(), brackets.end(), isBracket(c)) != brackets.end()){
+                openBrackets.push(c);
             }
+
+        } else {
+
+            openBrackets.pop();
+
         }
-        
-        if(!closed){
-            for( auto b : brackets){
-                if(source[i] == b.first || source[i] == b.second){
-                    notClosed.push(source[i]);
-                }
-            }
-        }
+
     }
 
-    return notClosed.empty();
+    return openBrackets.empty();
 }
