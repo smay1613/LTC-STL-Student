f774dce2a9b32b78727a098636825de9171227ba
Alberto Icardi <alberto.icardi@dxc.com>
2023-02-13 13:19:06 +0100

    trying to make the code more meaningful and readable

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,6 +1,7 @@
 #include "impl.h"
 #include <vector>
 #include <stack>
+#include <tuple> 
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
@@ -8,34 +9,59 @@ const std::vector<std::pair<char, char>> brackets {
     {'{', '}'}
 };
 
-bool isValid(const std::string& source){
-    std::stack<char> valid;
+enum class KindOfChar {OpenBracket, ClosedBracket, NoBracket};
 
-    for(const char & c : source){
+std::pair<KindOfChar,  char> Find_KindOfChar_CorrespondingBracket(const char & c){
+    for(const auto & b : brackets ){
+         if(c==b.first){
+            return std::make_pair(KindOfChar::OpenBracket, b.second);
+         }
+
+         if (c==b.second){
+            return std::make_pair(KindOfChar::ClosedBracket, b.first);
+         }
+
         
-        for(const auto & b : brackets ){
-            if(c==b.second && valid.empty()){
-                return false;
-            }
-
-            if(c==b.first){
-                valid.push(c);
-                break;
-            }
-
-            if(c==b.second && !valid.empty() ){
-                auto popped = valid.top();
-                if(popped!=b.first){
-                    return false;
-                }
-                valid.pop();
-            }
-            
-        }
-    
     }
 
-    return valid.empty();
+     return std::make_pair(KindOfChar::NoBracket,'\0');
+
+};
+
+bool isValid(const std::string& source){
+
+    std::stack<char> StackOfOpenBrackets;
+
+    for(const char & c : source){
+
+        KindOfChar KindOfc;
+        char CorrespondingBracket;
+
+        std::tie( KindOfc, CorrespondingBracket) = Find_KindOfChar_CorrespondingBracket (c);
+
+        switch (KindOfc){
+            case KindOfChar::OpenBracket :
+                StackOfOpenBrackets.push(c);
+                break;
+            case KindOfChar::ClosedBracket :
+                if(StackOfOpenBrackets.empty()){
+                    return false;
+                } else {
+                    auto LastOpenBracket = StackOfOpenBrackets.top();
+                    if(LastOpenBracket!=CorrespondingBracket){
+                        return false;
+                    }
+                    StackOfOpenBrackets.pop();
+                }
+                break;
+
+            case KindOfChar::NoBracket:
+                break;   
+        
+        }
+    }
+
+    return StackOfOpenBrackets.empty();
 
 
 }
