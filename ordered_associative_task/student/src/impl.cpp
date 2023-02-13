86eefdc5bceb0ccc35611d1e0d94cff05fb38b84
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-13 15:27:44 +0100

    ordered_associative_task completed

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -1,1 +1,16 @@
 #include "impl.h"
+#include <map>
+
+size_t convertMarkTo5PointSystem(size_t markIn100Point) {
+	const size_t MAX = 100;
+	std::map<int, int> ranges {
+		{29, 1},
+		{49, 2},
+		{69, 3},
+		{89, 4},
+		{MAX, 5}
+	};
+	auto nearestRange = ranges.lower_bound(markIn100Point);
+	const bool isOutOfBound = nearestRange == ranges.end();
+	return isOutOfBound? ranges[MAX] : nearestRange->second;
+}
