e8f3f6681b1f522760ddb2c7851ee91da027ca6d
Pana <bpana@luxoft.com>
2023-02-13 17:16:58 +0200

    ordered_associative_task

--- ordered_associative_task/student/include/impl.h
+++ ordered_associative_task/student/include/impl.h
@@ -15,4 +15,22 @@
  *
  * @return 1-5
  */
-size_t convertMarkTo5PointSystem(size_t markIn100Point);
+std::size_t convertMarkTo5PointSystem(std::size_t markIn100Point)
+{
+    if(markIn100Point > 100)
+    {
+        markIn100Point = 100;
+    }
+    std::map<std::size_t,std::size_t> MarkDict{
+        {29,1},
+        {49,2},
+        {69,3},
+        {89,4},
+        {100,5}
+    };
+
+    auto val = MarkDict.lower_bound(markIn100Point);
+    return MarkDict[val->first];
+
+
+}
