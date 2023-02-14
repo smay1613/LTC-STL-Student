cb04e1e8116b014470a1705bd5c3d85d3b080cf3
carina-eliza-ganea <carinaeliza.ganea@dxc.com>
2023-02-14 13:11:59 +0200

    Solved task using unordered multisets
    All tests pass.

--- unordered_associative_task_1/student/include/impl.h
+++ unordered_associative_task_1/student/include/impl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 struct Song
 {
@@ -32,4 +33,21 @@ using playlist = std::vector<Song>;
  *
  * @return true if first playlist is anagram of second
  */
-bool is_same_content(const playlist& first_playlist, const playlist& second_playlist);
+bool is_same_content(const playlist& first_playlist, const playlist& second_playlist){
+    
+    if(first_playlist.size() != second_playlist.size()){
+        return false;
+    }
+    
+    std::unordered_multiset<std::string> playlist1 {}, playlist2 {};
+
+    for( const auto& s : first_playlist){
+        playlist1.insert(s.name());
+    }
+
+    for( const auto& s : second_playlist){
+        playlist2.insert(s.name());
+    }
+    
+    return playlist1 == playlist2;
+}
