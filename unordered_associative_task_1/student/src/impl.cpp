d2d3c0fe7d5cff5455bfd59eea4fceb846411b50
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-14 14:58:45 +0100

    unordered_assiociative_task_1 completed

--- unordered_associative_task_1/student/src/impl.cpp
+++ unordered_associative_task_1/student/src/impl.cpp
@@ -1,1 +1,43 @@
 #include "impl.h"
+#include <unordered_set>
+
+struct SongHash {
+	std::size_t operator ()(const Song& s) const {
+		return std::hash<std::string>{}(s.name());
+	}
+};
+
+struct SongEqual {
+	bool operator()(const Song& s1, const Song& s2) const {
+		return s1.name() == s2.name();
+	}
+};
+
+template<typename T>
+const bool isEqualSize(const T& p1, const T& p2) {
+	return p1.size() == p2.size();
+}
+
+using unordered_set = std::unordered_set<Song, SongHash, SongEqual>;
+
+const bool operator== (const unordered_set& p1, const unordered_set& p2) {
+	if (!isEqualSize(p1, p2)) {
+		return false;
+	}
+	for (auto &song : p2) {
+		if (p1.count(song) != 1){
+			return false;
+		}
+	}
+	return true;
+}
+
+
+bool is_same_content(const playlist& first_playlist, const playlist& second_playlist) {
+	if (!isEqualSize(first_playlist, second_playlist)){
+		return false;
+	}
+	unordered_set playlist1(first_playlist.begin(), first_playlist.end());
+	unordered_set playlist2(second_playlist.begin(), second_playlist.end());
+	return playlist1 == playlist2;
+}
