1be95fcfc361233a6386cbd5073c3452e247213a
Giuseppe Picciuca <giuseppe.picciuca@dxc.com>
2023-02-14 12:02:41 +0100

    implemented playlists comparison method

--- unordered_associative_task_1/student/include/impl.h
+++ unordered_associative_task_1/student/include/impl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <algorithm>
 
 struct Song
 {
@@ -23,6 +24,17 @@ private:
 
 using playlist = std::vector<Song>;
 
+bool is_in_playlist(const Song& search, const playlist& playlist)
+{
+    size_t count = std::count_if(playlist.begin(), playlist.end(), 
+        [&](const Song& song) {
+            return song.name() == search.name();
+        }
+    );
+
+    return count > 0;
+}
+
 /**
  * @todo Implement function that will check if one playlist has the same songs
  * as the second one
@@ -32,4 +44,22 @@ using playlist = std::vector<Song>;
  *
  * @return true if first playlist is anagram of second
  */
-bool is_same_content(const playlist& first_playlist, const playlist& second_playlist);
+bool is_same_content(const playlist& first_playlist, const playlist& second_playlist)
+{
+    if (first_playlist.size() != second_playlist.size())
+        return false;
+    
+    for (auto& it : first_playlist)
+    {
+        if (!is_in_playlist(it, second_playlist))
+            return false;
+    }
+
+    for (auto& it : second_playlist)
+    {
+        if (!is_in_playlist(it, first_playlist))
+            return false;
+    }
+
+    return true;
+}
