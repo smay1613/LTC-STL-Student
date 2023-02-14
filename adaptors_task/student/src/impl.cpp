45eb1a4f48d63925d8499810ac805907aaf4c2f7
carina-eliza-ganea <carinaeliza.ganea@dxc.com>
2023-02-14 10:12:27 +0200

    Made some changes

--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -9,35 +9,49 @@ const std::vector<std::pair<char, char>> brackets {
     {'{', '}'}
 };
 
-struct isBracket
-{
-  isBracket(char _c) : c(_c) {}
-  bool operator()(const std::pair<char,char>& bracketPair) const {
-    return c == bracketPair.first || c == bracketPair.second;
-  }
-  private:
-    char c;
-};
+bool add_or_fail(std::stack<char>& openBrackets, char& c ){
+    auto is_opening_bracket = [c] (const std::pair<char,char>& b) { return c == b.first;};
+    auto is_closing_bracket = [c] (const std::pair<char,char>& b) { return c == b.second;};
+
+    if(std::find_if(brackets.begin(), brackets.end(),is_closing_bracket)!= brackets.end()){
+        return false;
+    }
+
+    if(std::find_if(brackets.begin(), brackets.end(),is_opening_bracket)!= brackets.end()){
+        openBrackets.push(c);
+    }
+
+    return true;
+}
 
 bool isValid(const std::string& source){
     std::stack<char> openBrackets;
 
     for(auto c : source){
-        const bool closeable_with_current_character = !openBrackets.empty() && 
-        std::find(brackets.begin(), brackets.end(), std::make_pair(openBrackets.top(), c)) != brackets.end();
 
-        if(openBrackets.empty() || !closeable_with_current_character){
-            
-            if(std::find_if(brackets.begin(), brackets.end(), isBracket(c)) != brackets.end()){
-                openBrackets.push(c);
+        if( !openBrackets.empty()){
+            auto bracket_pair = std::find(brackets.begin(), brackets.end(), std::make_pair(openBrackets.top(), c));
+
+            const bool closeable_with_current_character = bracket_pair != brackets.end();
+
+            if(closeable_with_current_character){
+                
+                openBrackets.pop();
+
+            } else {
+                const bool still_valid = add_or_fail(openBrackets, c);
+
+                if(!still_valid){
+                    return false;
+                }
             }
-
         } else {
+            const bool still_valid = add_or_fail(openBrackets, c);
 
-            openBrackets.pop();
-
+            if(!still_valid){
+                return false;
+            }
         }
-
     }
 
     return openBrackets.empty();
