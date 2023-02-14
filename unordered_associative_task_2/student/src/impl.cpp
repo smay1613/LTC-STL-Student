7ff627b76ad0ec3e68a40133b13a2c94e11ded13
Giuseppe Picciuca <giuseppe.picciuca@dxc.com>
2023-02-14 16:10:31 +0100

    implemented duplicate file grouping methods

--- unordered_associative_task_2/student/src/impl.cpp
+++ unordered_associative_task_2/student/src/impl.cpp
@@ -25,11 +25,68 @@ auto xx_hash = [] (const std::string &file)
     return xxh::xxhash<64>(buffer);
 };
 
+/** @todo With help of std filesystem, lists all regular files under specified directory recursively*/
+std::vector<std::string> listFiles(const std::string& directory)
+{
+    std::vector<std::string> fileList;
+
+    for (auto& it : fs::recursive_directory_iterator(directory))
+    {
+        if (it.path().has_extension())
+        {
+            fileList.push_back(it.path().string());
+        }
+    }
+
+    return fileList;
+}
+
+/** @todo Implement function that will remove group in if it has only one or zero elements */
+void removeUniqueGroups(filtering_map& filteredData)
+{
+    for (auto it = filteredData.begin(); it != filteredData.end();)
+    {
+        if (it->second.size() <= 1)
+        {
+            it = filteredData.erase(it);
+        }
+        else
+        {
+            it++;
+        }
+    }
+}
+
+/** @todo Implement function that will transform map to a vector */
+std::vector<std::vector<std::string>> flattenGrouped (const filtering_map& grouped)
+{
+    std::vector<std::vector<std::string>> flatGroups;
+
+    std::vector<std::string> group;
+
+    for (auto& it : grouped)
+    {
+        group.assign(it.second.begin(), it.second.end());
+        flatGroups.push_back(group);
+    }
+
+    return flatGroups;
+}
 
 /** @todo Implement filtering files by different buckets by specified criteria*/
 template<class FilterCriteria>
 filtering_map filter(const std::vector<std::string>& files, FilterCriteria filter)
 {
+    filtering_map map;
+
+    for (auto& file : files)
+    {
+        auto key = filter(file);
+
+        map[key].push_back(file);
+    }
+
+    return map;
 }
 
 /** @note HELPER */
@@ -41,9 +98,21 @@ filtering_map groupDuplicates (const std::vector<std::string>& dataSource, Filte
     return grouped;
 }
 
+/**
+ * @todo Implement function that fill find duplicated files under the directory recursively
+ * @param rootPath - directory to check
+ * @return list of duplicated files, grouped by content
+ */
 std::vector<std::vector<std::string> > findDuplicates(const std::string &rootPath)
 {
-    // filter by size
-    // filter by content
-    // flatten
+    auto fileList = listFiles(rootPath);
+
+    auto grouped = groupDuplicates(fileList,
+        [](const std::string& file)
+        {
+            return xx_hash(file);
+        }
+    );
+
+    return { flattenGrouped(grouped) };
 }
