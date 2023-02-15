e537075fad5a2abdef8fec700ba4607b1f4f1422
andreitulpan <andrei.tulpan2@gmail.com>
2023-02-15 18:16:34 +0200

    adaptors task improved

--- adaptors_task/student/include/impl.h
+++ adaptors_task/student/include/impl.h
@@ -12,38 +12,4 @@
  *
  * @return true if expression is valid
  */
-bool isValid(const std::string& source) {
-    std::stack<char> stack;
-    for (auto const &c : source) {
-        if (c == '(' || c == '[' || c == '{') {
-            stack.push(c);
-        } else if (c == ')' || c == ']' || c == '}') {
-            if (stack.empty()) {
-                return false;
-            }
-            char ch = stack.top();
-            stack.pop();
-            switch (ch) {
-                case '(':
-                    if (c != ')') {
-                        return false;
-                    }
-                    break;
-                case '[':
-                    if (c != ']') {
-                        return false;
-                    }
-                    break;
-                case '{':
-                    if (c != '}') {
-                        return false;
-                    }
-                    break;
-            }
-        }
-    }
-    if (!stack.empty()) {
-        return false;
-    }
-    return true;
-}
+bool isValid(const std::string& source);
--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -6,3 +6,28 @@ const std::vector<std::pair<char, char>> brackets {
     {'[', ']'},
     {'{', '}'}
 };
+
+bool isValid(const std::string& source) {
+    std::stack<char> openedBrackets;
+    for (auto const &c : source) {
+        for (auto const &currentBracket : brackets) {
+            if (c == currentBracket.first) {
+                openedBrackets.push(c);
+                break;
+            } else if (c == currentBracket.second) {
+                if (openedBrackets.empty()) {
+                    return false;
+                }
+                char ch = openedBrackets.top();
+                openedBrackets.pop();
+                for (auto const &bracket : brackets) {
+                    if (ch == bracket.first && c != bracket.second) {
+                        return false;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    return openedBrackets.empty();
+}
