d0a7c727e1bd6c9946f696d62abf6da704b5acfa
Alberto Icardi <alberto.icardi@dxc.com>
2023-02-15 15:20:36 +0100

    pseudocontainer task completed

--- pseudocontainers_task/student/src/impl.cpp
+++ pseudocontainers_task/student/src/impl.cpp
@@ -1,1 +1,31 @@
 #include "impl.h"
+#include <bitset>
+std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_map<worker_id, calendar>& workerVacations){
+
+    worker_id resultWorker1, resultWorker2;
+    days resultDaysOverlap=0;
+
+
+    for (auto & itOuter : workerVacations){
+
+        std::bitset<daysInMonth> bitsetOuter(itOuter.second,0,itOuter.second.size(),'o','x');
+    
+        for(auto & itInner : workerVacations){
+            if(itInner!=itOuter){
+                std::bitset<daysInMonth> bitsetInner(itInner.second,0,itInner.second.size(),'o','x');
+                std::bitset<daysInMonth> bitsetOverlap = bitsetOuter & bitsetInner;
+                auto countOverlap = bitsetOverlap.count();
+
+                if(countOverlap > resultDaysOverlap){
+                    resultDaysOverlap=countOverlap;
+                    resultWorker1=itOuter.first;
+                    resultWorker2=itInner.first;
+
+                }
+            }
+        }
+
+    }
+
+    return std::tie(resultWorker1,resultWorker2,resultDaysOverlap);
+}
