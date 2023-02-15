76771d697c9049da01ff3e15e87d3b174ae50f5f
andreitulpan <andrei.tulpan2@gmail.com>
2023-02-15 23:35:37 +0200

    unordered_associative_task_2 done

--- unordered_associative_task_2/student/src/impl.cpp
+++ unordered_associative_task_2/student/src/impl.cpp
@@ -25,11 +25,44 @@ auto xx_hash = [] (const std::string &file)
     return xxh::xxhash<64>(buffer);
 };
 
+std::vector<std::string> listFiles(const std::string& directory) {
+    std::vector<std::string> listFiles;
+    for (auto const& dir_entry : fs::recursive_directory_iterator(directory)) {
+        if (fs::is_regular_file(dir_entry)) {
+            listFiles.push_back(dir_entry.path());
+        }
+    }
+    return listFiles;
+}
+
+void removeUniqueGroups(filtering_map& filteredData) {
+    auto iter = filteredData.begin();
+    while (iter != filteredData.end()) {
+        if (iter->second.size() < 2) {
+            iter = filteredData.erase(iter);
+        } else {
+            iter++;
+        }
+    }
+}
+
+std::vector<std::string> flattenGrouped (const filtering_map& grouped) {
+    std::vector<std::string> flattenGroups;
+    for (auto const& pair : grouped) {
+        flattenGroups.insert(flattenGroups.end(), pair.second.begin(), pair.second.end());
+    }
+    return flattenGroups;
+}
 
 /** @todo Implement filtering files by different buckets by specified criteria*/
 template<class FilterCriteria>
 filtering_map filter(const std::vector<std::string>& files, FilterCriteria filter)
 {
+    filtering_map filteredFiles;
+    for (auto const& file : files) {
+        filteredFiles[filter(file)].push_back(file);
+    }
+    return filteredFiles;
 }
 
 /** @note HELPER */
@@ -41,9 +74,25 @@ filtering_map groupDuplicates (const std::vector<std::string>& dataSource, Filte
     return grouped;
 }
 
-std::vector<std::vector<std::string> > findDuplicates(const std::string &rootPath)
+std::vector<std::vector<std::string>> findDuplicates(const std::string &rootPath)
 {
     // filter by size
+    filtering_map filesBySize = groupDuplicates(listFiles(rootPath),
+        [](const std::string& file) { return fs::file_size(file); });
+
+    std::vector<std::string> filesList = flattenGrouped(filesBySize);
+
     // filter by content
+    filtering_map filesByContent = groupDuplicates(filesList,
+        [](const std::string& file) { return xx_hash(file); });
+
     // flatten
+    std::vector<std::vector<std::string>> filesGroupedByContent;
+    for (auto const& fileAndContent : filesByContent) {
+        if (fileAndContent.second.size() > 0) {
+            std::vector<std::string> files(fileAndContent.second.begin(), fileAndContent.second.end());
+            filesGroupedByContent.push_back(files);
+        }
+    }
+    return filesGroupedByContent;
 }
