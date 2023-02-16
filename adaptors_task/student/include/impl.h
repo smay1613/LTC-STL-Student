9570bfb72e0355cd01fdacfb8006fb9b49712a07
DariusAndreiParaschiv <paraschivdarius1@gmail.com>
2023-02-16 15:14:20 +0200

    refactor code

--- adaptors_task/student/include/impl.h
+++ adaptors_task/student/include/impl.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <stack>
 /**
  * @todo isValid - check if given expression is valid - all open brackets are closed
  * by CORRESPONDING closed bracket.
@@ -12,51 +11,4 @@
  * @return true if expression is valid
  */
 
-bool isValid(const std::string& source)
-{
-    std::stack<char> brackets;
-    std::string close = ")]}";
-    std::string open = "([{";
-    for(int i = 0; i < source.size(); i++)
-    {
-        if(open.find(source[i]) != std::string::npos)
-        {
-            brackets.push(source[i]);
-        }
-        else if(close.find(source[i]) != std::string::npos)
-        {
-            if(brackets.empty())
-                return false;
-            switch(source[i])
-            {
-                case ')':
-                {
-                    if(brackets.top() != '(')
-                        return false;
-                    else
-                        brackets.pop();
-                    break;
-                }
-                case ']':
-                {
-                    if(brackets.top() != '[')
-                        return false;
-                    else
-                        brackets.pop();
-                    break;
-                }
-                case '}':
-                {
-                    if(brackets.top() != '{')
-                        return false;
-                    else
-                        brackets.pop();
-                    break;
-                }
-            } 
-        }
-    }
-    if(!brackets.empty())
-        return false;
-    return true;
-};
+bool isValid(const std::string& source);
--- adaptors_task/student/src/impl.cpp
+++ adaptors_task/student/src/impl.cpp
@@ -1,8 +1,48 @@
 #include "impl.h"
 #include <vector>
+#include <stack>
 
 const std::vector<std::pair<char, char>> brackets {
     {'(', ')'},
     {'[', ']'},
     {'{', '}'}
+};
+
+bool isValid(const std::string& source)
+{
+    std::stack<char> bracketsStack;
+    std::string close;
+    std::string open;
+
+    for(auto bracket : brackets)
+    {
+        open+=bracket.first;
+        close+=bracket.second;
+    }
+
+    for(auto character : source)
+    {
+        if(open.find(character) != std::string::npos)
+        {
+            bracketsStack.push(character);
+        }
+        else if(close.find(character) != std::string::npos)
+        {
+            if(bracketsStack.empty())
+                return false;
+            for(auto bracket : brackets)
+            {
+                if(bracket.second == character)
+                {
+                    if(bracketsStack.top() != bracket.first)
+                        return false;
+                    else
+                        bracketsStack.pop();
+                }
+            }
+        }
+    }
+    if(!bracketsStack.empty())
+        return false;
+    return true;
 };
