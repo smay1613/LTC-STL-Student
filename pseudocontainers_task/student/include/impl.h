413362677361ef9bad1fb988abf2744863c8431b
Claros <aclaros@luxoft.com>
2023-02-16 15:10:56 +0100

    task completed

--- pseudocontainers_task/student/include/impl.h
+++ pseudocontainers_task/student/include/impl.h
@@ -16,4 +16,33 @@ constexpr char vacationMarker = 'x';
  * @param workerVacations - pairs of worker and his calendar
  * @return Max intersection of two workers and intersection days count
  */
-std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_map<worker_id, calendar>& workerVacations);
+
+using iterator = std::unordered_map<worker_id, calendar>::const_iterator;
+void checkOverlap( int& count, int& overlap, worker_id& w1,  worker_id& w2, iterator& it, iterator& nextWorker){
+    if(count > overlap){
+        w1 = it->first;
+        w2 = nextWorker->first;
+        overlap = count;                
+    }
+    count = 0;    
+}
+
+std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_map<worker_id, calendar>& workerVacations){
+    worker_id w1 = 0, w2 = 0;
+    int overlap = 0, count = 0;
+
+    for(auto it = workerVacations.begin(); it != workerVacations.end(); ++it){
+        for(auto nextWorker = workerVacations.begin(); nextWorker != workerVacations.end(); nextWorker++){
+            if(it == nextWorker) continue;
+            auto workerMonth = it->second;
+            for(int j = 0; j<workerMonth.length(); j++){
+                if(workerMonth.at(j) == 'x' && workerMonth.at(j) == nextWorker->second.at(j))
+                    count++;
+                else 
+                    checkOverlap(count, overlap, w1, w2, it, nextWorker);             
+            }
+            checkOverlap(count, overlap, w1, w2, it, nextWorker);
+        }   
+    }
+    return std::tuple<worker_id, worker_id, days> (w1, w2, overlap);
+}
