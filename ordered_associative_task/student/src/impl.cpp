da26cd92aeb654972e1f569b54a741d3f97fb11b
Alberto Icardi <alberto.icardi@dxc.com>
2023-02-16 11:20:25 +0100

    ordered task updated

--- ordered_associative_task/student/src/impl.cpp
+++ ordered_associative_task/student/src/impl.cpp
@@ -1,6 +1,14 @@
 #include "impl.h"
-std::map<size_t,size_t> mapConverter={{29,1},{49,2},{69,3},{89,4},{SIZE_MAX,5}};
+
+std::map<size_t,size_t> mapConverter={{0,1},{30,2},{50,3},{70,4},{90,5}};
 
 size_t convertMarkTo5PointSystem(size_t markIn100Point){
-     return mapConverter.lower_bound(markIn100Point)->second;
+    auto result=mapConverter.lower_bound(markIn100Point);
+
+    if(result==mapConverter.end() || result->first>markIn100Point){
+        result--;
+    }
+
+    return result->second;
+    
 }
