a2a52383fb9fac52d7e9abd07929e40b875de659
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-17 11:29:40 +0100

    [pseudocontainers_task] changes applied

--- pseudocontainers_task/student/src/impl.cpp
+++ pseudocontainers_task/student/src/impl.cpp
@@ -5,7 +5,7 @@ bool isBiggerIntersection(const calendar& c1,const  calendar& c2, days& maxInter
 	std::bitset<daysInMonth> bitFormatCalendar1(c1, 0, c1.size(),availableMarker, vacationMarker);
 	std::bitset<daysInMonth> bitFormatCalendar2(c2, 0, c2.size(),availableMarker, vacationMarker);
 	days IntersectionCount =  (bitFormatCalendar1 & bitFormatCalendar2).count();
-	if (IntersectionCount > maxIntersectionCount) {
+	if (std::max(IntersectionCount, maxIntersectionCount) != maxIntersectionCount) {
 		maxIntersectionCount = IntersectionCount;
 		return true;
 	}
@@ -17,10 +17,10 @@ std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_m
 	std::tuple<worker_id, worker_id, days> maxIntersection;
 	if (!workerVacations.empty())
 	{
-		for (auto it = workerVacations.begin();  std::next(it) != workerVacations.end() ; ++it) {
-			for (auto it2 = std::next(it); it2 != workerVacations.end() ; ++it2) {
-				if (isBiggerIntersection(it->second, it2->second, maxIntersectionCount)){
-					maxIntersection = std::tie(it->first, it2->first, maxIntersectionCount);
+		for (auto worker1 = workerVacations.begin();  std::next(worker1) != workerVacations.end() ; ++worker1) {
+			for (auto worker2 = std::next(worker1); worker2 != workerVacations.end() ; ++worker2) {
+				if (isBiggerIntersection(worker1->second, worker2->second, maxIntersectionCount)){
+					maxIntersection = std::tie(worker1->first, worker2->first, maxIntersectionCount);
 				}
 			}
 		}
