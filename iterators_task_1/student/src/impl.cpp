1eb46f0eee790582ac80c264cb4d5273af5c89ab
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-17 11:11:29 +0100

    iterator_task_1 completed

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,2 +1,14 @@
 #include "impl.h"
 
+std::string reverseWords(const std::string& input) {
+	std::stringstream ss(input);
+	std::string reversed, word;
+	while (ss >> word) {
+		if (!reversed.empty()){
+			reversed = " " + reversed;
+		}
+		reversed = word + reversed;
+	}
+	return reversed;
+}
+
