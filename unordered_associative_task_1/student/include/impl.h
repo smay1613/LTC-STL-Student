c70b5f1376cec38d39306d646cdcf937259b135c
DariusAndreiParaschiv <paraschivdarius1@gmail.com>
2023-02-17 15:38:11 +0200

    task complete

--- unordered_associative_task_1/student/include/impl.h
+++ unordered_associative_task_1/student/include/impl.h
@@ -32,4 +32,27 @@ using playlist = std::vector<Song>;
  *
  * @return true if first playlist is anagram of second
  */
-bool is_same_content(const playlist& first_playlist, const playlist& second_playlist);
+bool is_same_content(const playlist& first_playlist, const playlist& second_playlist)
+{
+    if(first_playlist.size() != second_playlist.size())
+        return false;
+
+    playlist temp_playlist {first_playlist.begin(), first_playlist.end()};
+
+    for(auto track : second_playlist)
+    {
+        for(auto it = temp_playlist.begin(); it != temp_playlist.end(); ++it)
+        {
+            if(it->name() == track.name())
+            {
+                temp_playlist.erase(it);
+                break;
+            }
+        }
+    }
+
+    if(temp_playlist.empty())
+        return true;
+    else
+        return false;
+};
