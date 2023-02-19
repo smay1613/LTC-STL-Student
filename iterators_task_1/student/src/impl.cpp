3b4e4b019421c9da3e37a4c7003c998fb78cbbd9
andreitulpan <andrei.tulpan2@gmail.com>
2023-02-19 16:16:49 +0200

    iterators_task_1 done

--- iterators_task_1/student/src/impl.cpp
+++ iterators_task_1/student/src/impl.cpp
@@ -1,2 +1,17 @@
 #include "impl.h"
+#include <vector>
 
+std::string reverseWords(const std::string& input) {
+    std::stringstream get_words {input};
+    const std::vector<std::string> words {std::istream_iterator<std::string> {get_words}, std::istream_iterator<std::string> {}};
+
+    std::stringstream reverse_words {};
+    std::copy(words.rbegin(), words.rend(), std::ostream_iterator<std::string> {reverse_words, " "});
+
+    std::string output {reverse_words.str()};
+    if (!output.empty()) {
+        output.pop_back();
+    }
+
+    return output;
+}
