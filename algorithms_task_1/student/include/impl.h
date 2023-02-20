277526c361f53085baf24e30b11235a316d949ed
Giuseppe Picciuca <giuseppe.picciuca@dxc.com>
2023-02-20 11:27:35 +0100

    implemented consecutive_group method

--- algorithms_task_1/student/include/impl.h
+++ algorithms_task_1/student/include/impl.h
@@ -2,6 +2,7 @@
 #include <utility>
 #include <algorithm>
 #include <functional>
+#include <iostream>
 
 /**
   * @todo Implement function that will find group of equal elements
@@ -10,5 +11,22 @@
   * @param comparator - function to compare for equality
   * @return consecutive range
 */
-template<class FI, class Comparator = std::equal_to<typename std::iterator_traits<FI>::value_type>>
-std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {});
+template<class FI, class T = typename std::iterator_traits<FI>::value_type, class Comparator = std::equal_to<T>>
+std::pair<FI, FI> consecutive_group(FI start, FI end, Comparator comp = {})
+{
+  auto first = std::adjacent_find(start, end, comp);
+  
+  if (first == end)
+    return {end, end};
+
+  auto val = *first;
+
+  auto last = std::find_if_not(first, end, 
+    [&](T element)
+    {
+      return val == element;
+    }
+  );
+
+  return {first, last};
+}
