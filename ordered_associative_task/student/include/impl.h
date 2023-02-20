47de3068eb9978663f64f5265b29a93a4a159f07
Apostol <rapostol@luxoft.com>
2023-02-20 15:15:50 +0200

    ordered_associative_task_ApostolRazvan

--- ordered_associative_task/student/include/impl.h
+++ ordered_associative_task/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <map>
+#include <stddef.h>
 
 /**
  * @todo Implement function for conversion from 100 point system to 5 point system
@@ -15,4 +16,20 @@
  *
  * @return 1-5
  */
-size_t convertMarkTo5PointSystem(size_t markIn100Point);
+size_t convertMarkTo5PointSystem(size_t markIn100Point){
+    std::map<size_t, size_t> gradeMap = {
+                {0, 1},
+                {30, 2},
+                {50, 3},
+                {70, 4},
+                {90, 5}
+            };
+
+    for (auto it = gradeMap.rbegin(); it != gradeMap.rend(); ++it) {
+        if (markIn100Point >= it->first) {
+            return it->second;
+        }
+    }
+
+    return 0;
+}
