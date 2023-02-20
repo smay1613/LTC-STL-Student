710a0aca27895cbb98c45010c3e4db2090d6e5a7
Ionut Demsa <demsa_ionut@yahoo.com>
2023-02-20 17:35:56 +0200

    Implement task

--- algorithms_task_2/student/include/impl.h
+++ algorithms_task_2/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+using std::size_t;
 
 /**
  * @todo Check if strings starts with another string
@@ -9,4 +10,7 @@
  *
  * @return true if source starts from a prefix
  */
-bool startsWith(const std::string& source, const std::string& prefix);
+bool startsWith(const std::string& source, const std::string& prefix) {
+    std::size_t isPrefixOf = source.find(prefix);
+    return isPrefixOf != std::string::npos;
+}
