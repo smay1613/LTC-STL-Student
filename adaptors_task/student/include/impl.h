7fd67d066e32d6cab840e8f47e890042b034749a
Apostol <rapostol@luxoft.com>
2023-02-20 15:10:07 +0200

    adaptors_task_ApostolRazvan

--- adaptors_task/student/include/impl.h
+++ adaptors_task/student/include/impl.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <stack>
+#include <unordered_map>
 
 /**
  * @todo isValid - check if given expression is valid - all open brackets are closed
@@ -11,4 +13,18 @@
  *
  * @return true if expression is valid
  */
-bool isValid(const std::string& source);
+bool isValid(const std::string& source){
+    std::unordered_map<char, char> brackets = {{')', '('}, {']', '['}, {'}', '{'}};
+            std::stack<char> stack;
+            for (char c : source) {
+                if (brackets.count(c)) {
+                    if (stack.empty() || stack.top() != brackets[c]) {
+                        return false;
+                    }
+                    stack.pop();
+                } else if (c == '(' || c == '[' || c == '{') {
+                    stack.push(c);
+                }
+            }
+            return stack.empty();
+}
