9a605e2a22ddeb8e339d704ec4b7734ebb88a929
Pana <bpana@luxoft.com>
2023-02-20 13:57:52 +0200

    algorithms_task_2

--- algorithms_task_2/student/include/impl.h
+++ algorithms_task_2/student/include/impl.h
@@ -9,4 +9,7 @@
  *
  * @return true if source starts from a prefix
  */
-bool startsWith(const std::string& source, const std::string& prefix);
+bool startsWith(const std::string& source, const std::string& prefix)
+{
+    return source.find(prefix) == 0;
+}
