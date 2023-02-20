41ff614fbba27a61a95974660f33942afa77bf30
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-20 13:02:54 +0100

    algorithm_)task_2_Spinello

--- algorithms_task_2/student/src/impl.cpp
+++ algorithms_task_2/student/src/impl.cpp
@@ -1,1 +1,6 @@
 #include "impl.h"
+#include <algorithm>
+
+bool startsWith(const std::string& source, const std::string& prefix) {
+	return std::search(source.begin(), source.end(), prefix.begin(), prefix.end()) == source.begin();
+}
