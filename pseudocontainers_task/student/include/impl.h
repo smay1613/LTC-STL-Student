b57059e031ccfd225e430781775ce5aad9c0f3ab
Pana <bpana@luxoft.com>
2023-02-20 11:03:09 +0200

    refactored with bitset

--- pseudocontainers_task/student/include/impl.h
+++ pseudocontainers_task/student/include/impl.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include <map>
 #include <unordered_map>
+#include <bitset>
 
 using calendar = std::string; // format: o - available, x - vacation
 using worker_id = size_t;
@@ -18,40 +20,20 @@ constexpr char vacationMarker = 'x';
  */
 std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_map<worker_id, calendar>& workerVacations)
 {
-    days vacationdaysintersect = 0;
+    using CalendarList = std::bitset<daysInMonth>;
+    days vacationdaysintersect = 0,overlap=0;
     worker_id id1,id2;
-
-    auto isnotthesameworker = [](const std::pair<worker_id,calendar> &worker1 , const std::pair<worker_id,calendar> &worker2)
-    {
-        return worker1.first != worker2.first;
-    };
-
-    auto vacationMarkeroverlap = [](const std::pair<worker_id,calendar> &worker1 , const std::pair<worker_id,calendar> &worker2 , const days &day)
-    {
-        return worker1.second.at(day)==worker2.second.at(day) && worker1.second.at(day) == vacationMarker;
-    };
-
-    auto maxvactionoverlap = [&](const std::pair<worker_id,calendar> &worker1 , const std::pair<worker_id,calendar> &worker2)
-    {
-        days vactiondays = 0;
-        for(days day = 0; day < daysInMonth; day++)
-        {
-            if(vacationMarkeroverlap(worker1,worker2,day))
-            {
-                vactiondays++;
-            }
-        }
-        return vactiondays;
-    };
 
     for(auto const& worker1 : workerVacations)
     {
         for(auto const& worker2 : workerVacations)
         {
-            if(isnotthesameworker(worker1,worker2))
+            if(worker1 != worker2)
             {
-                days overlap = maxvactionoverlap(worker1,worker2);
-                if(overlap > vacationdaysintersect)
+                CalendarList vacationcalendar1(worker1.second,0,daysInMonth,availableMarker,vacationMarker);
+                CalendarList vacationcalendar2(worker2.second,0,daysInMonth,availableMarker,vacationMarker);
+                overlap = (vacationcalendar1&vacationcalendar2).count();
+                if(vacationdaysintersect < overlap)
                 {
                     vacationdaysintersect = overlap;
                     id1 = worker1.first;
@@ -60,6 +42,5 @@ std::tuple<worker_id, worker_id, days> getMaxIntersection(const std::unordered_m
             }
         }
     }
-
     return std::make_tuple(id1,id2,vacationdaysintersect);
 }
