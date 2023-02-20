8837dd63663670b4cebca64f75d1bc7baafca002
Alberto Icardi <alberto.icardi@dxc.com>
2023-02-20 12:03:15 +0100

    iterators task 1 updated

--- iterators_task_1/student/include/impl.h
+++ iterators_task_1/student/include/impl.h
@@ -14,13 +14,14 @@
  */
 std::string reverseWords(const std::string& input){
     std::stringstream in{input};
-    std::stringstream out{};
     std::vector<std::string> token{std::istream_iterator<std::string> {in},std::istream_iterator<std::string> {}};
     
     if(token.empty()){
         return "";
     }
-    std::move(token.rbegin(),token.rend(),std::ostream_iterator<std::string> {out, " "});
+
+    std::stringstream out{};
+    std::copy(token.rbegin(),token.rend(),std::ostream_iterator<std::string> {out, " "});
     
     std::string result=out.str();
 
