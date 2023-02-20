ba125273798c6d474093ff019cf82ca3d6638ac0
Iulia Corici <iuliastefania.corici@dxc.com>
2023-02-20 17:26:01 +0200

    [Iulia Corici] algorithms_task_2

--- algorithms_task_2/student/include/impl.h
+++ algorithms_task_2/student/include/impl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <algorithm>
 
 /**
  * @todo Check if strings starts with another string
@@ -9,4 +10,6 @@
  *
  * @return true if source starts from a prefix
  */
-bool startsWith(const std::string& source, const std::string& prefix);
+bool startsWith(const std::string& source, const std::string& prefix) {
+    return std::equal(prefix.begin(), prefix.end(), source.begin());
+}
