36013797fe6ea20a8debf36669acc7773f1ffa31
Belaidi <sbelaidi@luxoft.com>
2023-02-21 13:46:39 +0100

    iterators_task_1_Belaidi

--- iterators_task_1/student/include/impl.h
+++ iterators_task_1/student/include/impl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <sstream>
 #include <iterator>
+#include <string>
 
 /**
  * @todo Implement function that will reverse words in a sentence
@@ -10,4 +11,50 @@
  *
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
-std::string reverseWords(const std::string& input);
+
+std::string reverseString(const std:: string& input){
+    if (input.size() <= 1){
+        return input;
+    }
+    return input.back() + reverseString(input.substr(0, input.size() - 1));
+}
+
+void reduce_space(std::ostringstream& oss, std::stringstream& ss, char prev){
+    char curr;
+    if (ss.get(curr)){
+        if(std::isspace(curr)){
+            if(!std::isspace(prev)){
+                oss.put(' ');
+            }
+            reduce_space(oss, ss, curr);
+        }else {
+            oss.put(curr);
+            reduce_space(oss, ss, curr);
+        }
+    }
+}
+
+std::string reverseWords(const std::string& input){
+    std::istringstream iss(input);
+    std::string word;
+
+    iss >> word;
+
+    if (!iss) {
+        return "";
+    }
+
+    std::string output = word + std::string(std::istream_iterator<char>(iss), std::istream_iterator<char>());
+    
+    std::stringstream ss(output);
+    std::string reversed_output = std::string(std::istream_iterator<std::string>(ss), std::istream_iterator<std::string>());
+    std::string final_output = reverseString(reversed_output);
+
+
+    std::stringstream ss2(final_output);
+    std::ostringstream oss;
+    char prev = '\0';
+    reduce_space(oss, ss2, prev);
+    return oss.str();
+
+}
