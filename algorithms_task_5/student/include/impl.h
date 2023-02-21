44fa0a0ea1c5f993929bfd0bc33bd2fad52cf435
Ionut Demsa <demsa_ionut@yahoo.com>
2023-02-21 18:28:53 +0200

    Implement task

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -10,4 +10,10 @@
  * @return gathered range
  */
 template <typename BiIt, typename UnaryPredicate>
-std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate);
+std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate) {
+    auto negatePredicate = [&predicate](const typename BiIt::value_type& value) {return !predicate(value);};
+    return {
+        std::partition(begin, position, negatePredicate),
+        std::partition(position, end, predicate)
+    };
+}
