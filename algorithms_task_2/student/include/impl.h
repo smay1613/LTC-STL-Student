fb6045067b28864c858cc718fe235597c09bc5ce
andreitulpan <andrei.tulpan2@gmail.com>
2023-02-21 14:12:35 +0200

    algorithms_task_2 done

--- algorithms_task_2/student/include/impl.h
+++ algorithms_task_2/student/include/impl.h
@@ -9,4 +9,7 @@
  *
  * @return true if source starts from a prefix
  */
-bool startsWith(const std::string& source, const std::string& prefix);
+bool startsWith(const std::string& source, const std::string& prefix) {
+    auto mismatchPoint = std::mismatch(source.begin(), source.end(), prefix.begin());
+    return mismatchPoint.second == prefix.end();
+}
