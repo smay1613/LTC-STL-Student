cd0eab39caeab342d50d02bb7b8bed2d44d87743
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-21 12:39:27 +0100

    algorithms_task_4 completed

--- algorithms_task_4/student/include/impl.h
+++ algorithms_task_4/student/include/impl.h
@@ -12,4 +12,14 @@
  * @return new slided range position
  */
 template <typename RandomAccessIterator>
-std::pair<RandomAccessIterator, RandomAccessIterator> slide(RandomAccessIterator begin, RandomAccessIterator last, RandomAccessIterator pos);
+std::pair<RandomAccessIterator, RandomAccessIterator> slide(RandomAccessIterator begin, RandomAccessIterator last, RandomAccessIterator pos){
+	if (pos < begin){
+		auto new_last = std::rotate(pos, begin, last);
+		return std::make_pair(pos ,new_last);
+	}
+	else if (pos > begin) {
+		auto new_first = rotate(begin, last, pos);
+		return std::make_pair(new_first, pos);
+	}
+	return std::make_pair(begin, last);
+}
