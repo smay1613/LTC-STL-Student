6a598ab1b4a13dbbcad2935eb87002f81ba735c9
Giuseppe Picciuca <giuseppe.picciuca@dxc.com>
2023-02-22 11:51:46 +0100

    rewrote startsWith

--- algorithms_task_2/student/include/impl.h
+++ algorithms_task_2/student/include/impl.h
@@ -11,5 +11,7 @@
  */
 bool startsWith(const std::string& source, const std::string& prefix)
 {
-     return source.find(prefix) == 0;
+     return source.rfind(prefix, source.find_first_not_of(' ')) == 0; // ignore initial whitespaces
+     //return source.rfind(prefix, 0) == 0;                           // string must start without whitespaces
+     //return source.find(prefix) == 0;                               // prefix must be at beginning of source
 }
