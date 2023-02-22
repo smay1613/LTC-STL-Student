1391e8b8dcdde07c427581ddbbcbf0ce13f90ce1
Giuseppe Picciuca <giuseppe.picciuca@dxc.com>
2023-02-22 11:30:10 +0100

    fibo

--- algorithms_task_7/student/include/impl.h
+++ algorithms_task_7/student/include/impl.h
@@ -7,4 +7,10 @@
  * @return N first fibonacci numbers
  */
 template<size_t N>
-std::array<size_t, N> fibonacci();
+std::array<size_t, N> fibonacci()
+{
+     std::array<size_t, N> arr;
+     std::fill(arr.begin(), arr.end(), 1);
+     std::adjacent_difference(arr.begin(), arr.end(), arr.begin() + 1, std::plus<size_t>());
+     return arr;
+}
