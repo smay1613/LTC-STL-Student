4e797e40276a31bf55ffad15efc02f4e12c39da5
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-22 15:48:07 +0100

    algorithms_task_2 changes applied

--- algorithms_task_2/student/src/impl.cpp
+++ algorithms_task_2/student/src/impl.cpp
@@ -2,5 +2,5 @@
 #include <algorithm>
 
 bool startsWith(const std::string& source, const std::string& prefix) {
-	return std::search(source.begin(), source.end(), prefix.begin(), prefix.end()) == source.begin();
+	return std::equal(source.begin(), source.begin() + prefix.size(), prefix.begin());
 }
