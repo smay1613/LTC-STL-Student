5461bf6a20c1002b046d20ededa24dc3277b0fc8
Giuseppe Picciuca <giuseppe.picciuca@dxc.com>
2023-02-22 11:37:14 +0100

    implemented gather func

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -1,6 +1,21 @@
 #pragma once
 #include <algorithm>
 
+template<typename argType, typename PredicateType>
+struct NegatedUnaryLambda : std::unary_function<argType, bool>
+{
+     PredicateType predicate;
+
+     NegatedUnaryLambda(PredicateType fn)
+          : predicate(fn)
+     { }
+
+     bool operator()(argType val)
+     {
+          return !predicate(val);
+     }
+};
+
 /**
  * @todo Implement function that will bring all values satisfying predicate to specified position
  * @param begin - begin of seeking range
@@ -10,4 +25,10 @@
  * @return gathered range
  */
 template <typename BiIt, typename UnaryPredicate>
-std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate);
+std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate)
+{
+     auto first = std::stable_partition(begin, position, NegatedUnaryLambda<int, UnaryPredicate>(predicate));
+     auto last = std::stable_partition(position, end, predicate);
+
+     return {first, last};
+}
