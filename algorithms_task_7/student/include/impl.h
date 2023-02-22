c2822c82f26416b8cb27de179ec82ab4c98aaa5c
carina-eliza-ganea <carinaeliza.ganea@dxc.com>
2023-02-22 14:57:25 +0200

    Finished algorithms_task_7

--- algorithms_task_7/student/include/impl.h
+++ algorithms_task_7/student/include/impl.h
@@ -1,10 +1,18 @@
 #pragma once
 #include <numeric>
 #include <array>
+#include <functional>
 
 /**
  * @todo Implement function that will return N first fibonnaci numbers, started from 1
  * @return N first fibonacci numbers
  */
-template<size_t N>
-std::array<size_t, N> fibonacci();
+
+template<std::size_t N>
+std::array<std::size_t, N> fibonacci(){
+    std::array<std::size_t, N> fibo;
+    std::fill(fibo.begin(), fibo.end(), 1);
+
+    std::adjacent_difference(fibo.begin(), fibo.end(), fibo.begin() + 1, std::plus<std::size_t>());
+    return fibo;
+}
