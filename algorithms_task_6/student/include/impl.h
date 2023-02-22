078f5c8b901c271c18cd484f7bcc88b71f5398f6
Alberto Icardi <alberto.icardi@dxc.com>
2023-02-22 16:42:30 +0100

    algorithms task 6 completed

--- algorithms_task_6/student/include/impl.h
+++ algorithms_task_6/student/include/impl.h
@@ -14,7 +14,11 @@
 
 template <typename Sequence, typename FwIt>
 void insert_to_sorted(Sequence& target, const FwIt sourceBegin, const FwIt sourceEnd)
-{
+{   
+    for(auto it= sourceBegin; it!=sourceEnd; ++it){
+        target.insert(std::upper_bound(target.begin(),target.end(),*it),*it);
+    }
+
     static_assert(std::is_const<typename std::remove_reference<
                           typename std::iterator_traits<FwIt>::reference>::type>::value,
                       "FwIt must point to const data");
