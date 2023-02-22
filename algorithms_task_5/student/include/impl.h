8526d87bcb4b9567d913f7174ffae89a4c0cdbf1
Vincenzo Spinello <vincenzo.spinello@dxc.com>
2023-02-22 11:19:21 +0100

    algorithms_task_5 completed

--- algorithms_task_5/student/include/impl.h
+++ algorithms_task_5/student/include/impl.h
@@ -10,4 +10,8 @@
  * @return gathered range
  */
 template <typename BiIt, typename UnaryPredicate>
-std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate);
+std::pair<BiIt, BiIt> gather(BiIt begin, BiIt end, BiIt position, UnaryPredicate predicate) {
+	auto partStart = std::stable_partition(begin, position, [&predicate](const typename BiIt::value_type& elem){return !predicate(elem);});
+	auto partEnd = std::stable_partition(position, end, predicate);
+	return std::make_pair(partStart, partEnd);
+}
