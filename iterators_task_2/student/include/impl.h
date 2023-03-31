e8ae94310f3e3bd8c7a41f67510b64e6582ecb8f
MarianVJ <jorascuvlad@gmail.com>
2023-03-31 14:47:10 +0200

    First Commit

--- iterators_task_2/student/include/impl.h
+++ iterators_task_2/student/include/impl.h
@@ -5,11 +5,36 @@ template <class Container>
 struct accumulator
 {
     /** @todo Iterator traits*/
+    using iterator_category = typename std::output_iterator_tag;
+    using difference_type = void;
+    using pointer = void;
+    using reference = void;
+    using value_type = void;
+
     /** @todo Constructor with container*/
+    accumulator(Container& container) : container{ container } {}
+
     /** @todo lvalue writing operator*/
+    template<typename T>
+    accumulator<Container>& operator=(const T& value) {
+        container.insert(container.end(), value.begin(), value.end());
+        return *this;
+    }
+
     /** @todo rvalue writing operator*/
+    template<typename T>
+    accumulator<Container>& operator=(T&& value) {
+        std::move(value.begin(), value.end(), std::back_inserter(container));
+        return *this;
+    }
 
     /** @todo Iterator operations */
+    accumulator<Container>& operator*() { return *this; }
+    accumulator<Container>& operator++() { return *this; }
+    accumulator<Container>& operator--() { return *this; }
+
+private:
+    Container& container;
 };
 
 /**
@@ -18,4 +43,5 @@ struct accumulator
 template<class Container>
 accumulator<Container> make_accumulator(Container& container)
 {
+    return accumulator<Container>(container);
 }
